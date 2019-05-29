97aeb8c744cb4502e404bcd19317326e0ef0e20b
SILMarkFuller <mark_fuller@sil.org>
2019-05-29 08:36:30 -0500

    dependancy injection initially works

---
+++ BackendFramework/IWordContext.cs
@@ -0,0 +1,24 @@
+﻿using BackendFramework.ValueModels;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using static BackendFramework.Startup;
+
+namespace BackendFramework.Context
+{
+
+    public class WordContext : IWordContext
+    {
+        private readonly IMongoDatabase _db;
+        public WordContext(IOptions<Settings> options)
+        {
+            var client = new MongoClient(options.Value.ConnectionString);
+            _db = client.GetDatabase(options.Value.Database);
+        }
+
+        public IMongoCollection<Word> Words => _db.GetCollection<Word>("Database");
+    }
+    public interface IWordContext
+    {
+        IMongoCollection<Word> Words { get; }
+    }
+}
---
+++ BackendFramework/Models/Word.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BackendFramework.ValueModels
+{
+    public class Word
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Vernacular")]
+        public string Vernacular { get; set; }
+
+        [BsonElement("Gloss")]
+        public decimal Gloss { get; set; }
+
+        [BsonElement("Audio")]
+        public string Audio { get; set; }
+
+        [BsonElement("Timestamp")]
+        public string Timestamp { get; set; }
+    }
+}
