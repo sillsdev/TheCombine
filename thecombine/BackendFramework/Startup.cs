64694fa3060142ecb897bbcb1806539776dc0a5b
James Pruitt <PruittJ@dallas.sil.org>
2019-05-31 10:04:45 -0500

    Add npm dependencies

--- thecombine/BackendFramework/Startup.cs
+++ thecombine/BackendFramework/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using BackendFramework.Services;
 
 namespace BackendFramework
 {
