1d1cef1c364df2b3231619d0adffdbe79b45d8e3
Jim Grady <jimgrady.jg@gmail.com>
2019-06-12 11:24:23 -0400

    Create common install & test structure for Ansible roles
    Structure the ansible roles so that main.yml calls install.yml with the "install" tag and test.yml with the "test" tag for each role.

--- deploy/setup-target.sh
+++ deploy/setup-target.sh
@@ -26,20 +26,27 @@
 
 function print_usage()
 {
-  echo "Usage: $0 [-c|--copyid|-h|--help|-t] user@machinename"
-  echo "where:"
-  echo "   -c"
-  echo "   --copyid   will use ssh-copy-id to copy your id to user@machinename"
-  echo "              so that you do not need to enter your password to connect"
-  echo "   -h"
-  echo "   --help     print this message"
-  echo "   -t         test argument parsing of the script - does not setup the"
-  echo "              target"
+  cat << .EOM
+Usage: $0 [options] user@machinename
+ where:
+   -c, --copyid
+              will use ssh-copy-id to copy your id to user@machinename
+              so that you do not need to enter your password to connect
 
+   -h, --help
+              print this message
+
+   -i, --install
+              run the installation portion of the playbook
+
+   -t, --test
+              run the test portion of the playbook
+.EOM
 }
 
 COPYID=0
-JUST_TEST=0
+TAGS=""
+declare -a TagList
 while [ "$#" -gt 0 ]; do
   case $1 in
     "-c" | "--copyid")
@@ -49,8 +56,11 @@ while [ "$#" -gt 0 ]; do
         print_usage
         exit 0
         ;;
-    "-t")
-        JUST_TEST=1
+    "-i" | "--install")
+        TagList+=("install")
+        ;;
+    "-t" | "--test")
+        TagList+=("test")
         ;;
     -*)
         echo "option $1 not recognized - ignored"
@@ -67,16 +77,13 @@ if [[ -n "$TARGET" ]] ; then
   MACHINE=`expr $TARGET : '.*@\([^@]*\)$'`
 fi
 
+if [[ ${#TagList[@]} -gt 0 ]] ; then
+  TAGS="--tags "$(IFS=, ; echo "${TagList[*]}")
+fi
+
 if [[ ! ( -n "$USER" && -n "$MACHINE" ) ]] ; then
   print_usage
   exit 1
-fi
-
-if [ "$JUST_TEST" -eq 1 ] ; then
-  echo "USER == $USER"
-  echo "MACHINE == $MACHINE"
-  echo "COPYID == $COPYID"
-  exit 0
 fi
 
 echo "Setting up $TARGET"
@@ -85,6 +92,6 @@ if [ "$COPYID" -eq 1 ] ; then
   echo "Copying ssh id to $USER@$MACHINE"
   ssh-copy-id "$USER@$MACHINE"
 fi
-CMD_STRING="ansible-playbook -i hosts playbook_setup.yml --limit $MACHINE -u $USER -K"
+CMD_STRING="ansible-playbook -i hosts playbook_setup.yml --limit $MACHINE -u $USER -K ${TAGS}"
 echo -e "Running \"${CMD_STRING}\""
 ${CMD_STRING}
