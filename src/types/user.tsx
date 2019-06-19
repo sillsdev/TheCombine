9f2f378dc31ff19057b773c37c4476a337e95721
James Pruitt <jamescpruitt@gmail.com>
2019-06-19 16:58:00 +0000

    Use api wrapper (#50)
    * Add authentication to wrapper
    
    * Update user model
    
    * Use api wrapper for available routes
    
    * Add authenticate methods to wrapper

--- src/types/user.tsx
+++ src/types/user.tsx
@@ -1,11 +1,27 @@
 export class User {
   name: string;
   username: string;
-  id: number;
+  id: string;
+  avatar: string;
+  email: string;
+  otherConnectionField: string;
+  workedProjects: string[];
+  agreement: boolean;
+  password: string;
+  uiLang: string;
+  token: string;
 
-  constructor(name: string, username: string, id: number) {
+  constructor(name: string, username: string, password: string) {
     this.name = name;
     this.username = username;
-    this.id = id;
+    this.id = "";
+    this.avatar = "";
+    this.email = "";
+    this.otherConnectionField = "";
+    this.workedProjects = [];
+    this.agreement = false;
+    this.password = password;
+    this.uiLang = "";
+    this.token = "";
   }
 }
