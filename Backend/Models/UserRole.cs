4b237d23ca3a9f574510e4ee92d97e7c538883bc
sam-skinner <46290787+sam-skinner@users.noreply.github.com>
2019-06-19 17:00:56 -0500

    Add UserRole API Testing (#58)
    * Add UserRole API Testing
    
    * Merge master into user-role-testing
    
    * Merge master into user-role-testing
    
    * Make permission enum more readable

--- Backend/Models/UserRole.cs
+++ Backend/Models/UserRole.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BackendFramework.ValueModels
 {
@@ -15,6 +16,58 @@ namespace BackendFramework.ValueModels
 
         [BsonElement("history")]
         public List<History> History { get; set; }
+
+        public UserRole()
+        {
+            Id = "";
+            Permission = new List<Permission>();
+            History = new List<History>();
+        }
+
+        public UserRole Clone()
+        {
+            UserRole clone = new UserRole
+            {
+                Id = Id.Clone() as string,
+                Permission = new List<Permission>(),
+                History = new List<History>()
+            };
+
+            //Enums like Permission have no need to be cloned
+            foreach(Permission permission in Permission)
+            {
+                clone.Permission.Add(permission);
+            }
+
+            foreach(History history in History)
+            {
+                clone.History.Add(history.Clone() as History);
+            }
+
+            return clone;
+        }
+
+        public bool ContentEquals(UserRole other)
+        {
+            return
+                other.Permission.Count == Permission.Count &&
+                other.Permission.All(Permission.Contains) &&
+                other.History.Count == History.Count &&
+                other.History.All(History.Contains);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                UserRole other = obj as UserRole;
+                return other.Id.Equals(Id) && ContentEquals(other);
+            }
+        }
     }
 
     public enum Permission
@@ -45,5 +98,17 @@ namespace BackendFramework.ValueModels
         public int NumSteps { get; set; }
         [BsonElement("stepData")]
         public dynamic StepData { get; set; }
+
+        public History Clone()
+        {
+            History clone = new History
+            {
+                GoalId = GoalId,
+                NumSteps = NumSteps,
+                StepData = StepData.Clone() as dynamic
+            };
+
+            return clone;
+        }
     }
 }
