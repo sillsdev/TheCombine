522ad6cb43ffda9261fea816733e8a91f19cd198
Simeon <51300614+SimeonSIL@users.noreply.github.com>
2019-06-20 13:20:22 -0500

    Upload character inventory to backend project (#61)
    * Add current project to store state
    
    * Add project to state
    
    * Fix root reducer
    
    * Add uploading functionality to Character Inventory creation component
    
    * Clean up create project actions
    
    * Change defaultproject to const
    
    * Remove alert
    
    * Remove temp changes
    
    * Change let to const
    
    * Fix project creation

--- src/components/CreateProject/CreateProjectActions.tsx
+++ src/components/CreateProject/CreateProjectActions.tsx
@@ -1,9 +1,8 @@
 import { Dispatch } from "react";
-import { authHeader } from "../Login/AuthHeaders";
-import { breakpoints } from "@material-ui/system";
 import { history } from "../App/component";
 import * as backend from "../../backend";
-import { Project } from "../../types/project";
+import { Project, defaultProject } from "../../types/project";
+import { setCurrentProject, ProjectAction } from "../Project/ProjectActions";
 
 export const CREATE_PROJECT = "CREATE_PROJECT";
 export type CREATE_PROJECT = typeof CREATE_PROJECT;
@@ -19,31 +18,22 @@ type CreateProjectType = CREATE_PROJECT;
 export interface CreateProjectAction {
   type: CreateProjectType;
   payload: CreateProjectData;
+  project?: Project;
 }
 
 //thunk action creator
 export function asyncCreateProject(name: string, languageData?: File) {
-  return async (dispatch: Dispatch<CreateProjectAction>) => {
+  return async (dispatch: Dispatch<CreateProjectAction | ProjectAction>) => {
     // Create project
-    let project: Project = {
-      id: "",
-      name: name,
-      semanticDomains: [],
-      userRoles: "",
-      vernacularWritingSystem: "",
-      analysisWritingSystems: [],
-      characterSet: [],
-      customFields: [],
-      wordFields: [],
-      partsOfSpeech: [],
-      words: []
-    };
-    await backend.createProject(project);
+    let project: Project = { ...defaultProject };
+    project.name = name;
+    let createdProject = await backend.createProject(project);
+    dispatch(setCurrentProject(createdProject));
 
     // Upload words
     if (languageData) {
       backend
-        .uploadLift(project, languageData)
+        .uploadLift(createdProject, languageData)
         .then(res => {
           dispatch(createProject(name, languageData));
           history.push("/nav");
--- src/components/CreateProject/CreateProjectComponent.tsx
+++ src/components/CreateProject/CreateProjectComponent.tsx
@@ -8,7 +8,6 @@ import {
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { Grid } from "@material-ui/core";
-import { history } from "../App/component";
 
 export interface CreateProjectProps {
   createProject?: (name: string, languageData: File) => void;
