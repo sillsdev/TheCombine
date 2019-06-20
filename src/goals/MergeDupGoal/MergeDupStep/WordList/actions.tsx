32766b4e4006aa888624eb7487f5672976c2bc65
James Pruitt <james_pruitt@sil.org>
2019-06-20 14:08:36 -0500

    Add sorting to word list

--- src/goals/MergeDupGoal/MergeDupStep/WordList/actions.tsx
+++ src/goals/MergeDupGoal/MergeDupStep/WordList/actions.tsx
@@ -14,29 +14,45 @@ export type REMOVE_LIST_WORD = typeof REMOVE_LIST_WORD;
 export const CLEAR_LIST_WORDS = "CLEAR_LIST_WORDS";
 export type CLEAR_LIST_WORDS = typeof CLEAR_LIST_WORDS;
 
+export const SET_LIST_WORDS_SORT = "SET_LIST_WORDS_SORT";
+export type SET_LIST_WORDS_SORT = typeof SET_LIST_WORDS_SORT;
+
 export interface WordListAction {
-  type: ADD_LIST_WORD | REMOVE_LIST_WORD | CLEAR_LIST_WORDS;
-  payload: Word[];
+  type:
+    | ADD_LIST_WORD
+    | REMOVE_LIST_WORD
+    | CLEAR_LIST_WORDS
+    | SET_LIST_WORDS_SORT;
+  payload: { words: Word[]; sort?: (a: Word, b: Word) => number };
+}
+
+export function setListWordsSort(
+  sort: (a: Word, b: Word) => number
+): WordListAction {
+  return {
+    type: SET_LIST_WORDS_SORT,
+    payload: { words: [], sort }
+  };
 }
 
 export function clearListWords(): WordListAction {
   return {
     type: CLEAR_LIST_WORDS,
-    payload: []
+    payload: { words: [] }
   };
 }
 
 export function addListWords(words: Word[]): WordListAction {
   return {
     type: ADD_LIST_WORD,
-    payload: words
+    payload: { words }
   };
 }
 
 export function removeListWords(words: Word[]): WordListAction {
   return {
     type: REMOVE_LIST_WORD,
-    payload: words
+    payload: { words }
   };
 }
 
--- src/goals/MergeDupGoal/MergeDupStep/WordList/reducer.tsx
+++ src/goals/MergeDupGoal/MergeDupStep/WordList/reducer.tsx
@@ -2,31 +2,41 @@ import {
   WordListAction,
   ADD_LIST_WORD,
   REMOVE_LIST_WORD,
-  CLEAR_LIST_WORDS
+  CLEAR_LIST_WORDS,
+  SET_LIST_WORDS_SORT
 } from "./actions";
 import { Word } from "../../../../types/word";
 
 export interface WordListState {
   words: Word[];
+  sortComparison: (wordA: Word, wordB: Word) => number;
 }
 
 export const defaultState: WordListState = {
-  words: []
+  words: [],
+  sortComparison: (a: Word, b: Word): number =>
+    a.vernacular.localeCompare(b.vernacular)
 };
 
 export const wordListReducer = (
   state: WordListState = defaultState, //createStore() calls each reducer with undefined state
   action: WordListAction
 ): WordListState => {
-  let words: Word[];
   switch (action.type) {
+    case SET_LIST_WORDS_SORT:
+      if (action.payload.sort) {
+        return { ...state, sortComparison: action.payload.sort };
+      } else {
+        return state;
+      }
     case ADD_LIST_WORD: //_LIST_WORD actions affect the list of possible duplicates
-      words = state.words;
-      words = words.concat(action.payload);
+      var words = state.words;
+      words = words.concat(action.payload.words);
+      words = words.sort(state.sortComparison);
       return { ...state, words };
     case REMOVE_LIST_WORD:
-      words = state.words;
-      action.payload.forEach(word => {
+      var words = state.words;
+      action.payload.words.forEach(word => {
         // finds last matching by index
         var foundIndex = words.lastIndexOf(word);
         // remove previously found word
