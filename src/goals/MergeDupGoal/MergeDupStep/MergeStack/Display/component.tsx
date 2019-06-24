1170db752eb3614d7c6c98fa449c6a5ee7e586eb
James Pruitt <james_pruitt@sil.org>
2019-06-24 15:29:35 -0500

    Fix broken reorg

--- src/goals/MergeDupGoal/MergeDupStep/MergeStack/Display/component.tsx
+++ src/goals/MergeDupGoal/MergeDupStep/MergeStack/Display/component.tsx
@@ -6,89 +6,97 @@ import WordContent from "../../WordCard";
 import { Sense } from "../../component";
 
 export interface StackDisplayProps {
-	sense: Sense;
-	dragWord?: (word: Word) => void;
-	dropWord?: () => void;
-	moveWord?: (word: Word, index: number) => void;
-	addDuplicate?: (word: Word, parent: number) => void;
-	removeDuplicate?: (word: Word, parent: number) => void;
-	draggedWord?: Word;
+  sense: Sense;
+  dragWord?: (word: Word) => void;
+  dropWord?: () => void;
+  moveWord?: (word: Word, index: number) => void;
+  addDuplicate?: (word: Word, parent: number) => void;
+  removeDuplicate?: (word: Word, parent: number) => void;
+  draggedWord?: Word;
 }
 
-export interface StackDisplayState { }
+export interface StackDisplayState {}
 
 class StackDisplay extends React.Component<
-	StackDisplayProps & LocalizeContextProps,
-	StackDisplayState>{
+  StackDisplayProps & LocalizeContextProps,
+  StackDisplayState
+> {
+  dragDrop(event: React.DragEvent<HTMLElement>, index: number) {
+    console.log(index);
+    event.preventDefault();
+    event.stopPropagation();
+    if (this.props.draggedWord) {
+      var word = this.props.draggedWord;
+      // if we are getting our card from external to this list we want to add it and then move it to it's new position
+      if (
+        !this.props.sense.dups.includes(this.props.draggedWord) &&
+        this.props.addDuplicate
+      ) {
+        // Card is coming from somewhere external to this stack
+        // add the card to the stack before reorganizing it
+        this.props.addDuplicate(word, this.props.sense.id);
+        // in this case we also want to remove the word from it's source list
+        // we only want to do this when it is coming from elsewhere not when moving
+        // cards within the list
+        if (this.props.dropWord) this.props.dropWord();
+      }
+      // now we can move dragged word to the requested position
+      if (this.props.moveWord) {
+        this.props.moveWord(word, index);
+      }
+    }
+    this.setState({});
+  }
 
-	dragDrop(event: React.DragEvent<HTMLElement>, index: number) {
-		event.preventDefault();
-		if (this.props.draggedWord) {
-			var word = this.props.draggedWord;
-			// if we are getting our card from external to this list we want to add it and then move it to it's new position
-			if (!this.props.sense.dups.includes(this.props.draggedWord) && this.props.addDuplicate) {
-				// Card is coming from somewhere external to this stack
-				// add the card to the stack before reorganizing it
-				this.props.addDuplicate(word, this.props.sense.id);
-				// in this case we also want to remove the word from it's source list
-				// we only want to do this when it is coming from elsewhere not when moving
-				// cards within the list
-				if (this.props.dropWord) this.props.dropWord();
-			}
-			// now we can move dragged word to the requested position
-			if (this.props.moveWord) {
-				this.props.moveWord(word, index);
-			}
-		}
-		this.setState({});
-	}
+  drag(word: Word) {
+    if (this.props.dragWord) {
+      this.props.dragWord(word);
+    }
+  }
 
-	drag(word: Word) {
-		if (this.props.dragWord) {
-			this.props.dragWord(word);
-		}
-	}
-
-	removeCard(word: Word) {
-		if (this.props.draggedWord && this.props.dropWord) {
-			this.props.dropWord();
-		} else {
-			if (this.props.removeDuplicate) {
-				this.props.removeDuplicate(word, this.props.sense.id);
-			}
-		}
-		this.setState({});
-	}
-	render() {
-		var len = this.props.sense.dups.length;
-		return (
-			<Paper>
-				{this.props.sense.dups.map((word, index) => (
-					<div key={word.id}>
-						<div style={{ height: 20 }}
-							onDragOver={e => e.preventDefault()}
-							onDrop={e => this.dragDrop(e, index)}
-						/>
-						<Card
-							draggable={true}
-							onDragStart={() => this.drag(word)}
-							onDragOver={e => e.preventDefault()}
-							onDrop={e => this.dragDrop(e, index)}
-							onDragEnd={() => this.removeCard(word)}
-
-						>
-							<CardContent>
-								<WordContent word={word} />
-							</CardContent>
-						</Card>
-					</div>
-				))}
-				<div style={{ height: 20 }}
-					onDragOver={e => e.preventDefault()}
-					onDrop={e => this.dragDrop(e, len)}/>
-			</Paper>
-		);
-	}
+  removeCard(word: Word) {
+    if (this.props.draggedWord && this.props.dropWord) {
+      this.props.dropWord();
+    } else {
+      if (this.props.removeDuplicate) {
+        this.props.removeDuplicate(word, this.props.sense.id);
+      }
+    }
+    this.setState({});
+  }
+  render() {
+    var len = this.props.sense.dups.length;
+    return (
+      <Paper>
+        {this.props.sense.dups.map((word, index) => (
+          <div key={word.id}>
+            <div
+              style={{ height: 20 }}
+              onDragOver={e => {
+                e.preventDefault();
+                e.stopPropagation();
+              }}
+              onDrop={e => this.dragDrop(e, index)}
+            />
+            <Card
+              draggable={true}
+              onDragStart={() => this.drag(word)}
+              onDragOver={e => {
+                e.preventDefault();
+                e.stopPropagation();
+              }}
+              onDrop={e => this.dragDrop(e, index)}
+              onDragEnd={() => this.removeCard(word)}
+            >
+              <CardContent>
+                <WordContent word={word} />
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+      </Paper>
+    );
+  }
 }
 
 export default withLocalize(StackDisplay);
--- src/goals/MergeDupGoal/MergeDupStep/reducer.tsx
+++ src/goals/MergeDupGoal/MergeDupStep/reducer.tsx
@@ -1,121 +1,132 @@
 import {
-	MergeTreeAction,
-	ADD_PARENT,
-	ADD_SENSE,
-	ADD_DUPLICATE,
-	REMOVE_DUPLICATE,
-	CLEAR_MERGES,
-	SWAP_DUPLICATE
+  MergeTreeAction,
+  ADD_PARENT,
+  ADD_SENSE,
+  ADD_DUPLICATE,
+  REMOVE_DUPLICATE,
+  CLEAR_MERGES,
+  SWAP_DUPLICATE
 } from "./actions";
 import { ParentWord } from "./component";
 import { Word } from "../../../types/word";
 
 export const defaultState: MergeTreeState = {
-	parentWords: []
+  parentWords: []
 };
 
 export interface MergeTreeState {
-	parentWords: ParentWord[];
+  parentWords: ParentWord[];
 }
 
 //ID assigned to parents as senses to help differentiate between them.
 function generateID(): number {
-	return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
 }
 
 export const mergeDupStepReducer = (
-	state: MergeTreeState = defaultState, //createStore() calls each reducer with undefined state
-	action: MergeTreeAction
+  state: MergeTreeState = defaultState, //createStore() calls each reducer with undefined state
+  action: MergeTreeAction
 ): MergeTreeState => {
-	let parentWords: ParentWord[];
-	switch (action.type) {
-		case SWAP_DUPLICATE:
-			parentWords = state.parentWords;
-			var { word, parent: dest } = action.payload;
-			// find sense containing word
-			parentWords = parentWords.map(parent => {
-				parent.senses = parent.senses.map(sense => {
-					if (sense.dups.includes(word) && dest) {
-						// split array at dest
-						console.log(dest);
-						var ends = [sense.dups.slice(0, dest), sense.dups.slice(dest)];
-						// remove word from both ends;
-						ends  = ends.map(end => end.filter(el => el.id != word.id));
-						console.log(ends);
-						// splice together
-						sense.dups = ends[0].concat(word, ends[1]);
-						console.log(sense.dups);
-					}
-					return sense;
-				});
-				return parent;
-			});
-			return { ...state, parentWords: parentWords };
-		case ADD_PARENT:
-			parentWords = state.parentWords;
-			var word = action.payload.word;
-			parentWords.push({
-				id: generateID(),
-				senses: [{ id: generateID(), dups: [word] }]
-			});
-			return {
-				...state,
-				parentWords
-			};
-		case ADD_SENSE:
-			parentWords = state.parentWords;
-			var { word: merge, parent } = action.payload;
-			if (parent) {
-				parentWords = parentWords.map(item => {
-					if (item.id === parent) {
-						item.senses.push({
-							id: generateID(),
-							dups: [merge]
-						});
-					}
-					return item;
-				});
-			}
-			return {
-				...state,
-				parentWords
-			};
-		case ADD_DUPLICATE:
-			var { word: merge, parent } = action.payload;
-			parentWords = state.parentWords;
-			parentWords = parentWords.map(item => {
-				item.senses = item.senses.map(item => {
-					if (item.id === parent) {
-						item.dups.push(merge);
-					}
-					return item;
-				});
-				return item;
-			});
-			return { ...state, parentWords };
-		case REMOVE_DUPLICATE:
-			var { word: merge, parent: root } = action.payload;
-			parentWords = state.parentWords;
+  let parentWords: ParentWord[];
+  switch (action.type) {
+    case SWAP_DUPLICATE:
+      parentWords = state.parentWords;
+      var { word, parent: dest } = action.payload;
 
-			parentWords = parentWords.map(parent => {
-				parent.senses = parent.senses.map(sense => {
-					if (sense.id === root) {
-						sense.dups = sense.dups.filter(word => word !== merge);
-					}
-					return sense;
-				});
-				parent.senses = parent.senses.filter(sense => sense.dups.length > 0);
-				return parent;
-			});
-			parentWords = parentWords.filter(parent => parent.senses.length > 0);
+      // find sense containing word
+      parentWords = parentWords.map(parent => {
+        parent.senses = parent.senses.map(sense => {
+          if (sense.dups.includes(word)) {
+            dest = dest ? dest : 0;
+            // find location of src word
+            var src = sense.dups.findIndex(el => word.id == el.id);
+            // split array at dest
+            if (src > dest) {
+              var ends = [sense.dups.slice(0, dest), sense.dups.slice(dest)];
+            } else {
+              var ends = [
+                sense.dups.slice(0, dest + 1),
+                sense.dups.slice(dest + 1)
+              ];
+            }
 
-			return { ...state, parentWords };
-		case CLEAR_MERGES:
-			return {
-				...state,
-				parentWords: []
-			};
-		default:
-			return state;
-	}
+            // remove word from both ends;
+            ends = ends.map(end => end.filter(el => el.id != word.id));
+            console.log(ends);
+            // splice together
+            sense.dups = ends[0].concat(word, ends[1]);
+            console.log(sense.dups);
+          }
+          return sense;
+        });
+        return parent;
+      });
+      return { ...state, parentWords: parentWords };
+    case ADD_PARENT:
+      parentWords = state.parentWords;
+      var word = action.payload.word;
+      parentWords.push({
+        id: generateID(),
+        senses: [{ id: generateID(), dups: [word] }]
+      });
+      return {
+        ...state,
+        parentWords
+      };
+    case ADD_SENSE:
+      parentWords = state.parentWords;
+      var { word: merge, parent } = action.payload;
+      if (parent) {
+        parentWords = parentWords.map(item => {
+          if (item.id === parent) {
+            item.senses.push({
+              id: generateID(),
+              dups: [merge]
+            });
+          }
+          return item;
+        });
+      }
+      return {
+        ...state,
+        parentWords
+      };
+    case ADD_DUPLICATE:
+      var { word: merge, parent } = action.payload;
+      parentWords = state.parentWords;
+      parentWords = parentWords.map(item => {
+        item.senses = item.senses.map(item => {
+          if (item.id === parent) {
+            item.dups.push(merge);
+          }
+          return item;
+        });
+        return item;
+      });
+      return { ...state, parentWords };
+    case REMOVE_DUPLICATE:
+      var { word: merge, parent: root } = action.payload;
+      parentWords = state.parentWords;
+
+      parentWords = parentWords.map(parent => {
+        parent.senses = parent.senses.map(sense => {
+          if (sense.id === root) {
+            sense.dups = sense.dups.filter(word => word !== merge);
+          }
+          return sense;
+        });
+        parent.senses = parent.senses.filter(sense => sense.dups.length > 0);
+        return parent;
+      });
+      parentWords = parentWords.filter(parent => parent.senses.length > 0);
+
+      return { ...state, parentWords };
+    case CLEAR_MERGES:
+      return {
+        ...state,
+        parentWords: []
+      };
+    default:
+      return state;
+  }
 };
