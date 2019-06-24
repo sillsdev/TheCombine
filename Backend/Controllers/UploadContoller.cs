d0a785945664c1838713ba18097cb0678e2855e4
Mark Fuller <50843051+SILMarkFuller@users.noreply.github.com>
2019-06-24 12:01:32 -0500

    Backend Audio Upload (#74)
    * BEgin audio file upload api
    
    * Add temporary fontent audio upload component
    
    * Add prototype API route to controller
    
    * Minor edits to audio upload
    
    * Implement Audio Upload to nuk filesystem
    
    * Fix errors in PR
    
    * Fixed build errors from contructor change
    
    * UT failed on lack of directory: added dir
    
    * Generate Words folder if it does not already exist
    
    * Update gitignore with new folder
    
    * Chane '\' to '\'
    
    * Re add Words dir
    
    * Merge master into Backend-Audio-Upload

--- Backend/Controllers/UploadContoller.cs
+++ Backend/Controllers/UploadContoller.cs
@@ -1,4 +1,5 @@
-﻿using BackendFramework.ValueModels;
+﻿using BackendFramework.Interfaces;
+using BackendFramework.ValueModels;
 using Microsoft.AspNetCore.Mvc;
 using SIL.Lift.Parsing;
 using System;
@@ -7,28 +8,36 @@ using System.Threading.Tasks;
 
 namespace BackendFramework.Controllers
 {
+    //[Authorize]
     [Produces("application/json")]
-    [Route("v1/Projects/Words/Upload")]
+    [Route("v1/projects/words")]
     public class UploadContoller : Controller
     {
         public readonly ILexiconMerger<LiftObject, LiftEntry, LiftSense, LiftExample> _merger;
+        public readonly IWordService _wordService;
+        public readonly IWordRepository _wordRepo;
 
-        public UploadContoller(ILexiconMerger<LiftObject, LiftEntry, LiftSense, LiftExample> merger)
+        public UploadContoller(ILexiconMerger<LiftObject, LiftEntry, LiftSense, LiftExample> merger, IWordRepository repo, IWordService service)
         {
             _merger = merger;
+            _wordRepo = repo;
+            _wordService = service;
         }
 
         // POST: v1/Project/Words/upload
         // Implements: Upload(), Arguments: FileUpload model
-        [HttpPost]
-        public async Task<IActionResult> Post([FromForm] FileUpload model)
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadLiftFile([FromForm] FileUpload model)
         {
             var file = model.File;
 
             if (file.Length > 0)
             {
-                model.FilePath = Path.Combine("./uploadFile-" + model.Name + ".xml");
-                using (var fs = new FileStream(model.FilePath, FileMode.Create))
+                string wanted_path = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+                System.IO.Directory.CreateDirectory(wanted_path + "/Words");
+
+                model.FilePath = Path.Combine(wanted_path + "/Words/UploadFile-" + model.Name + ".xml");
+                using (var fs = new FileStream(model.FilePath, FileMode.OpenOrCreate))
                 {
                     await file.CopyToAsync(fs);
                 }
@@ -42,6 +51,29 @@ namespace BackendFramework.Controllers
             {
                 return new UnsupportedMediaTypeResult();
             }
+        }
+
+        [HttpPost("{Id}/upload/audio")]
+        public async Task<IActionResult> UploadAudioFile(string wordId, [FromForm] FileUpload model)
+        {
+            var file = model.File;
+
+            if (file.Length > 0)
+            {
+                model.FilePath = Path.Combine("./Audio/" + wordId + ".mp3");
+                using (var fs = new FileStream(model.FilePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fs);
+                }
+                //add the relative path to the audio field of 
+                Word gotWord = await _wordRepo.GetWord(wordId);
+                gotWord.Audio = model.FilePath;
+                //update the entry
+                _ = await _wordService.Update(wordId, gotWord);
+
+                return new ObjectResult(model.FilePath);
+            }
+            return new UnsupportedMediaTypeResult();
         }
     }
 }
