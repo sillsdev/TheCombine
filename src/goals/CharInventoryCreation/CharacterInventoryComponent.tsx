f0193182f7284f72c8717707619b9a867a365c6d
Simeon <51300614+SimeonSIL@users.noreply.github.com>
2019-06-24 15:20:56 -0500

    Get words in database that don't fit Char inventory (#73)
    * Update component.tsx (#66)
    
    * Add button title text
    
    * Test-Teamcity integration (#62)
    
    * Big Fix for upload files (#70)
    
    * Big Fix for upload files
    
    * Modified samplewords
    
    * Implement getting words in database that don't fit Char inventory
    
    * Make sample words list update when chars added
    
    * Undo previous commit
    
    * Make requested changes
    
    * Add tests
    
    * Change action format
    
    * Change payload name in test
    
    * Add unit tests for multiple and non-latin characters

--- src/goals/CharInventoryCreation/CharacterInventoryComponent.tsx
+++ src/goals/CharInventoryCreation/CharacterInventoryComponent.tsx
@@ -2,17 +2,20 @@ import * as React from "react";
 import {
   LocalizeContextProps,
   withLocalize,
-  Translate
+  Translate,
+  TranslateFunction
 } from "react-localize-redux";
 import CharacterSet from "./components/CharacterSet";
 import { Grid, Button } from "@material-ui/core";
 import { Project } from "../../types/project";
+import SampleWords from "./components/SampleWords";
 
 export interface CharacterInventoryProps {
   setInventory: (inventory: string[]) => void;
   uploadInventory: () => void;
   inventory: string[];
   currentProject: Project;
+  translate: TranslateFunction;
 }
 
 interface CharacterInventoryState {}
@@ -29,38 +32,48 @@ class CharacterInventory extends React.Component<
 
   render() {
     return (
-      <Grid container justify="center" alignItems="center">
-        <Grid item xs={6}>
+      <Grid
+        container
+        justify="center"
+        spacing={2}
+        style={{ background: "#eee" }}
+      >
+        <Grid item sm={6} xs={12}>
           <CharacterSet
             setInventory={inventory => this.props.setInventory(inventory)}
             inventory={this.props.inventory}
           />
         </Grid>
-        <Grid item xs={6}>
-          {/* William, put the other component here */}
+        <Grid item sm={6} xs={12}>
+          <SampleWords
+            setInventory={inventory => this.props.setInventory(inventory)}
+            inventory={this.props.inventory}
+          />
         </Grid>
         <Grid item xs={12}>
           {/* submission buttons */}
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              this.props.uploadInventory();
-            }}
-            style={{ margin: 10 }} // remove when we can add theme
-          >
-            <Translate id="charInventory.save" />
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => {
-              alert("are you sure?"); //obviously this needs to be a dialog
-            }}
-            style={{ margin: 10 }} // remove when we can add theme
-          >
-            {" "}
-            <Translate id="charInventory.cancel" />
-          </Button>
+          <Grid container justify="center">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                this.props.uploadInventory();
+              }}
+              style={{ margin: 10 }} // remove when we can add theme
+            >
+              <Translate id="charInventory.save" />
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                alert("are you sure?"); //obviously this needs to be a dialog
+              }}
+              style={{ margin: 10 }} // remove when we can add theme
+            >
+              {" "}
+              <Translate id="charInventory.cancel" />
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     );
