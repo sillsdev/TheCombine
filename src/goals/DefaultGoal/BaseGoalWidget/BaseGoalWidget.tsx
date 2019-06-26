7348a87cc6c0d259f7dfa047f47d2a027bfe90e5
tyasw <34723161+tyasw@users.noreply.github.com>
2019-06-26 11:04:56 -0500

    Goal widgets (#91)
    * Implement a simple default goal widget
    
    * Update unit test for Goal widget to use constructor for User object
    
    * Rename BaseGoalSelect to BaseGoalWidget

---
+++ src/goals/DefaultGoal/BaseGoalWidget/BaseGoalWidget.tsx
@@ -0,0 +1,41 @@
+import { GoalProps } from "../../../types/goals";
+import React from "react";
+import {
+  LocalizeContextProps,
+  withLocalize,
+  Translate
+} from "react-localize-redux";
+import { Card, CardContent } from "@material-ui/core";
+
+export const WIDTH: number = 200; // Width of each card
+const SCALE_FACTOR_FOR_DESELECTED = 0.9; // The percent of regular size that deselected cards shrink to
+const DESELECTED_WIDTH: number = WIDTH * SCALE_FACTOR_FOR_DESELECTED; // Width of each not-selected card
+
+class BaseGoalSelect extends React.Component<GoalProps & LocalizeContextProps> {
+  constructor(props: GoalProps & LocalizeContextProps) {
+    super(props);
+  }
+
+  readonly style = {
+    inactiveCard: {
+      width: DESELECTED_WIDTH,
+      margin: (WIDTH - DESELECTED_WIDTH) / 2,
+      backgroundColor: "lightGray",
+      color: "gray"
+    }
+  };
+
+  render() {
+    return (
+      <div className={"GoalWidget" + this.props.goal.id}>
+        <Card key={this.props.goal.id} style={this.style.inactiveCard}>
+          <CardContent>
+            <Translate id={"goal.name." + this.props.goal.name} />
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default withLocalize(BaseGoalSelect);
