81ceb6f79b94ed131333be022d3ec09b01336842
sam-skinner <samuelskinner1998@gmail.com>
2019-07-01 11:34:08 -0500

    Fix add Vern Efficency

--- Backend/Services/LiftService.cs
+++ Backend/Services/LiftService.cs
@@ -86,16 +86,27 @@ namespace BackendFramework.Services
                 ";
 
             writer.WriteHeader(header);
-
             var allWords = _repo.GetAllWords().Result;
+            var lang = _projService.GetAllProjects().Result;
+            string getProject;
+            if (lang.Count != 0)
+            {
+                //this next line is temporary until James and the rest of the backend 
+                //can implement multiple Projects per database
+                getProject = lang[0].VernacularWritingSystem;
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
 
             foreach (Word wordEntry in allWords )
             {
                 LexEntry entry = new LexEntry();
 
                 //add vernacular (lexical form)
-                addVern(wordEntry, entry);
-
+                addVern(wordEntry, entry, getProject);
+                
                 //add audio (pronunciation media)
                 addAudio(entry, wordEntry);
 
@@ -108,25 +119,14 @@ namespace BackendFramework.Services
         }
 
         //add vernacular
-        public void addVern(Word wordEntry, LexEntry entry)
+        private void addVern(Word wordEntry, LexEntry entry, string getProject)
         {
-            LiftMultiText lexMultiText = new LiftMultiText();
-            var lang = _projService.GetAllProjects().Result;
-            if (lang.Count != 0)
-            {
-                //this next line is temporary until James and the rest of the backend 
-                //can implement multiple Projects per database
-                var getProject = lang[0].VernacularWritingSystem;
+                LiftMultiText lexMultiText = new LiftMultiText();
                 lexMultiText.Add(getProject, wordEntry.Vernacular);
                 entry.LexicalForm.MergeIn(MultiText.Create(lexMultiText));
-            }
-            else
-            {
-                throw new FileNotFoundException();
-            }
         }
 
-        public void addAudio(LexEntry entry, Word wordEntry)
+        private void addAudio(LexEntry entry, Word wordEntry)
         {
             LexPhonetic lexPhonetic = new LexPhonetic();
             LiftMultiText proMultiText = new LiftMultiText { { "href", wordEntry.Audio } };
@@ -134,7 +134,7 @@ namespace BackendFramework.Services
             entry.Pronunciations.Add(lexPhonetic);
         }
 
-        public void addSense(LexEntry entry, Word wordEntry)
+        private void addSense(LexEntry entry, Word wordEntry)
         {
             for (int i = 0; i < wordEntry.Senses.Count; i++)
             {
