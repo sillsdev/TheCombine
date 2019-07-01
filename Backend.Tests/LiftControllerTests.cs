b58fb23e7cb38b194e0f0eac5f213cb7e5a9b194
SILMarkFuller <mark_fuller@sil.org>
2019-07-01 14:48:53 -0500

    Pull Backend.Tests

--- Backend.Tests/LiftControllerTests.cs
+++ Backend.Tests/LiftControllerTests.cs
@@ -124,7 +124,7 @@ namespace Backend.Tests
         public void TestRoundtrip()
         {
             string wanted_path = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
-            string filepath = Path.Combine(wanted_path, "NewLiftFile.lift");
+            string filepath = wanted_path + "/EXAMPLE.lift";
             File.Delete(filepath);
 
             var fileUpload = InitFile();
@@ -139,7 +139,7 @@ namespace Backend.Tests
             Assert.NotZero(allWords.Result.Count);
 
             //export
-            _ = liftController.ExportLiftFile().Result;
+            _ = liftController.ExportLiftFile(proj.Id).Result;
 
             //assert file was created
             Assert.IsTrue(File.Exists(filepath));
