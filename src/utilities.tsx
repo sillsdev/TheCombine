9e951ed7b3013d61ec99e8ed94303ce782a3c62e
James Pruitt <jamescpruitt@gmail.com>
2019-07-01 11:23:05 -0500

    Add UUID generator

---
+++ src/utilities.tsx
@@ -0,0 +1,7 @@
+export function uuid(): string {
+  let bytes = (count: number) => Math.floor(
+    (1 + Math.random()) * (2 ** (count * 8 + 4)))
+    .toString(16)
+    .substring(1);
+  return bytes(4) + '-' + bytes(2) + '-' + bytes(2) + '-' + bytes(2) + '-' + bytes(6);
+}
