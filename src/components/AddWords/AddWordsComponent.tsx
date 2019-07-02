eac26e3f4401c9a49b4e4109e32720bda2e6092f
SimeonSIL <simeon_eberz@sil.org>
2019-07-02 18:01:28 -0500

    Add list

--- src/components/AddWords/AddWordsComponent.tsx
+++ src/components/AddWords/AddWordsComponent.tsx
@@ -4,25 +4,87 @@ import {
   Typography,
   Container,
   TextField,
-  Grid
+  Grid,
+  Divider,
+  Button
 } from "@material-ui/core";
-import { ReactReduxContext } from "react-redux";
 import theme from "../../types/theme";
 import { LocalizeContextProps } from "react-localize-redux";
+import { Word, State } from "../../types/word";
 
 interface AddWordsProps {
   domain: string;
 }
 
-interface AddWordsState {}
+interface AddWordsState {
+  words: Word[];
+  editing?: number;
+  newVern: string;
+  newGloss: string;
+}
 
 export default class AddWords extends React.Component<
   AddWordsProps & LocalizeContextProps,
   AddWordsState
 > {
+  constructor(props: AddWordsProps & LocalizeContextProps) {
+    super(props);
+    this.state = { words: [], newVern: "", newGloss: "" };
+  }
+
+  submit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const vernacular = this.state.newVern;
+    const gloss = this.state.newGloss;
+
+    if (vernacular === "") return;
+
+    let word: Word = {
+      id: "",
+      vernacular: vernacular,
+      senses: [
+        {
+          glosses: [
+            {
+              language: "",
+              def: gloss
+            }
+          ],
+          semanticDomains: []
+        }
+      ],
+      audio: "",
+      created: "",
+      modified: "",
+      history: [],
+      partOfSpeech: "",
+      editedBy: [],
+      accessability: State.active,
+      otherField: "",
+      plural: ""
+    };
+    let words = JSON.parse(JSON.stringify(this.state.words));
+    words.push(word);
+    this.setState({ words, newVern: "", newGloss: "" });
+  }
+
+  updateField<K extends keyof AddWordsState>(
+    e: React.ChangeEvent<
+      HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+    >,
+    field: K
+  ) {
+    const value = e.target.value;
+
+    this.setState({
+      [field]: value
+    } as Pick<AddWordsState, K>);
+  }
+
   render() {
     let rows = [];
-    for (let i = 0; i < 60; i++) {
+    for (const word of this.state.words) {
       rows.push(
         <React.Fragment>
           <Grid
@@ -33,7 +95,7 @@ export default class AddWords extends React.Component<
               paddingRight: theme.spacing(2)
             }}
           >
-            <TextField fullWidth />
+            {word.vernacular}
           </Grid>
         </React.Fragment>
       );
@@ -41,7 +103,14 @@ export default class AddWords extends React.Component<
 
     return (
       <Container>
-        <Paper style={{ padding: theme.spacing(2) }}>
+        <Paper
+          style={{
+            padding: theme.spacing(2),
+            width: 600,
+            marginLeft: "auto",
+            marginRight: "auto"
+          }}
+        >
           <Typography
             variant="h4"
             align="center"
@@ -49,18 +118,106 @@ export default class AddWords extends React.Component<
           >
             Domain: Sky
           </Typography>
-          <Grid container spacing={1}>
-            <Grid item xs={6}>
-              <Typography variant="h5" align="center">
-                Vernacular
-              </Typography>
+          <Divider />
+          <form onSubmit={e => this.submit(e)}>
+            <input type="submit" style={{ display: "none" }} />
+            <Grid container spacing={1}>
+              <Grid item xs={6}>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  style={{ marginTop: theme.spacing(2) }}
+                >
+                  Vernacular
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  style={{ marginTop: theme.spacing(2) }}
+                >
+                  Gloss
+                </Typography>
+              </Grid>
+              {this.state.words.map((word, index) => (
+                <React.Fragment>
+                  <Grid
+                    item
+                    xs={6}
+                    style={{
+                      paddingLeft: theme.spacing(3),
+                      paddingRight: theme.spacing(2)
+                    }}
+                  >
+                    <Typography variant="body1">{word.vernacular}</Typography>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={6}
+                    style={{
+                      paddingLeft: theme.spacing(2),
+                      paddingRight: theme.spacing(2)
+                    }}
+                  >
+                    <Typography variant="body1">
+                      {word.senses[0].glosses[0].def}
+                    </Typography>
+                  </Grid>
+                </React.Fragment>
+              ))}
+              <React.Fragment>
+                <Grid
+                  item
+                  xs={6}
+                  style={{
+                    paddingLeft: theme.spacing(2),
+                    paddingRight: theme.spacing(2)
+                  }}
+                >
+                  <TextField
+                    autoFocus
+                    label={"Vernacular"}
+                    fullWidth
+                    variant="outlined"
+                    value={this.state.newVern}
+                    onChange={e => {
+                      this.updateField(e, "newVern");
+                    }}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                  style={{
+                    paddingLeft: theme.spacing(2),
+                    paddingRight: theme.spacing(2)
+                  }}
+                >
+                  <TextField
+                    label={"Gloss"}
+                    fullWidth
+                    variant="outlined"
+                    value={this.state.newGloss}
+                    onChange={e => {
+                      this.updateField(e, "newGloss");
+                    }}
+                  />
+                </Grid>
+              </React.Fragment>
             </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h5" align="center">
-                Gloss
-              </Typography>
+          </form>
+          <Grid container justify="flex-end" spacing={2}>
+            <Grid item>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                style={{ marginTop: theme.spacing(2) }}
+              >
+                Next
+              </Button>
             </Grid>
-            {rows}
           </Grid>
         </Paper>
       </Container>
