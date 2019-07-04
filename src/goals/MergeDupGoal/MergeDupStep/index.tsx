70b22232c9b9250d96332d9b137162159d176c52
James Pruitt <jamescpruitt@gmail.com>
2019-07-04 11:30:07 -0500

    Merge Dups reorg cleanup

--- src/goals/MergeDupGoal/MergeDupStep/index.tsx
+++ src/goals/MergeDupGoal/MergeDupStep/index.tsx
@@ -1,24 +1,28 @@
-import {connect} from "react-redux";
-import {ThunkDispatch} from "redux-thunk";
-import {dropWord, WordDragAction} from "../../../components/DraggableWord/actions";
-import {StoreState} from "../../../types";
-import {MergeTreeAction, refreshWords, moveSense, mergeAll} from "./actions";
-import MergeDupStepComponent from "./component";
-import {MergeTreeReference} from './MergeDupsTree';
+import { connect } from "react-redux";
+import { ThunkDispatch } from "redux-thunk";
+import {
+  dropWord,
+  WordDragAction
+} from "../../../components/DraggableWord/actions";
+import { StoreState } from "../../../types";
+import {
+  MergeTreeAction,
+  refreshWords,
+  moveSense,
+  mergeAll
+} from "./MergeDupStepActions";
+import MergeDupStepComponent from "./MergeDupStepComponent";
+import { MergeTreeReference } from "./MergeDupsTree";
 
 export function mapStateToProps(state: StoreState) {
   return {
     draggedSense: state.mergeDuplicateGoal.wordDragState.draggedWord,
-    words: state.mergeDuplicateGoal.mergeTreeState.tree.words,
+    words: state.mergeDuplicateGoal.mergeTreeState.tree.words
   };
 }
 
 export function mapDispatchToProps(
-  dispatch: ThunkDispatch<
-    StoreState,
-    any,
-    MergeTreeAction | WordDragAction 
-  >
+  dispatch: ThunkDispatch<StoreState, any, MergeTreeAction | WordDragAction>
 ) {
   return {
     dropWord: () => {
