3311c8bc7f9d6fce86bd260ce1c13b86ecea4d85
James Pruitt <jamescpruitt@gmail.com>
2019-07-08 16:57:46 -0500

    Made backend wrapper string template format consistent

--- src/backend/index.tsx
+++ src/backend/index.tsx
@@ -10,13 +10,13 @@ const backendServer = axios.create({ baseURL: "https://localhost:5001/v1" });
 
 let projectId: string = "";
 
-export function setProjectID(id: string){
+export function setProjectID(id: string) {
   projectId = id;
 }
 
 export async function createWord(word: Word): Promise<Word> {
   return await backendServer
-    .post("projects/" + projectId + "/words", word)
+    .post(`projects/"${projectId}/words`, word)
     .then(resp => {
       return { ...word, id: resp.data };
     });
@@ -24,13 +24,13 @@ export async function createWord(word: Word): Promise<Word> {
 
 export async function getWord(id: string): Promise<Word> {
   return await backendServer
-    .get("projects/" + projectId + "/words/" + id)
+    .get(`projects/${projectId}/words/${id}`)
     .then(resp => resp.data);
 }
 
 export async function getAllWords(): Promise<Word[]> {
   return await backendServer
-    .get("projects/" + projectId + "/words")
+    .get(`projects/${projectId}/words`)
     .then(resp => resp.data);
 }
 
@@ -48,13 +48,13 @@ export async function mergeWords(
     Time: Date.now().toString()
   };
   return await backendServer
-    .put("projects/" + projectId + "/words", merge)
+    .put(`projects/${projectId}/words`, merge)
     .then(resp => resp.data);
 }
 
 export async function updateWord(word: Word): Promise<Word> {
   return await backendServer
-    .put("projects/" + projectId + "/words/" + word.id, word)
+    .put(`projects/${projectId}/words/${word.id}`, word)
     .then(resp => {
       return { ...word, id: resp.data };
     });
@@ -62,7 +62,7 @@ export async function updateWord(word: Word): Promise<Word> {
 
 export async function deleteWord(word: Word): Promise<Word> {
   return await backendServer
-    .delete("projects/" + projectId + "/words/" + word.id)
+    .delete(`projects/${projectId}/words/${word.id}`)
     .then(resp => {
       return { ...word, id: resp.data };
     });
@@ -70,13 +70,13 @@ export async function deleteWord(word: Word): Promise<Word> {
 
 export async function getFrontierWords(): Promise<Word[]> {
   return await backendServer
-    .get("projects/" + projectId + "/words/frontier")
+    .get(`projects/${projectId}/words/frontier`)
     .then(resp => resp.data);
 }
 
 export async function addUser(user: User): Promise<User> {
   return await backendServer
-    .post("users", user, { headers: authHeader() })
+    .post(`users`, user, { headers: authHeader() })
     .then(resp => {
       return { ...user, id: resp.data };
     });
@@ -88,7 +88,7 @@ export async function authenticateUser(
 ): Promise<string> {
   return await backendServer
     .post(
-      "users/authenticate",
+      `users/authenticate`,
       { Username: username, Password: password },
       { headers: authHeader() }
     )
@@ -96,43 +96,43 @@ export async function authenticateUser(
 }
 
 export async function getAllUsers(): Promise<User[]> {
-  return await backendServer.get("users").then(resp => resp.data);
+  return await backendServer.get(`users`).then(resp => resp.data);
 }
 
 export async function getUser(id: string): Promise<User> {
-  return await backendServer.get("users/" + id).then(resp => resp.data);
+  return await backendServer.get(`users/${id}`).then(resp => resp.data);
 }
 
 export async function updateUser(user: User): Promise<User> {
-  return await backendServer.put("users/" + user.id, user).then(resp => {
+  return await backendServer.put(`users/${user.id}`, user).then(resp => {
     return { ...user, id: resp.data };
   });
 }
 
 export async function createProject(project: Project): Promise<Project> {
   return await backendServer
-    .post("projects/", project, { headers: authHeader() })
+    .post(`projects/`, project, { headers: authHeader() })
     .then(resp => {
       return { ...project, id: resp.data };
     });
 }
 
 export async function getAllProjects(): Promise<Project[]> {
-  return await backendServer.get("projects").then(resp => resp.data);
+  return await backendServer.get(`projects`).then(resp => resp.data);
 }
 
 export async function getProject(id: string): Promise<Project> {
-  return await backendServer.get("projects/" + id).then(resp => resp.data);
+  return await backendServer.get(`projects/"${id}`).then(resp => resp.data);
 }
 
 export async function updateProject(project: Project) {
-  await backendServer.put("projects/" + project.id, project);
+  await backendServer.put(`projects/"${project.id}`, project);
 }
 
 export async function uploadLift(project: Project, lift: File) {
   let data = new FormData();
   data.append("file", lift);
-  await backendServer.post("projects/" + projectId + "/words/upload", data, {
+  await backendServer.post(`projects/${projectId}/words/upload`, data, {
     headers: { ...authHeader(), "Content-Type": "multipart/form-data" }
   });
 }
@@ -140,13 +140,9 @@ export async function uploadLift(project: Project, lift: File) {
 export async function uploadMp3(project: Project, mp3: File) {
   let data = new FormData();
   data.append("file", mp3);
-  await backendServer.post(
-    "projects/" + projectId + "/words/upload/audio",
-    data,
-    {
-      headers: { ...authHeader(), "content-type": "application/json" }
-    }
-  );
+  await backendServer.post(`projects/${projectId}/words/upload/audio`, data, {
+    headers: { ...authHeader(), "content-type": "application/json" }
+  });
 }
 
 export async function addGoalToUserEdit(
@@ -210,7 +206,7 @@ export async function getUserEditById(index: string): Promise<UserEdit> {
 
 export async function getAllUserEdits(): Promise<Goal[]> {
   return await backendServer
-    .get("projects/" + projectId + "/useredits")
+    .get(`projects/${projectId}/useredits`)
     .then(resp => {
       return resp.data;
     });
