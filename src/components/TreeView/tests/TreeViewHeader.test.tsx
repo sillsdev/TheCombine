9e232ed44b967cb9e4d9badb96b5ac8d574696a3
Micah-Schiewe <51131332+Micah-Schiewe@users.noreply.github.com>
2019-07-12 11:13:35 -0500

    Add a semantic domain tree view (#160)
    * Add a semantic domain tree view

---
+++ src/components/TreeView/tests/TreeViewHeader.test.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import renderer, { ReactTestRenderer } from "react-test-renderer";
+import SemanticDomain from "../SemanticDomain";
+import TreeViewHeader from "../TreeViewHeader";
+import MockTree from "./MockSemanticTree";
+
+// Variable event
+var event = {
+  bubbles: false,
+  key: "Enter",
+  preventDefault: jest.fn(),
+  target: {
+    value: ""
+  }
+};
+
+// Handles
+var treeMaster: ReactTestRenderer;
+var treeHandle: TreeViewHeader;
+const MOCK_ANIMATE = jest.fn();
+
+// Mock out window
+const ADD_LISTENER = window.addEventListener.prototype;
+const REMOVE_LISTENER = window.removeEventListener.prototype;
+window = {
+  ...window,
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn()
+};
+
+beforeEach(() => {
+  setTree(MockTree.subDomains[1]);
+  MOCK_ANIMATE.mockClear();
+});
+
+afterAll(() => {
+  window = {
+    ...window,
+    addEventListener: ADD_LISTENER,
+    removeEventListener: REMOVE_LISTENER
+  };
+});
+
+describe("Tests TreeViewHeader", () => {
+  it("Renders correctly", () => {
+    // Default snapshot test
+    snapTest("default view");
+  });
+
+  // onKeyDown
+  it("Search & select domain switches semantic domain if given number found", () => {
+    event.target.value = MockTree.number;
+    treeHandle.searchAndSelectDomain((event as any) as React.KeyboardEvent);
+    expect(MOCK_ANIMATE).toHaveBeenCalledWith(MockTree);
+  });
+
+  it("Search & select domain does not switch semantic domain if given number not found", () => {
+    event.target.value = "NO_NUMBER";
+    treeHandle.searchAndSelectDomain((event as any) as React.KeyboardEvent);
+    expect(MOCK_ANIMATE).toHaveBeenCalledTimes(0);
+  });
+
+  // getBrotherDomain
+  it("provides the proper brother domains", () => {
+    // Standard navigation
+    expect(treeHandle.getBrotherDomain(-1)).toEqual(MockTree.subDomains[0]);
+    expect(treeHandle.getBrotherDomain(1)).toEqual(MockTree.subDomains[2]);
+
+    // Check with indices out-of-bounds
+    expect(treeHandle.getBrotherDomain(-2)).toEqual(undefined);
+    expect(treeHandle.getBrotherDomain(2)).toEqual(undefined);
+
+    // Check that a domain w/ no parentDomain has no brotherDomains
+    setTree(MockTree);
+    expect(treeHandle.getBrotherDomain(-1)).toEqual(undefined);
+  });
+
+  // navigateKeys
+  it("navigateKeys w/ Arrow Down switches to parent domain", () => {
+    event.key = "ArrowDown";
+    treeHandle.navigateDomainArrowKeys((event as any) as KeyboardEvent);
+    expect(MOCK_ANIMATE).toHaveBeenCalledWith(MockTree);
+  });
+
+  it("navigateKeys w/ Arrow Left switches to left brother domain", () => {
+    event.key = "ArrowLeft";
+    treeHandle.navigateDomainArrowKeys((event as any) as KeyboardEvent);
+    expect(MOCK_ANIMATE).toHaveBeenCalledWith(MockTree.subDomains[0]);
+  });
+
+  it("navigateKeys w/ Arrow Right switches to right brother domain", () => {
+    event.key = "ArrowRight";
+    treeHandle.navigateDomainArrowKeys((event as any) as KeyboardEvent);
+    expect(MOCK_ANIMATE).toHaveBeenCalledWith(MockTree.subDomains[2]);
+  });
+});
+
+// Creates the tree
+function setTree(domain: SemanticDomain) {
+  renderer.act(() => {
+    treeMaster = renderer.create(
+      <TreeViewHeader currentDomain={domain} animate={MOCK_ANIMATE} />
+    );
+  });
+  treeHandle = treeMaster.root.findByType(TreeViewHeader).instance;
+}
+
+// Perform a snapshot test
+function snapTest(name: string) {
+  expect(treeMaster.toJSON()).toMatchSnapshot();
+}
