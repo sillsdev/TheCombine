e60aa89574bbe18fb2ee06ab3facf73588011f45
James Pruitt <jamescpruitt@gmail.com>
2019-07-12 16:57:57 -0500

    Add redux devtools (#165)
    * Add redux devtools

--- src/store.tsx
+++ src/store.tsx
@@ -1,6 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import { rootReducer } from "./rootReducer";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
