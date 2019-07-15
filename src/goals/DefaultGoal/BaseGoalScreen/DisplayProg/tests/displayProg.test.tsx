0f25419d59bbd689ceb245a0b9c2292e4f4f89a3
Micah-Schiewe <51131332+Micah-Schiewe@users.noreply.github.com>
2019-07-15 14:39:29 -0500

    Step progress indicator (#168)
    * Add a base goal step progress indicator
    
    * Modifies reducers to reduce store mutation and allow progress indicator to update
    
    * Add base goal progress indicator
    
    * Lowers store state mutation in reducers to allow progress indicator to progress naturally

--- src/goals/DefaultGoal/BaseGoalScreen/DisplayProg/tests/displayProg.test.tsx
+++ src/goals/DefaultGoal/BaseGoalScreen/DisplayProg/tests/displayProg.test.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { User } from "../../../../../types/user";
-import { Goal } from "../../../../../types/goals";
 import DisplayProg from "../displayProg";
-import { CreateCharInv } from "../../../../CreateCharInv/CreateCharInv";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  let user: User = { name: "TestUser", username: "TestUsername", id: 0 };
-  let goal: Goal = new CreateCharInv();
-  goal.user = user;
-  ReactDOM.render(<DisplayProg goal={goal} />, div);
+  ReactDOM.render(<DisplayProg currentStep={1} numSteps={2} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
