6fe91e29a5d94661510626cd253bb5cd5f381b3d
William Tyas <wtyas@hotmail.com>
2019-07-15 13:13:56 -0500

    Remove unused mock data

--- src/types/mockAction.tsx
+++ src/types/mockAction.tsx
@@ -1,7 +1,6 @@
 import { Action } from "redux";
 import { Goal } from "./goals";
 import { CreateCharInv } from "../goals/CreateCharInv/CreateCharInv";
-import { MergeDups } from "../goals/MergeDupGoal/MergeDups";
 
 export interface ActionWithPayload<T> extends Action {
   payload: T;
@@ -15,21 +14,10 @@ interface MockAction extends ActionWithPayload<Goal> {
   payload: Goal;
 }
 
-interface MockActionGoalArray extends ActionWithPayload<Goal[]> {
-  type: MOCK_TYPE;
-  payload: Goal[];
-}
-
 const goal: Goal = new CreateCharInv();
-const goalArray: Goal[] = [new CreateCharInv(), new MergeDups()];
 
 // Used for passing a non-existent action to a reducer
 export const MockActionInstance: MockAction = {
   type: MOCK_TYPE,
   payload: goal
-};
-
-export const MockActionGoalArrayInstance: MockActionGoalArray = {
-  type: MOCK_TYPE,
-  payload: goalArray
 };
