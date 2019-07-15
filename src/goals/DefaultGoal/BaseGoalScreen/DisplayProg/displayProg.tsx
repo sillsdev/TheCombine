46f693fac5d32414777735b6b1c0e1797e92baff
Micah-Schiewe <51131332+Micah-Schiewe@users.noreply.github.com>
2019-07-15 14:39:29 -0500

    Step progress indicator (#168)
    * Add a base goal step progress indicator
    
    * Modifies reducers to reduce store mutation and allow progress indicator to update
    
    * Add base goal progress indicator
    
    * Lowers store state mutation in reducers to allow progress indicator to progress naturally

--- src/goals/DefaultGoal/BaseGoalScreen/DisplayProg/displayProg.tsx
+++ src/goals/DefaultGoal/BaseGoalScreen/DisplayProg/displayProg.tsx
@@ -1,12 +1,48 @@
-import { GoalProps } from "../../../../types/goals";
 import React from "react";
-import { LocalizeContextProps, withLocalize } from "react-localize-redux";
+import {
+  LocalizeContextProps,
+  withLocalize,
+  Translate
+} from "react-localize-redux";
+import { LinearProgress, Paper, Grid, Typography } from "@material-ui/core";
+
+interface ProgressProps {
+  currentStep: number;
+  numSteps: number;
+}
 
 export class DisplayProg extends React.Component<
-  GoalProps & LocalizeContextProps
+  ProgressProps & LocalizeContextProps
 > {
   render() {
-    return <div />;
+    return this.props.numSteps > 1 ? (
+      <Paper key={this.props.currentStep}>
+        <Grid container direction="column">
+          <Grid item xs>
+            <Typography variant={"h4"}>
+              <Translate id="goal.progress.step" />
+              {` ${this.props.currentStep} `}
+              <Translate id="goal.progress.of" />
+              {` ${this.props.numSteps}`}
+            </Typography>
+          </Grid>
+          <Grid item xs>
+            <LinearProgress
+              variant="determinate"
+              value={this.getAmountComplete()}
+            />
+          </Grid>
+        </Grid>
+      </Paper>
+    ) : null;
+  }
+
+  getAmountComplete(): number {
+    return (this.props.currentStep / this.props.numSteps) * 100;
+  }
+
+  getHeaderString(): string {
+    return `${this.props.currentStep} / ${this.props.numSteps}`;
   }
 }
 
