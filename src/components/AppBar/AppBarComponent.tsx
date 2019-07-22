343e51225b20bf43ac7f29390791cbf05ca1ae50
James Pruitt <jamescpruitt@gmail.com>
2019-07-22 09:36:22 -0500

    Improve sidebar (#187)

--- src/components/AppBar/AppBarComponent.tsx
+++ src/components/AppBar/AppBarComponent.tsx
@@ -5,13 +5,14 @@ import { withLocalize, LocalizeContextProps } from "react-localize-redux";
 import { Grid } from "@material-ui/core";
 import { LogoutButton } from "../Login/LogoutButton";
 import { GoToHomeButton } from "../GoToHomeButton/GoToHomeButtonComponent";
+import theme from "../../types/theme";
 
 /** An app bar shown at the top of almost every page of The Combine */
 export class AppBarComponent extends React.Component<LocalizeContextProps> {
   render() {
     return (
-      <div className="NavigationBar">
-        <AppBar position="static">
+      <div className="NavigationBar" style={{marginBottom: 64}}>
+        <AppBar position="fixed" style={{ zIndex: theme.zIndex.drawer + 1 }}>
           <Toolbar>
             <Grid
               container
