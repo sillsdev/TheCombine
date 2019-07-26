9e72633c33cf968c3f1094b8256a8720f1efb945
sam-skinner <46290787+sam-skinner@users.noreply.github.com>
2019-07-26 09:47:44 -0500

    Fix DataEntry Duplicate Finder & Doc Comments (#199)
    * Add more doc comments to controllers and runtime files
    
    * Refactor WordIsUnique and more doc comments

--- Backend/Services/UserEditApiServices.cs
+++ Backend/Services/UserEditApiServices.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace BackendFramework.Services
 {
+    /// <summary> More complex functions and application logic for <see cref="UserEdit"/>s </summary>
     public class UserEditService : IUserEditService
     {
         private readonly IUserEditRepository _repo;
@@ -14,34 +15,36 @@ namespace BackendFramework.Services
             _repo = repo;
         }
 
+        /// <summary> Adds an <see cref="Edit"/> to a specified <see cref="UserEdit"/> </summary>
+        /// <returns> Tuple of a bool: success of operation and int: index at which the new edit was placed </returns>
         public async Task<Tuple<bool, int>> AddGoalToUserEdit(string projectId, string userEditId, Edit edit)
         {
             //get userEdit to change
             var userEntry = await _repo.GetUserEdit(projectId, userEditId);
-
             UserEdit newUserEdit = userEntry.Clone();
 
             //add the new goal index to Edits list
             newUserEdit.Edits.Add(edit);
 
-            //replace the old UserEdit object with the new one that contains  the new list entryz
-            bool validation = _repo.Replace(projectId, userEditId, newUserEdit).Result;
+            //replace the old UserEdit object with the new one that contains the new list entry
+            bool replaceSucceeded = _repo.Replace(projectId, userEditId, newUserEdit).Result;
 
             int indexOfNewestEdit = -1;
 
-            if (validation)
+            if (replaceSucceeded)
             {
                 var newestEdit = _repo.GetUserEdit(projectId, userEditId).Result;
-                indexOfNewestEdit = newestEdit.Edits.Count -1;
+                indexOfNewestEdit = newestEdit.Edits.Count - 1;
             }
 
-            return new Tuple<bool, int>(validation, indexOfNewestEdit);
+            return new Tuple<bool, int>(replaceSucceeded, indexOfNewestEdit);
         }
 
+        /// <summary> Adds a string representation of a step to a <see cref="Edit"/> at a specified index </summary>
+        /// <returns> A bool: success of operation </returns>
         public async Task<bool> AddStepToGoal(string projectId, string userEditId, int goalIndex, string userEdit)
         {
             UserEdit addUserEdit = await _repo.GetUserEdit(projectId, userEditId);
-
             UserEdit newUserEdit = addUserEdit.Clone();
 
             newUserEdit.Edits[goalIndex].StepData.Add(userEdit);
--- Backend/Services/UserRoleApiServices.cs
+++ Backend/Services/UserRoleApiServices.cs
@@ -1,12 +1,12 @@
 using BackendFramework.Interfaces;
 using BackendFramework.ValueModels;
 using MongoDB.Driver;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BackendFramework.Services
 {
+    /// <summary> Atomic database functions for <see cref="UserRole"/>s </summary>
     public class UserRoleService : IUserRoleService
     {
         private readonly IUserRoleContext _userRoleDatabase;
@@ -16,11 +16,14 @@ namespace BackendFramework.Services
             _userRoleDatabase = collectionSettings;
         }
 
+        /// <summary> Finds all <see cref="UserRole"/>s with specified projectId </summary>
         public async Task<List<UserRole>> GetAllUserRoles(string projectId)
         {
             return await _userRoleDatabase.UserRoles.Find(u => u.ProjectId == projectId).ToListAsync();
         }
 
+        /// <summary> Removes all <see cref="UserRole"/>s for specified <see cref="Project"/> </summary>
+        /// <returns> A bool: success of operation </returns>
         public async Task<bool> DeleteAllUserRoles(string projectId)
         {
             var deleted = await _userRoleDatabase.UserRoles.DeleteManyAsync(u => u.ProjectId == projectId);
@@ -31,6 +34,7 @@ namespace BackendFramework.Services
             return false;
         }
 
+        /// <summary> Finds <see cref="UserRole"/> with specified userRoleId and projectId </summary>
         public async Task<UserRole> GetUserRole(string projectId, string userRoleId)
         {
             var filterDef = new FilterDefinitionBuilder<UserRole>();
@@ -41,12 +45,16 @@ namespace BackendFramework.Services
             return userRoleList.FirstOrDefault();
         }
 
+        /// <summary> Adds a <see cref="UserRole"/> </summary>
+        /// <returns> The UserRole created </returns>
         public async Task<UserRole> Create(UserRole userRole)
         {
             await _userRoleDatabase.UserRoles.InsertOneAsync(userRole);
             return userRole;
         }
 
+        /// <summary> Removes <see cref="UserRole"/> with specified userRoleId and projectId </summary>
+        /// <returns> A bool: success of operation </returns>
         public async Task<bool> Delete(string projectId, string userRoleId)
         {
             var filterDef = new FilterDefinitionBuilder<UserRole>();
@@ -56,6 +64,8 @@ namespace BackendFramework.Services
             return deleted.DeletedCount > 0;
         }
 
+        /// <summary> Updates permissions of <see cref="UserRole"/> with specified userRoleId </summary>
+        /// <returns> A <see cref="ResultOfUpdate"/> enum: success of operation </returns>
         public async Task<ResultOfUpdate> Update(string userRoleId, UserRole userRole)
         {
             FilterDefinition<UserRole> filter = Builders<UserRole>.Filter.Eq(x => x.Id, userRoleId);
