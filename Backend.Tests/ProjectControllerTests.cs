857f1d261d8822ca159f503ef18cfa93588700bd
sam-skinner <samuelskinner1998@gmail.com>
2019-07-28 17:37:12 -0500

    Return user with CreateProject

--- Backend.Tests/ProjectControllerTests.cs
+++ Backend.Tests/ProjectControllerTests.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using System.Collections.Generic;
+using static BackendFramework.Controllers.ProjectController;
 
 namespace Backend.Tests
 {
@@ -96,7 +97,7 @@ namespace Backend.Tests
         public void TestCreateProject()
         {
             Project project = RandomProject();
-            string id = (_controller.Post(project).Result as ObjectResult).Value as string;
+            string id = ((_controller.Post(project).Result as ObjectResult).Value as CreateProjectReturnType).Project.Id;
             project.Id = id;
             Assert.Contains(project, _projectService.GetAllProjects().Result);
         }
