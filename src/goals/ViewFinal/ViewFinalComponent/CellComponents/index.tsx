2ddbb3fd0fa18deabcf88f909859d9707951adc5
Micah-Schiewe <51131332+Micah-Schiewe@users.noreply.github.com>
2019-07-29 14:45:38 -0500

    Add View Final goal (#195)
    * Create view final goal
    
    * Includes basic sense, gloss, and semantic domain addition. Does not yet support deletion
    
    * Upgrade ViewFinalGoal
    
    * Updates input of glosses and semantic domains to enforce both being present on a sense
    * Makes the user go through the Semantic Domain Tree View when inputting domains
    
    * Ascend View Final state to store
    
    * Upgrade ViewFinalComponent
    
    * Add search by gloss and domain
    * Remove storing of entire frontier in store
    
    * Start switching edit methodology
    
    * Halfway point, partway between original inline editing and edit button.
    
    * Finalize 1.0 of View Final Goal
    
    * Allows addition/editing/removal of semantic domains, glosses, and senses
    * Can search, sort, and filter data
    
    * Fix bugs in ViewFinal
    
    * Adds a handler for no vernacular
    * Repairs handling no glosses/domains
    * Moves word data cleanup to ViewFinalAction, so that bad data cannot be fed to the database
    * Reverse accidental commit of App/component.tsx
    
    * Connect ViewFinal to Goal Selector
    
    * Implement code review suggestions
    
    * Reduces row height further
    * Clarifies certain files
    * Fixes some bugs found during these modifications
    * Repair App/component mixup
    * Convert action types to string enums
    * Upsizes number of columns that can be displayed.
    * Bugfix: deleting all glosses results in a gloss with no def being saved
    
    * Implement further code review suggestions
    
    * Fix selection of keys for various React elements
    * Remove payload holder for actions to improve code readability
    * Remove various artifacts from when edits were all pushed at once
    
    * Implement code review suggestions
    
    * Fix the fix of the React keys
    * Upgrade: block the user from submitting data which is irrepairably bad, and give an error
    
    * Add unit tests

---
+++ src/goals/ViewFinal/ViewFinalComponent/CellComponents/index.tsx
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+
+import DomainCell from "./DomainCell";
+import { StoreState } from "../../../../types";
+
+function mapStateToProps(state: StoreState) {
+  return {
+    selectedDomain: state.treeViewState.currentdomain
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(DomainCell);
