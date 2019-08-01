5e51f7fa0e6b2a5be833402ec645015a44f4d7e7
SimeonSIL <simeon_eberz@sil.org>
2019-08-01 15:43:54 -0500

    Remove unused imports

--- src/components/AppBar/AppBarComponent.tsx
+++ src/components/AppBar/AppBarComponent.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import { withLocalize, LocalizeContextProps } from "react-localize-redux";
-import { Grid, Container, Box } from "@material-ui/core";
+import { withLocalize } from "react-localize-redux";
+import { Grid } from "@material-ui/core";
 import UserMenu from "./UserMenu";
 import GoToHomeButton from "../GoToHomeButton/GoToHomeButtonComponent";
 import theme from "../../types/theme";
 
 /** An app bar shown at the top of almost every page of The Combine */
-export function AppBarComponent(props: LocalizeContextProps) {
+export function AppBarComponent() {
   return (
     <React.Fragment>
       <div className="NavigationBar" style={{ marginBottom: theme.spacing(6) }}>
