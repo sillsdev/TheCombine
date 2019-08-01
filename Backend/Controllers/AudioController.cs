c3e057496d8a221311f69d0efcc21990345f1b30
SILMarkFuller <mark_fuller@sil.org>
2019-08-01 08:33:51 -0500

    Fix scope of controller

--- Backend/Controllers/AudioController.cs
+++ Backend/Controllers/AudioController.cs
@@ -42,15 +42,16 @@ namespace BackendFramework.Controllers
             }
 
             var filePath = _wordService.GetAudioFilePath(projectId, wordId);
-            
-            if(filePath == null)
+
+            if (filePath == null)
             {
                 return new BadRequestObjectResult("There was more than one subDir of the extracted zip");
             }
 
             Stream stream = System.IO.File.OpenRead(filePath);
 
-            if (stream == null){
+            if (stream == null)
+            {
                 return new BadRequestObjectResult("The file does not exist");
             }
 
@@ -58,45 +59,45 @@ namespace BackendFramework.Controllers
         }
 
 
+
+
+
+        /// <summary> Adds a pronunciation <see cref="FileUpload"/> to a <see cref="Word"/> and saves locally to ~/.CombineFiles/{ProjectId}/Import/Audio </summary>
+        /// <remarks> POST: v1/projects/{projectId}/words/{wordId}/upload/audio </remarks>
+        /// <returns> Path to local audio file </returns>
+        [HttpPost("{wordId}/upload/audio")]
+        public async Task<IActionResult> UploadAudioFile(string projectId, string wordId, [FromForm] FileUpload fileUpload)
+        {
+            if (!_permissionService.IsProjectAuthenticated("1", HttpContext))
+            {
+                return new UnauthorizedResult();
+            }
+            var file = fileUpload.File;
+
+            //ensure file is not empty
+            if (file.Length == 0)
+            {
+                return new BadRequestObjectResult("Empty File");
+            }
+
+            //get path to home
+            Utilities util = new Utilities();
+            fileUpload.FilePath = util.GenerateFilePath(Filetype.audio, false, wordId, Path.Combine(projectId, "Import", "Audio"));
+
+            //copy the file data to a new local file
+            using (var fs = new FileStream(fileUpload.FilePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fs);
+            }
+
+            //add the relative path to the audio field
+            Word gotWord = await _wordRepo.GetWord(projectId, wordId);
+            gotWord.Audio.Add(fileUpload.FilePath); //TODO: this isnt relative
+
+            //update the word with new audio file
+            _ = await _wordService.Update(projectId, wordId, gotWord);
+
+            return new ObjectResult(fileUpload.FilePath);
+        }
     }
-
-
-    /// <summary> Adds a pronunciation <see cref="FileUpload"/> to a <see cref="Word"/> and saves locally to ~/.CombineFiles/{ProjectId}/Import/Audio </summary>
-    /// <remarks> POST: v1/projects/{projectId}/words/{wordId}/upload/audio </remarks>
-    /// <returns> Path to local audio file </returns>
-    [HttpPost("{wordId}/upload/audio")]
-    public async Task<IActionResult> UploadAudioFile(string projectId, string wordId, [FromForm] FileUpload fileUpload)
-    {
-        if (!_permissionService.IsProjectAuthenticated("1", HttpContext))
-        {
-            return new UnauthorizedResult();
-        }
-        var file = fileUpload.File;
-
-        //ensure file is not empty
-        if (file.Length == 0)
-        {
-            return new BadRequestObjectResult("Empty File");
-        }
-
-        //get path to home
-        Utilities util = new Utilities();
-        fileUpload.FilePath = util.GenerateFilePath(Filetype.audio, false, wordId, Path.Combine(projectId, "Import", "Audio"));
-
-        //copy the file data to a new local file
-        using (var fs = new FileStream(fileUpload.FilePath, FileMode.Create))
-        {
-            await file.CopyToAsync(fs);
-        }
-
-        //add the relative path to the audio field
-        Word gotWord = await _wordRepo.GetWord(projectId, wordId);
-        gotWord.Audio.Add(fileUpload.FilePath); //TODO: this isnt relative
-
-        //update the word with new audio file
-        _ = await _wordService.Update(projectId, wordId, gotWord);
-
-        return new ObjectResult(fileUpload.FilePath);
-    }
-}
 }
