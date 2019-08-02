09f6be2b203f12949beaadc3713ef3040436fdbb
Simeon <51300614+SimeonSIL@users.noreply.github.com>
2019-08-02 08:06:39 -0500

    Increase spacing under app bar (#245)

--- src/components/AppBar/AppBarComponent.tsx
+++ src/components/AppBar/AppBarComponent.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import { withLocalize, LocalizeContextProps } from "react-localize-redux";
+import { withLocalize } from "react-localize-redux";
 import { Grid } from "@material-ui/core";
 import UserMenu from "./UserMenu";
 import GoToHomeButton from "../GoToHomeButton/GoToHomeButtonComponent";
 import theme from "../../types/theme";
 
 /** An app bar shown at the top of almost every page of The Combine */
-export class AppBarComponent extends React.Component<LocalizeContextProps> {
-  render() {
-    return (
-      <div className="NavigationBar" style={{ marginBottom: 64 }}>
+export function AppBarComponent() {
+  return (
+    <React.Fragment>
+      <div className="NavigationBar" style={{ marginBottom: theme.spacing(6) }}>
         <AppBar position="fixed" style={{ zIndex: theme.zIndex.drawer + 1 }}>
           <Toolbar>
             <Grid
@@ -30,8 +30,8 @@ export class AppBarComponent extends React.Component<LocalizeContextProps> {
           </Toolbar>
         </AppBar>
       </div>
-    );
-  }
+    </React.Fragment>
+  );
 }
 
 export default withLocalize(AppBarComponent);
