69d73215da65bf41af30f3c877f02ff28b9d8cf9
Simeon <51300614+SimeonSIL@users.noreply.github.com>
2019-08-05 15:53:21 -0500

    Add Project Export Button (#261)
    * Make zip download work
    
    * Add loading button

---
+++ src/components/UserSettings/ExportProjectButton.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { Button } from "@material-ui/core";
+import { exportLift } from "../../backend";
+import LoadingButton from "./LoadingButton";
+import { ButtonProps } from "@material-ui/core/Button";
+import { Translate } from "react-localize-redux";
+
+/**
+ * Button for getting lift export from backend
+ */
+export default function ExportProjectButton(props: ButtonProps) {
+  const [exportedFile, setExportedFile] = React.useState<null | string>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  let downloadLink = React.createRef<HTMLAnchorElement>();
+
+  async function getFile() {
+    setLoading(true);
+    setExportedFile(await exportLift());
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if (downloadLink.current && exportedFile !== null) {
+      downloadLink.current.click();
+      setExportedFile(null);
+    }
+  });
+
+  return (
+    <React.Fragment>
+      <LoadingButton
+        onClick={() => getFile()}
+        color="primary"
+        loading={loading}
+        {...props}
+      >
+        <Translate id="projectSettings.export" />
+      </LoadingButton>
+      {exportedFile && (
+        <a ref={downloadLink} href={exportedFile} style={{ display: "none" }}>
+          (This link should not be visible)
+        </a>
+      )}
+    </React.Fragment>
+  );
+}
