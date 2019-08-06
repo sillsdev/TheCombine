4e0ee3eb020ada3e21a8b0a33992a97cc0551ce4
Simeon <51300614+SimeonSIL@users.noreply.github.com>
2019-08-06 15:23:12 -0500

    Fix avatar download (#262)
    * Fix avatar download
    
    * Fix tests

--- Backend/Interfaces/IUserService.cs
+++ Backend/Interfaces/IUserService.cs
@@ -8,6 +8,7 @@ namespace BackendFramework.Interfaces
     {
         Task<List<User>> GetAllUsers();
         Task<User> GetUser(string userId);
+        Task<string> GetUserAvatar(string userId);
         Task<User> Create(User user);
         Task<ResultOfUpdate> Update(string userId, User user);
         Task<bool> Delete(string userId);
