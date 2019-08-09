a52089ee5fc7c64b358b5299bf8a111381da60f6
SimeonSIL <simeon_eberz@sil.org>
2019-08-09 15:03:32 -0500

    Add navigation buttons to appbar

--- src/components/AppBar/Logo.tsx
+++ src/components/AppBar/Logo.tsx
@@ -1,18 +1,11 @@
-import history from "../../history";
 import React from "react";
 import { Button } from "@material-ui/core";
 import logo from "../../resources/CombineLogoV1.png";
 
 /** A button that redirects to the home page */
-export default function GoToHomeButton() {
+export default function Logo() {
   return (
-    <Button
-      onClick={() => {
-        history.location.pathname === "/"
-          ? history.push("/")
-          : history.push("/goals");
-      }}
-    >
+    <Button>
       <img src={logo} height="50" alt="Logo" />
     </Button>
   );
