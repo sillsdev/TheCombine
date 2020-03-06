74c56b28028e527158812c169cd96f481625763d
Jim Grady <jimgrady.jg@gmail.com>
2020-03-06 11:00:25 -0500

    Update trigger_cert_sync to be called when wired ethernet connected.

--- deploy/roles/cert_sync_service/files/trigger_cert_sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-set -euf -o pipefail
-
-# get PID for the certificate update process
-SVC_PID=`systemctl status sync_cert | grep "Main PID" | sed -E 's/^ *Main PID: ([0-9][0-9]*) *.*/\1/'`
-
-kill -SIGUSR1 ${SVC_PID}
