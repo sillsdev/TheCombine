11456e0d655afb8121647bd893a57901c0924439
johnthagen <johnthagen@gmail.com>
2020-05-19 19:34:55 -0400

    Remove duplicated user reading logic

--- src/components/Login/AuthHeaders.tsx
+++ src/components/Login/AuthHeaders.tsx
@@ -1,14 +1,15 @@
+import { getUser } from "../../backend/localStorage";
+
 /**
- * Returns authorization header with jwt token
+ * Returns authorization header with JWT token.
  *
- * When making an axios request on a protected endpoint, include `{headers:authHeader()}`
+ * When making an axios request on a protected endpoint, include
+ * `{ headers:authHeader() }`
  *
- * example: `axios.post("localhost:5001", data, { headers: authHeader()})`
+ * example: `axios.post("localhost:5001", data, { headers: authHeader() })`
  */
 export function authHeader() {
-  let userString = localStorage.getItem("user");
-  let user = userString ? JSON.parse(userString) : null;
-
+  let user = getUser();
   if (user && user.token) {
     return { Authorization: "Bearer " + user.token };
   } else {
