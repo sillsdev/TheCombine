ea8f68889ca686d1af816671c7479c52d8cc7cb3
SeHee Hyung <sehee.hyung@gordon.edu>
2020-06-01 03:57:04 -0400

    Always show submit button

--- src/components/DataEntry/DataEntryTable/DataEntryTable.tsx
+++ src/components/DataEntry/DataEntryTable/DataEntryTable.tsx
@@ -112,6 +112,7 @@ export class DataEntryTable extends React.Component<
       existingWords: words,
       recentlyAddedWords: updatedNewWords,
     });
+    window.scrollTo(0, document.body.scrollHeight);
   }
 
   /** Update the word in the backend and the frontend */
