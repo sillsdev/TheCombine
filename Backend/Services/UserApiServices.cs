7b4c5503ea26b91d344a5656f55c2d47c1b05e84
johnthagen <johnthagen@gmail.com>
2020-06-05 13:10:53 -0400

    Revert hash algorithm changes to defer until another PR

--- Backend/Services/UserApiServices.cs
+++ Backend/Services/UserApiServices.cs
@@ -31,14 +31,12 @@ namespace BackendFramework.Services
         {
             private const int SaltLength = 16;
 
-            /// <summary> Use SHA256 length. </summary>
-            private const int HashLength = 256 / 8;
+            /// <summary> Use SHA1 length. </summary>
+            private const int HashLength = 160 / 8;
 
             /// <summary> Hash iterations to slow down brute force password cracking. </summary>
             /// It's important that this value is not too low, or password cracking is made easier.
-            /// Value selected from default Django 3.1 iteration count (appropriate as of August 2020).
-            /// https://docs.djangoproject.com/en/dev/releases/3.1/#django-contrib-auth
-            private const int HashIterations = 216000;
+            private const int HashIterations = 10000;
 
             /// <summary>
             /// Hash a password with a generated salt and return the combined bytes suitable for storage.
@@ -86,8 +84,7 @@ namespace BackendFramework.Services
             /// <summary> Hash a password and salt using PBKDF2. </summary>
             private static byte[] HashPassword(string password, byte[] salt)
             {
-                // SHA256 is the recommended PBKDF2 hash algorithm.
-                using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, HashIterations, HashAlgorithmName.SHA256);
+                using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, HashIterations, HashAlgorithmName.SHA1);
                 return pbkdf2.GetBytes(HashLength);
             }
 
