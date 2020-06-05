1dae26c4964fba7df5ef47c9ff3402f5f15a1207
johnthagen <johnthagen@users.noreply.github.com>
2020-06-05 13:59:35 -0400

    Move ResultOfUpdate into a common module (#408)

--- Backend/Controllers/UserController.cs
+++ Backend/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using BackendFramework.Helper;
 using BackendFramework.Interfaces;
 using BackendFramework.Models;
 using Microsoft.AspNetCore.Authorization;
