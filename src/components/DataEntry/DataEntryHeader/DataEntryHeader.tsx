931d5b60d6fd457ea0a05ddb3c2daf35696a507f
charjhall <jackson.hall@gatech.edu>
2020-06-05 16:48:50 -0400

    Add functional but ugly ui update

--- src/components/DataEntry/DataEntryHeader/DataEntryHeader.tsx
+++ src/components/DataEntry/DataEntryHeader/DataEntryHeader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Switch, Typography } from "@material-ui/core";
+import { HelpOutline } from "@material-ui/icons";
 import theme from "../../../types/theme";
 import {
   Translate,
@@ -12,13 +13,33 @@ interface DataEntryHeaderProps {
   domain: DomainTree;
 }
 
+interface DataEntryHeaderState {
+  questionsVisible: boolean;
+}
+
 /**
  * Displays information about the current data entry view
  */
 export class DataEntryHeader extends React.Component<
-  DataEntryHeaderProps & LocalizeContextProps
+  DataEntryHeaderProps & LocalizeContextProps,
+  DataEntryHeaderState
 > {
+  constructor(props: DataEntryHeaderProps & LocalizeContextProps) {
+    super(props);
+    this.state = {
+      questionsVisible: false,
+    };
+  }
   render() {
+    let questions;
+    if (this.state.questionsVisible) {
+      questions = (
+        <Typography>
+          The Questions will go here eventually, it will likely need an
+          iteration
+        </Typography>
+      );
+    }
     return (
       <Typography
         variant="h4"
@@ -29,6 +50,13 @@ export class DataEntryHeader extends React.Component<
         {": "}
         {this.props.domain.name + " (" + this.props.domain.id + ")"}
         <Typography>{this.props.domain.description}</Typography>
+        <Switch
+          onChange={() => {
+            this.setState({ questionsVisible: !this.state.questionsVisible });
+          }}
+          icon={<HelpOutline />}
+        />
+        {questions}
       </Typography>
     );
   }
