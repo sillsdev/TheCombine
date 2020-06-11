c71da1a97a0d41879bd5fd9b28312a77ee38ab72
charjhall <jackson.hall@gatech.edu>
2020-06-11 17:28:30 -0400

    Add npm run command and complete untested version

--- importSemanticDomains.ts
+++ importSemanticDomains.ts
@@ -26,24 +26,24 @@ fs.readFile(__dirname + "/SemanticDomains-es.xml", function (
   parser.parseString(data, function (err: Error, result) {
     const foriegnLanguage: string = result.List.Name[0].AUni[1].$.ws;
     const parsedDomain = result.List.Possibilities[0].CmSemanticDomain;
-    let cleanedEnglishDomain: SemanticDomainWithSubdomains = new SemanticDomainWithSubdomains(),
-      cleanedForiegnDomain: SemanticDomainWithSubdomains = new SemanticDomainWithSubdomains();
+    let cleanedEnglishDomain: SemanticDomainWithSubdomains[] = [],
+      cleanedForiegnDomain: SemanticDomainWithSubdomains[] = [];
     generateCleanJSON(parsedDomain, cleanedEnglishDomain, cleanedForiegnDomain);
 
-    fs.writeFile(
-      process.cwd() + "src/resources/semantic-domains/en.json",
-      JSON.stringify(cleanedEnglishDomain, null, "  "),
-      (err) => console.log(err)
-    );
+    // fs.writeFile(
+    //   process.cwd() + "src/resources/semantic-domains/en.json",
+    //   JSON.stringify(cleanedEnglishDomain, null, "  "),
+    //   (err) => console.log(err)
+    // );
 
-    fs.writeFile(
-      process.cwd() +
-        "src/resources/semantic-domains/" +
-        foriegnLanguage +
-        ".json",
-      JSON.stringify(cleanedForiegnDomain, null, "  "),
-      (err) => console.log(err)
-    );
+    // fs.writeFile(
+    //   process.cwd() +
+    //     "src/resources/semantic-domains/" +
+    //     foriegnLanguage +
+    //     ".json",
+    //   JSON.stringify(cleanedForiegnDomain, null, "  "),
+    //   (err) => console.log(err)
+    // );
 
     console.log("Done");
   });
@@ -51,38 +51,50 @@ fs.readFile(__dirname + "/SemanticDomains-es.xml", function (
 
 function generateCleanJSON(
   domain: any[],
-  enDom: SemanticDomainWithSubdomains,
-  fnDom: SemanticDomainWithSubdomains
-): SemanticDomainWithSubdomains[] {
-  let cleanArr: SemanticDomainWithSubdomains[] = [];
+  enDom: SemanticDomainWithSubdomains[],
+  fnDom: SemanticDomainWithSubdomains[]
+) {
   domain.forEach(function (subDomain) {
-    let newEntry: SemanticDomainWithSubdomains = new SemanticDomainWithSubdomains();
-    newEntry.Name = subDomain.Name[0].AUni[0]._;
-    newEntry.Id = subDomain.Abbreviation[0].AUni[0]._;
-    console.log(newEntry.Id);
-    newEntry.Description = subDomain.Description[0].AStr[0].Run[0]._;
-    newEntry.Questions = [];
+    let newEnglishEntry: SemanticDomainWithSubdomains = new SemanticDomainWithSubdomains(),
+      newForiegnEntry: SemanticDomainWithSubdomains = new SemanticDomainWithSubdomains();
+
+    newEnglishEntry.Name = subDomain.Name[0].AUni[0]._;
+    newEnglishEntry.Id = subDomain.Abbreviation[0].AUni[0]._;
+    newEnglishEntry.Description = subDomain.Description[0].AStr[0].Run[0]._;
+    newEnglishEntry.Questions = [];
+    newEnglishEntry.Subdomains = [];
+
+    newForiegnEntry.Name = subDomain.Name[0].AUni[1]._;
+    newForiegnEntry.Id = subDomain.Abbreviation[0].AUni[1]._;
+    newForiegnEntry.Description = subDomain.Description[0].AStr[1].Run[0]._;
+    newForiegnEntry.Questions = [];
+    newEnglishEntry.Subdomains = [];
 
     if (subDomain.hasOwnProperty("Questions")) {
-      //Iterate through the questions and add them to newEntry
+      //Iterate through the questions and add them to the new entries
       for (
         let i: number = 0;
         i < subDomain.Questions[0].CmDomainQ.length;
         i++
       ) {
-        newEntry.Questions.push(
+        newEnglishEntry.Questions.push(
           subDomain.Questions[0].CmDomainQ[i].Question[0].AUni[0]._
+        );
+        newForiegnEntry.Questions.push(
+          subDomain.Questions[0].CmDomainQ[i].Question[0].AUni[1]._
         );
       }
     }
 
     if (subDomain.hasOwnProperty("SubPossibilities")) {
-      // newEntry.Subdomains = generateCleanJSON(
-      //   subDomain.SubPossibilities[0].CmSemanticDomain
-      // );
+      generateCleanJSON(
+        subDomain.SubPossibilities[0].CmSemanticDomain,
+        newEnglishEntry.Subdomains,
+        newForiegnEntry.Subdomains
+      );
     }
 
-    cleanArr.push(newEntry);
+    enDom.push(newEnglishEntry);
+    fnDom.push(newForiegnEntry);
   });
-  return cleanArr;
 }
