8e59f3316ef9e1ac69e20904d115015ed936d376
Joseph <jgaynier2014@gmail.com>
2020-06-12 15:47:25 -0400

    Change audio recorder color while recording

--- src/components/Pronunciations/AudioRecorder.tsx
+++ src/components/Pronunciations/AudioRecorder.tsx
@@ -23,8 +23,11 @@ const useStyles = makeStyles((theme: Theme) =>
     button: {
       margin: theme.spacing(1),
     },
-    icon: {
-      color: red[800],
+    iconPress: {
+      color: red[900],
+    },
+    iconRelease: {
+      color: red[600],
     },
   })
 );
@@ -95,7 +98,9 @@ export default function AudioRecorder(props: RecorderProps) {
         className={classes.button}
         aria-label="record"
       >
-        <FiberManualRecord className={classes.icon} />
+        <FiberManualRecord
+          className={isRecording ? classes.iconPress : classes.iconRelease}
+        />
       </IconButton>
     </Tooltip>
   );
