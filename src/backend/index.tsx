230e69547c23c0c9559fca429ce6e1acd5cfce0e
Joseph <jgaynier2014@gmail.com>
2020-06-12 12:57:01 -0400

    Undo last commit

--- src/backend/index.tsx
+++ src/backend/index.tsx
@@ -267,9 +267,10 @@ export async function deleteAudio(
   wordId: string,
   fileName: string
 ): Promise<string> {
-  let resp = await backendServer.delete(getAudioUrl(wordId, fileName), {
-    headers: authHeader(),
-  });
+  let resp = await backendServer.delete(
+    `${baseURL}/projects/${LocalStorage.getProjectId()}/words/${wordId}/audio/delete/${fileName}`,
+    { headers: authHeader() }
+  );
   return resp.data;
 }
 
