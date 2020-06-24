df7380b79346ba710a587ea1d986221b1e73da19
James Pruitt <jamescpruitt@gmail.com>
2020-06-24 16:03:48 -0400

    Enforce password rules

--- src/components/PasswordReset/ResetPage/component.tsx
+++ src/components/PasswordReset/ResetPage/component.tsx
@@ -22,6 +22,8 @@ export interface PasswordResetState {
   password: string;
   passwordConfirm: string;
   sentAttempt: boolean;
+  passwordLength: boolean;
+  passwordSame: boolean;
 }
 
 export default class PasswordReset extends React.Component<
@@ -37,6 +39,8 @@ export default class PasswordReset extends React.Component<
       password: "",
       passwordConfirm: "",
       sentAttempt: false,
+      passwordLength: true,
+      passwordSame: true,
     };
   }
 
@@ -45,11 +49,18 @@ export default class PasswordReset extends React.Component<
       ...this.state,
       sentAttempt: true,
     });
-    this.props.passwordReset(
-      this.state.token,
-      this.state.password
-    );
+    this.props.passwordReset(this.state.token, this.state.password);
     event.preventDefault();
+  };
+
+  onChangePassword = (password: string, confirmPassword: string) => {
+    this.setState({
+      ...this.state,
+      passwordLength: password.length < 8,
+      passwordSame: password !== confirmPassword,
+      password: password,
+      passwordConfirm: confirmPassword,
+    });
   };
 
   render() {
@@ -77,31 +88,49 @@ export default class PasswordReset extends React.Component<
               <Grid item>
                 <TextField
                   variant="outlined"
-                  label={<Translate id="passwordReset.passwordLabel" />}
+                  label={<Translate id="login.password" />}
                   type="password"
                   value={this.state.password}
                   style={{ width: "100%" }}
                   margin="normal"
+                  error={this.state.passwordLength}
                   onChange={(e) =>
-                    this.setState({ ...this.state, password: e.target.value })
+                    this.onChangePassword(
+                      e.target.value,
+                      this.state.passwordConfirm
+                    )
                   }
                 />
+                {this.state.passwordLength && (
+                  <Typography
+                    variant="body2"
+                    style={{ display: "inline", margin: 24, color: "red" }}
+                  >
+                    <Translate id="login.passwordRequirements"/>
+                  </Typography>
+                )}
               </Grid>
               <Grid item>
                 <TextField
                   variant="outlined"
-                  label={<Translate id="passwordReset.passwordConfLabel" />}
+                  label={<Translate id="login.confirmPassword" />}
                   type="password"
                   value={this.state.passwordConfirm}
                   style={{ width: "100%" }}
                   margin="normal"
+                  error={this.state.passwordSame && this.state.passwordConfirm.length > 0}
                   onChange={(e) =>
-                    this.setState({
-                      ...this.state,
-                      passwordConfirm: e.target.value,
-                    })
+                    this.onChangePassword(this.state.password, e.target.value)
                   }
                 />
+                {this.state.passwordSame && this.state.passwordConfirm.length > 0 &&
+                  <Typography
+                    variant="body2"
+                    style={{display: "inline", margin: 24, color: "red"}}
+                  >
+                  <Translate id="login.confirmPasswordError"/>
+                  </Typography>
+                }
               </Grid>
 
               <Grid container justify="flex-end" spacing={2}>
@@ -117,6 +146,7 @@ export default class PasswordReset extends React.Component<
                   <Button
                     variant="contained"
                     color="primary"
+                    disabled={this.state.passwordLength || this.state.passwordSame}
                     onClick={this.onSubmit}
                   >
                     <Translate id="passwordReset.submit" />
