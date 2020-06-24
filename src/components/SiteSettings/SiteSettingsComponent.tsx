85d930025b70d8d0a9abee1ab00841f45fe5061e
14JAW <38673644+14JAW@users.noreply.github.com>
2020-06-24 16:50:45 -0500

    Feature/398 (#438)
    * Shade current tab
    
    switch back to buttons
    
    Make NavButtons render knowing current page
    
    make ProjectName button go to project settings
    
    Turn AppBar Text White
    
    Run Prettier
    
    Create CurTab Type and TabColor Function
    
    Cut project settings out of userMenu
    
    Create Test for AppBar
    
    fix ProjectNameButton Unit Test
    
    * remove unneeded imports
    
    * Commit final changes
    
    * Merge branch 'master' into feature/398
    
    * Prettier.
    
    * Fix invisible merge error.
    
    * Add SiteSettings to CurrentTab options.

--- src/components/SiteSettings/SiteSettingsComponent.tsx
+++ src/components/SiteSettings/SiteSettingsComponent.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-localize-redux";
 import { Grid } from "@material-ui/core";
 import { List, People } from "@material-ui/icons";
+import { CurrentTab } from "../../types/currentTab";
 import AppBarComponent from "../AppBar/AppBarComponent";
 import BaseSettingsComponent from "../ProjectSettings/BaseSettingsComponent/BaseSettingsComponent";
 import ProjectsExport from "./ProjectsExport";
@@ -18,7 +19,7 @@ class SiteSettingsComponent extends React.Component<LocalizeContextProps> {
   render() {
     return (
       <React.Fragment>
-        <AppBarComponent />
+        <AppBarComponent currentTab={CurrentTab.SiteSettings} />
         <Grid container justify="center" spacing={6}>
           {/* Project List */}
           <BaseSettingsComponent
