700572855ca9ee1c80b64e03da769ce241482d09
imnasnainaec <imnasnainaec@gmail.com>
2020-06-25 14:20:29 -0400

    Restore multiGlossWords in testWordList.

--- src/types/word.tsx
+++ src/types/word.tsx
@@ -105,8 +105,8 @@ export function testWordList(): Word[] {
     simpleWord("Yeet", "Please"),
     simpleWord("Yeet", "Mandatory"),
     simpleWord("Yang", "Die"),
-    simpleWord("Yuino", "Love"),
-    simpleWord("Yuino", "Boba Fett"),
+    multiGlossWord("Yuino", ["Love", "Boba Fett", "Life"]),
+    multiGlossWord("Yuilo", ["Sadness", "Tree bark"]),
     simpleWord("Yes", "Wumbo"),
     simpleWord("Yes", "Mayonnaise"),
   ];
