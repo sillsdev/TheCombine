54602073075f3d1d9ab2321bdb31b19a9da7e3e9
Joel Akers <joelwakers@cedarville.edu>
2020-07-15 12:50:38 -0500

    combine mockWords, reverse DataEntryCom changes

--- src/components/DataEntry/tests/MockDomainWord.ts
+++
@@ -1,7 +0,0 @@
-import { mockWord } from "./MockWord";
-import { DomainWord } from "../../../types/word";
-
-export const mockDomainWord: DomainWord = {
-  word: mockWord,
-  gloss: mockWord.senses[0].glosses[0],
-};
