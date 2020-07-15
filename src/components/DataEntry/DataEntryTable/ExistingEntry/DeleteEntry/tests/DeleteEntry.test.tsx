25e2dc4d6959743fa0a3662688ea512148f79374
imnasnainaec <imnasnainaec@gmail.com>
2020-07-15 10:58:53 -0400

    Cleanup DeleteEntry test file.

--- src/components/DataEntry/DataEntryTable/ExistingEntry/DeleteEntry/tests/DeleteEntry.test.tsx
+++ src/components/DataEntry/DataEntryTable/ExistingEntry/DeleteEntry/tests/DeleteEntry.test.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+
 import LocalizedDeleteEntry from "../DeleteEntry";
 
 describe("Tests DeleteEntry", () => {
   it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(
-      <LocalizedDeleteEntry entryIndex={0} removeEntry={() => null} />,
-      div
-    );
-    ReactDOM.unmountComponentAtNode(div);
+    renderer.act(() => {
+      renderer.create(<LocalizedDeleteEntry removeEntry={() => null} />);
+    });
   });
 });
