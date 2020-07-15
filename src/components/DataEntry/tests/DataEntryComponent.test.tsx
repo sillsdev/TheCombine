48f5df064f8f013bb544a2e962bd0b3e88ace538
charjhall <jackson.hall@gatech.edu>
2020-07-15 16:24:01 -0400

    Add unit test for DataEntryComponent

--- src/components/DataEntry/tests/DataEntryComponent.test.tsx
+++ src/components/DataEntry/tests/DataEntryComponent.test.tsx
@@ -11,65 +11,52 @@ import renderer, {
 import { Provider } from "react-redux";
 import { DataEntryTable } from "../DataEntryTable/DataEntryTable";
 import { DataEntryHeader } from "../DataEntryHeader/DataEntryHeader";
-import { AppBarComponent } from "../../AppBar/AppBarComponent";
-import { Divider } from "@material-ui/core";
-jest.mock("../DataEntryHeader/DataEntryHeader");
-jest.mock("../DataEntryTable/DataEntryTable");
+
 jest.mock("../../TreeView");
 jest.mock("../../AppBar/AppBarComponent"); //ReactTestRenderer doesn't like rendering UserMenu
 jest.mock("@material-ui/core/Dialog");
+jest.mock("../../Pronunciations/Recorder");
 const createMockStore = configureMockStore([]);
 const mockStore = createMockStore({});
 
-let testRenderer: ReactTestRenderer;
-const newDomain = { ...baseDomain, questions: ["Q1", "Q2", "Q3"] };
-
-beforeEach(() => {
-  renderer.act(() => {
-    testRenderer = renderer.create(
-      <Provider store={mockStore}>
-        <DataEntryComponent domain={newDomain} />
-      </Provider>
-    );
-  });
-});
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
   })),
 });
+
 describe("Tests DataEntryComponent", () => {
   fit("Questions hidden on complete clicked", () => {
     const newDomain = { ...baseDomain, questions: ["Q1", "Q2", "Q3"] };
-    // const parentInstance: ReactTestInstance = createDataEntryComponentInstance(
-    //   newDomain
-    // );
-    const componentInstance = testRenderer.root.findAllByType(
-      DataEntryComponent
-    )[0];
+    const parentInstance: ReactTestInstance = createDataEntryComponentInstance(
+      newDomain
+    );
 
-    const tableInstances = testRenderer.root.findAllByType(DataEntryTable);
-    const headerInstances = testRenderer.root.findAllByType(DataEntryHeader);
+    const tableInstances = parentInstance.findAllByType(DataEntryTable);
+    const headerInstances = parentInstance.findAllByType(DataEntryHeader);
     expect(tableInstances.length).toBe(1);
     expect(headerInstances.length).toBe(1);
     const tableInstance = tableInstances[0];
     const headerInstance = headerInstances[0];
 
-    componentInstance.instance.setState({ questionsVisible: true }, () => {
-      tableInstance.findAllByProps({ id: "complete" })[0].props.onClick();
-      const questionSwitch: ReactTestInstance = headerInstance.findByProps({
-        id: "questionVisibilitySwitch",
-      });
-      expect(questionSwitch.props.checked).toBeFalsy();
+    const questionSwitch: ReactTestInstance = headerInstance.findByProps({
+      id: "questionVisibilitySwitch",
     });
+    questionSwitch.props.onChange();
+    expect(questionSwitch.props.checked).toBeTruthy();
+    const completeButtonHandle = tableInstance.findAllByProps({
+      id: "complete",
+    })[0];
+    completeButtonHandle.props.onClick();
+    expect(questionSwitch.props.checked).toBeFalsy();
   });
 });
 
