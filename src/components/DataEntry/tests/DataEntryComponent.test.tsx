6578fea91b80562496e8c4ed8b06a027085ab8c3
charjhall <jackson.hall@gatech.edu>
2020-07-15 14:39:41 -0400

    Update for consulatation

--- src/components/DataEntry/tests/DataEntryComponent.test.tsx
+++ src/components/DataEntry/tests/DataEntryComponent.test.tsx
@@ -11,7 +11,8 @@ import renderer, {
 import { Provider } from "react-redux";
 import { DataEntryTable } from "../DataEntryTable/DataEntryTable";
 import { DataEntryHeader } from "../DataEntryHeader/DataEntryHeader";
-
+import { AppBarComponent } from "../../AppBar/AppBarComponent";
+import { Divider } from "@material-ui/core";
 jest.mock("../DataEntryHeader/DataEntryHeader");
 jest.mock("../DataEntryTable/DataEntryTable");
 jest.mock("../../TreeView");
@@ -21,17 +22,17 @@ const createMockStore = configureMockStore([]);
 const mockStore = createMockStore({});
 
 let testRenderer: ReactTestRenderer;
-// const newDomain = { ...baseDomain, questions: ["Q1", "Q2", "Q3"] };
+const newDomain = { ...baseDomain, questions: ["Q1", "Q2", "Q3"] };
 
-// beforeEach(() => {
-//   renderer.act(() => {
-//     testRenderer = renderer.create(
-//       <Provider store={mockStore}>
-//         <DataEntryComponent domain={newDomain} />
-//       </Provider>
-//     );
-//   });
-// });
+beforeEach(() => {
+  renderer.act(() => {
+    testRenderer = renderer.create(
+      <Provider store={mockStore}>
+        <DataEntryComponent domain={newDomain} />
+      </Provider>
+    );
+  });
+});
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
@@ -48,20 +49,27 @@ Object.defineProperty(window, "matchMedia", {
 describe("Tests DataEntryComponent", () => {
   fit("Questions hidden on complete clicked", () => {
     const newDomain = { ...baseDomain, questions: ["Q1", "Q2", "Q3"] };
-    const parentInstance: ReactTestInstance = createDataEntryComponentInstance(
-      newDomain
-    );
-    // parentInstance.instance.setState({ questionsVisible: true }, () => {
-    const tableInstance = parentInstance.findAllByType(DataEntryTable)[0];
-    tableInstance.findByProps({ id: "complete" }).props.onClick();
-    const headerInstance = parentInstance.findAllByType(DataEntryHeader)[0];
-    newDomain.questions.forEach((questionString, index) => {
-      let question: ReactTestInstance = headerInstance.findByProps({
-        id: `q${index}`,
+    // const parentInstance: ReactTestInstance = createDataEntryComponentInstance(
+    //   newDomain
+    // );
+    const componentInstance = testRenderer.root.findAllByType(
+      DataEntryComponent
+    )[0];
+
+    const tableInstances = testRenderer.root.findAllByType(DataEntryTable);
+    const headerInstances = testRenderer.root.findAllByType(DataEntryHeader);
+    expect(tableInstances.length).toBe(1);
+    expect(headerInstances.length).toBe(1);
+    const tableInstance = tableInstances[0];
+    const headerInstance = headerInstances[0];
+
+    componentInstance.instance.setState({ questionsVisible: true }, () => {
+      tableInstance.findAllByProps({ id: "complete" })[0].props.onClick();
+      const questionSwitch: ReactTestInstance = headerInstance.findByProps({
+        id: "questionVisibilitySwitch",
       });
-      expect(question.props.children).toEqual(questionString);
+      expect(questionSwitch.props.checked).toBeFalsy();
     });
-    // });
   });
 });
 
