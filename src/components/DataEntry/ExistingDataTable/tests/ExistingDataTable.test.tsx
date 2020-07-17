6f3518a46a3d49dcecbc911862e6d645e0e7872c
Joel Akers <joelwakers@cedarville.edu>
2020-07-17 12:55:15 -0500

    fix mockDomainTree

--- src/components/DataEntry/ExistingDataTable/tests/ExistingDataTable.test.tsx
+++ src/components/DataEntry/ExistingDataTable/tests/ExistingDataTable.test.tsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import renderer, { ReactTestRenderer } from "react-test-renderer";
 import configureMockStore from "redux-mock-store";
 import { defaultState } from "../../../App/DefaultState";
-import { mockDomainTree } from "../../tests/MockDomainTree";
+import { baseDomain } from "../../../../types/SemanticDomain";
 import { ExistingDataTable } from "../ExistingDataTable";
 
 jest.mock("../ImmutableExistingData/ImmutableExistingData");
@@ -12,11 +12,7 @@ var testRenderer: ReactTestRenderer;
 const createMockStore = configureMockStore([]);
 const mockStore = createMockStore(defaultState);
 
-const domains = [
-  { ...mockDomainTree },
-  { ...mockDomainTree },
-  { ...mockDomainTree },
-];
+const domains = [{ ...baseDomain }, { ...baseDomain }, { ...baseDomain }];
 
 domains[0].name = "daily";
 domains[1].name = "weather";
@@ -27,7 +23,7 @@ describe("Tests ExistingData", () => {
       testRenderer = renderer.create(
         <Provider store={mockStore}>
           <ExistingDataTable
-            domain={mockDomainTree}
+            domain={baseDomain}
             typeDrawer={false}
             domainWords={[]}
             drawerOpen={false}
@@ -42,7 +38,7 @@ describe("Tests ExistingData", () => {
       testRenderer = renderer.create(
         <Provider store={mockStore}>
           <ExistingDataTable
-            domain={mockDomainTree}
+            domain={baseDomain}
             typeDrawer={true}
             domainWords={[]}
             drawerOpen={true}
