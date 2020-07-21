c8d60f2f687996b30fa38c2b610d45a19d16f3cd
Joel Akers <joelwakers@cedarville.edu>
2020-07-21 15:41:37 -0500

    put commas between Analysis Langs

--- src/components/ProjectSettings/ProjectLanguages/ProjectLanguages.tsx
+++ src/components/ProjectSettings/ProjectLanguages/ProjectLanguages.tsx
@@ -32,18 +32,41 @@ class ProjectName extends React.Component<
     }
   }
 
+  getAnalysisLangNames() {
+    let names: string[] = [
+      "read, ",
+      "this, ",
+      "before, ",
+      "you, ",
+      "give, ",
+      "up, ",
+    ];
+    for (let languages of this.props.project.analysisWritingSystems) {
+      names.push(languages.concat(", "));
+    }
+    let tempName = names[names.length - 1];
+    names[names.length - 1] = tempName.substr(0, tempName.length - 2);
+    return names;
+  }
+
+  getVernLang() {
+    let result: string = this.props.project.vernacularWritingSystem;
+    result = "The writing system does not work right now";
+    return result;
+  }
+
   render() {
     return (
       <React.Fragment>
         <Typography>
           <Translate id="projectSettings.language.vernacular" />
           {": "}
-          {this.props.project.vernacularWritingSystem}
+          {this.getVernLang()}
         </Typography>
         <Typography>
           <Translate id="projectSettings.language.analysis" />
           {": "}
-          {this.props.project.name}
+          {this.getAnalysisLangNames()}
         </Typography>
       </React.Fragment>
     );
