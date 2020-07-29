c1282f8dc7ee78cbe8deb118e53dbcc6d2a6880f
Joel Akers <joelwakers@cedarville.edu>
2020-07-29 01:16:14 -0500

    remove unused imports

--- src/components/ProjectSettings/ProjectLanguages/index.tsx
+++ src/components/ProjectSettings/ProjectLanguages/index.tsx
@@ -2,10 +2,6 @@ import ProjectLanguages from "./ProjectLanguages";
 import { StoreState } from "../../../types";
 import { connect } from "react-redux";
 
-import { Dispatch } from "redux";
-import { ProjectAction, setCurrentProject } from "../../Project/ProjectActions";
-import { Project } from "../../../types/project";
-
 function mapStateToProps(state: StoreState) {
   return {
     project: state.currentProject,
