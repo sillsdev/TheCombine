7177133a80c420f83331ea3d4c7101a027bc27cb
Joseph <jgaynier2014@gmail.com>
2020-08-13 17:29:43 -0400

    Change ToLower to ToLowerInvariant

--- Backend/Controllers/UserController.cs
+++ Backend/Controllers/UserController.cs
@@ -37,7 +37,7 @@ namespace BackendFramework.Controllers
         [HttpPost("forgot")]
         public async Task<IActionResult> ResetPasswordRequest([FromBody] PasswordResetData data)
         {
-            var email = data.Email.ToLower();
+            var email = data.Email.ToLowerInvariant();
             // create password reset
             var resetRequest = await _passwordResetService.CreatePasswordReset(email);
 
@@ -116,7 +116,7 @@ namespace BackendFramework.Controllers
         {
             try
             {
-                cred.Username = cred.Username.ToLower();
+                cred.Username = cred.Username.ToLowerInvariant();
                 var user = await _userService.Authenticate(cred.Username, cred.Password);
                 if (user == null)
                 {
@@ -157,8 +157,8 @@ namespace BackendFramework.Controllers
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] User user)
         {
-            user.Username = user.Username.ToLower();
-            user.Email = user.Email.ToLower();
+            user.Username = user.Username.ToLowerInvariant();
+            user.Email = user.Email.ToLowerInvariant();
             var returnUser = await _userService.Create(user);
             if (returnUser == null)
             {
@@ -175,7 +175,7 @@ namespace BackendFramework.Controllers
         [HttpPost("checkusername/{username}")]
         public async Task<IActionResult> CheckUsername(string username)
         {
-            username = username.ToLower();
+            username = username.ToLowerInvariant();
             var usernameTaken = (await _userService.GetAllUsers()).Find(x => x.Username == username) != null;
             if (usernameTaken)
             {
@@ -192,7 +192,7 @@ namespace BackendFramework.Controllers
         [HttpPost("checkemail/{email}")]
         public async Task<IActionResult> CheckEmail(string email)
         {
-            email = email.ToLower();
+            email = email.ToLowerInvariant();
             var emailTaken = (await _userService.GetAllUsers()).Find(x => x.Email == email) != null;
             if (emailTaken)
             {
