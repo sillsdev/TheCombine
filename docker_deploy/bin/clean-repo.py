3459f7c55b1237d275fbf7afea6a3e5194ac18dc
Jim Grady <jimgrady.jg@gmail.com>
2020-10-01 07:31:42 -0400

    Consistent capitalization in documentation/comments.

--- docker_deploy/bin/clean-repo.py
+++ docker_deploy/bin/clean-repo.py
@@ -16,7 +16,7 @@ import subprocess
 import re
 
 def parse_args() -> argparse.Namespace:
-    """Parse user command line arguments."""
+    # Parse user command line arguments
     parser = argparse.ArgumentParser(
         description="Clean an AWS ECR repository of all images/tags except those listed",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -34,25 +34,25 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--keep",
         nargs='+',
-        help="list of tags to keep",
+        help="List of tags to keep",
     )
     parser.add_argument(
         "--keep-pattern",
         dest='keep_pattern',
         nargs='+',
-        help="list of regular expressions that specify tags to keep"
+        help="List of regular expressions that specify tags to keep"
     )
     # Add some debugging options to see what's going on
     parser.add_argument(
         "--dry-run",
         dest='dry_run',
         action="store_true",
-        help="print delete command that would be run instead of executing it"
+        help="Print delete command that would be run instead of executing it"
     )
     parser.add_argument(
         "--verbose",
         action="store_true",
-        help="print intermediate values to aid in debugging"
+        help="Print intermediate values to aid in debugging"
     )
     return parser.parse_args()
 
@@ -82,29 +82,29 @@ def buildAwsCommand(profile, repo, subcommand, awsArgs=None):
 def main() -> None:
     args = parse_args()
 
-    # get a list of the current image tags for the specified repo
+    # Get a list of the current image tags for the specified repo
     awsCmd = buildAwsCommand(args.profile, args.repo, "describe-images")
     awsResult = runAwsCmd(awsCmd, args.verbose)
 
-    # create a list of tags that are not on our list of tags to keep
+    # Create a list of tags that are not on our list of tags to keep
     oldTags = []
 
-    # load the JSON output of the describe-images command into a 'repoImages'
+    # Load the JSON output of the describe-images command into a 'repoImages'
     # dictionary
     repoImages = json.loads(awsResult.stdout)
 
-    # initialize list of images to be removed with the option name for the
+    # Initialize list of images to be removed with the option name for the
     # aws ecr command
     image_ids = ["--image-ids"]
 
-    # join patterns of tags to keep to a single regular expression
+    # Join patterns of tags to keep to a single regular expression
     keep_pattern = ''
     if args.keep_pattern != None:
         keep_pattern = '^(?:% s)$' % '|'.join(args.keep_pattern)
         if args.verbose:
             print("keep_pattern: ", keep_pattern)
 
-    # iterate over image descriptions returned by AWS
+    # Iterate over image descriptions returned by AWS
     for imageStruct in repoImages['imageDetails']:
         # check to see if each tag should be kept
         for tag in imageStruct['imageTags']:
@@ -118,7 +118,7 @@ def main() -> None:
 
     # Remove all the specified image(s)
     if len(oldTags) > 0:
-    # convert the list of tags to a set of image-ids for the AWS ECR command
+    # Convert the list of tags to a set of image-ids for the AWS ECR command
         for tag in oldTags:
             image_ids.append("imageTag="+tag)
         awsCmd = buildAwsCommand(args.profile, args.repo, "batch-delete-image", image_ids)
