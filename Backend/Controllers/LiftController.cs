b29c48b252aaccb6f0c76b722c6116092e4c47ee
johnthagen <johnthagen@gmail.com>
2020-10-11 13:53:19 -0400

    Remove unused import

--- Backend/Controllers/LiftController.cs
+++ Backend/Controllers/LiftController.cs
@@ -6,7 +6,6 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using BackendFramework.Interfaces;
 using BackendFramework.Models;
-using BackendFramework.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
