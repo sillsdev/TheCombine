ba9c8420d4dd2de8f7065c023a92186b7da49d34
Jim Grady <jimgrady.jg@gmail.com>
2020-10-12 09:29:04 -0400

    Move functions for self-signed certs to selfsigned.sh

--- certmgr/scripts/func.sh
+++ certmgr/scripts/func.sh
@@ -40,18 +40,6 @@ clean_certs() {
   rm -f "${CERT_PATH}/renewal/${CERT_NAME}.conf"
 }
 
-create_selfsigned_cert() {
-  DEST_DIR="${CERT_PATH}/live/${CERT_NAME}"
-  mkdir -p ${DEST_DIR}
-
-  cd ${DEST_DIR}
-  openssl req -x509 -nodes -newkey rsa:4096 -days ${CERT_SELF_SIGNED_EXPIRE} -keyout privkey.pem -out fullchain.pem -subj '/CN=localhost'
-  if [ "$CERT_VERBOSE" = "1" ] ; then
-    echo "Created certificate in ${CERT_PATH} for ${cert_domains}"
-    echo "Expires: "`openssl x509 -in fullchain.pem -noout -enddate`
-  fi
-}
-
 create_certbot_cert() {
   if [ "$CERT_VERBOSE" = "1" ] ; then
     echo "create certbot certificate"
@@ -79,18 +67,4 @@ wait_for_webserver() {
   done
   echo "Failed to connect to ${cert_domains}" >2
   return 1;
-}
-
-check_renewal()
-{
-  if [ "$CERT_VERBOSE" = "1" ] ; then
-    echo "Checking for renewal of ${CERT_PATH}/live/${CERT_NAME}/fullchain.pem"
-  fi
-
-  CERT_MIN_SEC_TO_EXPIRE=$(( ${CERT_MIN_DAYS_TO_EXPIRE} * 3600 * 24 ))
-  openssl x509 -noout -in "${CERT_PATH}/live/${CERT_NAME}/fullchain.pem" -checkend ${CERT_MIN_SEC_TO_EXPIRE} > /dev/null
-  if [ "$?" = "1" ] ; then
-    echo "Renewing the certificate for ${cert_domains}"
-    create_selfsigned_cert
-  fi
 }
--- certmgr/scripts/selfsigned.sh
+++ certmgr/scripts/selfsigned.sh
@@ -1,5 +1,32 @@
 #! /bin/bash
 
+create_selfsigned_cert() {
+  DEST_DIR="${CERT_PATH}/live/${CERT_NAME}"
+  mkdir -p ${DEST_DIR}
+
+  cd ${DEST_DIR}
+  openssl req -x509 -nodes -newkey rsa:4096 -days ${CERT_SELF_SIGNED_EXPIRE} -keyout privkey.pem -out fullchain.pem -subj '/CN=localhost'
+  if [ "$CERT_VERBOSE" = "1" ] ; then
+    echo "Created certificate in ${CERT_PATH} for ${cert_domains}"
+    echo "Expires: "`openssl x509 -in fullchain.pem -noout -enddate`
+  fi
+}
+
+
+renew_selfsigned_cert()
+{
+  if [ "$CERT_VERBOSE" = "1" ] ; then
+    echo "Checking for renewal of ${CERT_PATH}/live/${CERT_NAME}/fullchain.pem"
+  fi
+
+  CERT_MIN_SEC_TO_EXPIRE=$(( ${CERT_MIN_DAYS_TO_EXPIRE} * 3600 * 24 ))
+  openssl x509 -noout -in "${CERT_PATH}/live/${CERT_NAME}/fullchain.pem" -checkend ${CERT_MIN_SEC_TO_EXPIRE} > /dev/null
+  if [ "$?" = "1" ] ; then
+    echo "Renewing the certificate for ${cert_domains}"
+    create_selfsigned_cert
+  fi
+}
+
 # Source in util.sh so we can have our nice tools
 . $(cd $(dirname $0); pwd)/func.sh
 
@@ -20,7 +47,7 @@ fi
 
 if [ "$CERT_CREATE_ONLY" = "0" ] ; then
   while true; do
-    check_renewal
+    renew_selfsigned_cert
     sleep 60 &
     wait $!
 
