ce083a58ef21271ce465a79de0267f99a7000d9d
imnasnainaec <imnasnainaec@gmail.com>
2020-11-04 10:14:03 -0500

    On login, only fetch avatar if user.hasAvatar

--- src/components/Login/LoginActions.tsx
+++ src/components/Login/LoginActions.tsx
@@ -1,6 +1,6 @@
 import { Dispatch } from "react";
-import { ThunkAction } from "redux-thunk";
 import { AnyAction } from "redux";
+import { ThunkAction } from "redux-thunk";
 
 import * as backend from "../../backend";
 import * as LocalStorage from "../../backend/localStorage";
@@ -65,16 +65,14 @@ export function asyncLogin(username: string, password: string) {
       .then(async (user: User) => {
         LocalStorage.setCurrentUser(user);
         dispatch(loginSuccess(user.username));
-        try {
-          const avatar: string = await backend.avatarSrc(user.id);
-          LocalStorage.setAvatar(avatar);
-        } catch (e) {
-          LocalStorage.setAvatar("");
+        if (user.hasAvatar) {
+          backend.avatarSrc(user.id).then((avatar) => {
+            LocalStorage.setAvatar(avatar);
+          });
         }
-
         history.push("/");
       })
-      .catch((err) => {
+      .catch(() => {
         dispatch(loginFailure(username));
       });
   };
