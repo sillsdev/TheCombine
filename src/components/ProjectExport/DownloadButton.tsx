be8bc2d22216ad358523da1ebcf628c26905f1ca
imnasnainaec <imnasnainaec@gmail.com>
2020-11-20 16:20:04 -0500

    Don't reuse global var name locally

--- src/components/ProjectExport/DownloadButton.tsx
+++ src/components/ProjectExport/DownloadButton.tsx
@@ -35,9 +35,9 @@ export default function DownloadButton() {
     const projectName = await getProjectName(exportState.projectId);
     setFileName(`${projectName}_${getNowDateTimeString()}.zip`);
     asyncDownloadExport(exportState.projectId)(dispatch)
-      .then((fileUrl) => {
-        if (fileUrl) {
-          setFileUrl(fileUrl);
+      .then((url) => {
+        if (url) {
+          setFileUrl(url);
           reset();
         }
       })
