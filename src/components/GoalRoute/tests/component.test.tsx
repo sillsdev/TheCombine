353c8129415cbde4b4cbf51717cf5c3dfd31270f
D. Ror <imnasnainaec@gmail.com>
2020-12-03 12:51:17 -0500

    Update GoalRoute testing.

--- src/components/GoalRoute/tests/component.test.tsx
+++ src/components/GoalRoute/tests/component.test.tsx
@@ -1,27 +1,19 @@
+import "jest-canvas-mock";
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router";
-import configureMockStore from "redux-mock-store";
+import { MemoryRouter } from "react-router-dom";
+import renderer from "react-test-renderer";
 
 import { Path } from "../../../history";
-import { defaultState } from "../../App/DefaultState";
 import GoalRoute from "../component";
 
-const createMockStore = configureMockStore([]);
-
-it("renders without crashing", () => {
-  const mockStore = createMockStore({
-    ...defaultState,
+describe("GoalRoute", () => {
+  it("renders without crashing", () => {
+    renderer.act(() => {
+      renderer.create(
+        <MemoryRouter initialEntries={[Path.Goals]} initialIndex={0}>
+          <GoalRoute />
+        </MemoryRouter>
+      );
+    });
   });
-  const div = document.createElement("div");
-  ReactDOM.render(
-    <Provider store={mockStore}>
-      <MemoryRouter initialEntries={[Path.Goals]} initialIndex={0}>
-        <GoalRoute />
-      </MemoryRouter>
-    </Provider>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
 });
