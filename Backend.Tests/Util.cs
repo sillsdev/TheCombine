2197dd8e24dca0bc9d9b1cb73d26a528571c61d2
johnthagen <johnthagen@gmail.com>
2020-12-04 16:00:53 -0500

    Fix typo

--- Backend.Tests/Util.cs
+++ Backend.Tests/Util.cs
@@ -6,7 +6,7 @@ namespace Backend.Tests
 {
     public static class Util
     {
-        /// <summary> Path to Asserts directory from debugging folder </summary>
+        /// <summary> Path to Assets directory from debugging folder </summary>
         public static readonly string AssetsDir = Path.Combine(Directory.GetParent(Directory.GetParent(
             Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()).ToString(), "Assets");
 
