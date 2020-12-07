4f3c73e500b2435e06ffed030b41121612eb049a
Jim Grady <jimgrady.jg@gmail.com>
2020-12-07 18:55:51 -0500

    Fix S3 URI when uploading certificates

--- certmgr/scripts/aws.py
+++ certmgr/scripts/aws.py
@@ -7,7 +7,13 @@ from utils import get_setting
 
 
 def aws_s3_put(src: Path, dest: str) -> bool:
-    """Push a file to the configured AWS S3 Bucket."""
+    """
+    Push a file to the configured AWS S3 Bucket.
+
+    NOTE: "dest" needs to be relative to the certs folder in the AWS bucket
+    that is configured for the container.  aws_s3_put will add the bucket
+    information.
+    """
     aws_s3_bucket = get_setting("AWS_S3_CERT_LOC")
     aws_s3_profile = get_setting("AWS_S3_PROFILE")
     aws_s3_uri = f"s3://{aws_s3_bucket}/{dest}"
@@ -16,7 +22,13 @@ def aws_s3_put(src: Path, dest: str) -> bool:
 
 
 def aws_s3_get(src: str, dest: Path) -> bool:
-    """Get a file from the configured AWS S3 Bucket."""
+    """
+    Get a file from the configured AWS S3 Bucket.
+
+    NOTE: "src" needs to be relative to the certs folder in the AWS bucket
+    that is configured for the container.  aws_s3_get will add the bucket
+    information.
+    """
     aws_s3_bucket = get_setting("AWS_S3_CERT_LOC")
     aws_s3_profile = get_setting("AWS_S3_PROFILE")
     aws_s3_uri = f"s3://{aws_s3_bucket}/{src}"
@@ -27,9 +39,8 @@ def aws_s3_get(src: str, dest: Path) -> bool:
 def aws_push_certs() -> None:
     """Push all proxy certificates to AWS S3."""
     cert_file_list = ("cert.pem", "chain.pem", "fullchain.pem", "privkey.pem")
-    s3_bucket = get_setting("AWS_S3_CERT_LOC")
     domain_list: List[str] = get_setting("CERT_PROXY_DOMAINS").split()
     for domain in domain_list:
         cert_dir = Path(f"/etc/letsencrypt/live/{domain}")
         for cert_file in cert_file_list:
-            aws_s3_put(Path(f"{cert_dir}/{cert_file}"), f"s3://{s3_bucket}/{domain}/{cert_file}")
+            aws_s3_put(Path(f"{cert_dir}/{cert_file}"), f"{domain}/{cert_file}")
