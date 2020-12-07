c58932739b55802c5faced8c78f63aea2b33d84b
D. Ror <imnasnainaec@gmail.com>
2020-12-07 17:07:57 -0500

    Update.

--- src/goals/ReviewEntries/ReviewEntriesComponent/tests/ReviewEntriesComponent.test.tsx
+++ src/goals/ReviewEntries/ReviewEntriesComponent/tests/ReviewEntriesComponent.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
 import configureMockStore from "redux-mock-store";
 
 import * as utilities from "../../../../utilities";
@@ -74,22 +74,20 @@ beforeAll(() => {
       MOCK_UUID.mockImplementationOnce(() => sense.senseId);
   }
 
-  // Create + mount + unmount
-  const div = document.createElement("div");
-  ReactDOM.render(
-    <Provider store={mockStore}>
-      <ReviewEntriesConnected
-        words={mockWords}
-        language="en"
-        setAnalysisLanguage={jest.fn()}
-        clearState={jest.fn()}
-        updateAllWords={MOCK_UPDATE}
-        updateFrontierWord={jest.fn()}
-      />
-    </Provider>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+  renderer.act(() => {
+    renderer.create(
+      <Provider store={mockStore}>
+        <ReviewEntriesConnected
+          words={mockWords}
+          language="en"
+          setAnalysisLanguage={jest.fn()}
+          clearState={jest.fn()}
+          updateAllWords={MOCK_UPDATE}
+          updateFrontierWord={jest.fn()}
+        />
+      </Provider>
+    );
+  });
 });
 
 describe("ReviewEntriesComponent", () => {
