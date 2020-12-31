f50445639f69bd700043fb010f638d364d1a1fa9
johnthagen <johnthagen@gmail.com>
2020-12-31 10:39:33 -0500

    Add unit tests for SanitizeId

--- Backend.Tests/Helper/Sanitization.cs
+++ Backend.Tests/Helper/Sanitization.cs
@@ -7,6 +7,49 @@ namespace Backend.Tests.Helper
 {
     public class SanitizationTests
     {
+        private static List<string> _validIds = new List<string>
+        {
+            "a",
+            "1",
+            "a-5",
+        };
+
+        [TestCaseSource(nameof(_validIds))]
+        public void TestValidIds(string id)
+        {
+            Assert.That(Sanitization.SanitizeId(id));
+        }
+
+        private static List<string> _invalidIds = new List<string>
+        {
+            "_",
+            "a_1",
+            ".",
+            "a.1",
+            "/",
+            "\\",
+            "../a",
+            "..\\a",
+            "../1",
+            "..\\1",
+            "!",
+            "@",
+            "#",
+            "$",
+            "%",
+            "^",
+            "&",
+            "*",
+            "(",
+            ")",
+            "+"
+        };
+        [TestCaseSource(nameof(_invalidIds))]
+        public void TestInvalidIds(string id)
+        {
+            Assert.False(Sanitization.SanitizeId(id));
+        }
+
         private static List<string> _validFileNames = new List<string>
         {
             "a",
