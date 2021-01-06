64e772bd321579377593deed46547f95794a6068
D. Ror <imnasnainaec@gmail.com>
2021-01-06 14:21:55 -0500

    Cleanup test file.

--- src/goals/MergeDupGoal/DuplicateFinder/tests/DuplicateFinder.test.ts
+++ src/goals/MergeDupGoal/DuplicateFinder/tests/DuplicateFinder.test.ts
@@ -3,7 +3,6 @@
 import {
   simpleWord,
   testWordList as mockTestWordList,
-  Word,
 } from "../../../../types/word";
 import DupFinder, { DefaultParams } from "../DuplicateFinder";
 
@@ -15,9 +14,7 @@ jest.mock("../../../../backend", () => {
   };
 });
 
-describe("dupFinder Tests", () => {
-  //TEST UTILITIES
-
+describe("dupFinder", () => {
   test("getNextDups returns correct number of word collections", async () => {
     let finder = new DupFinder();
 
@@ -26,7 +23,7 @@ describe("dupFinder Tests", () => {
     });
   });
 
-  test("finder can get words from frontier", async () => {
+  test("fetchWordsFromDB can get words from frontier", async () => {
     let finder = new DupFinder();
 
     expect(finder.maskedWords.length).toBe(0);
@@ -37,7 +34,7 @@ describe("dupFinder Tests", () => {
     });
   });
 
-  test("the finder can search for duplicates with one parent", async () => {
+  test("getDuplicatesOfWord search for duplicates with one parent", async () => {
     const finder = new DupFinder();
 
     const parent = simpleWord("Yank", "Mayonnaise");
@@ -65,48 +62,46 @@ describe("dupFinder Tests", () => {
     });
   });
 
-  test("Levenshtein Distance with same Word", () => {
-    let finder = new DupFinder();
-    expect(finder.getLevenshteinDistance("testing", "testing")).toEqual(0);
-  });
+  describe("getLevenshteinDistance", () => {
+    test("with same Word", () => {
+      let finder = new DupFinder();
+      expect(finder.getLevenshteinDistance("testing", "testing")).toEqual(0);
+    });
 
-  test("Levenshtein Distance with similar Word", () => {
-    let finder = new DupFinder();
-    //one insertion, one substitution
-    expect(finder.getLevenshteinDistance("testing", "toasting")).toEqual(3);
-  });
+    test("with similar Word, one insertion and one substitution", () => {
+      let finder = new DupFinder();
+      expect(finder.getLevenshteinDistance("testing", "toasting")).toEqual(3);
+    });
 
-  test("Levenshtein Distance with single substitution", () => {
-    let finder = new DupFinder();
-    //one insertion, one substitution
-    expect(finder.getLevenshteinDistance("testing", "tasting")).toEqual(
-      DefaultParams.subCost
-    );
-  });
+    test("with single substitution", () => {
+      let finder = new DupFinder();
+      expect(finder.getLevenshteinDistance("testing", "tasting")).toEqual(
+        DefaultParams.subCost
+      );
+    });
 
-  test("Levenshtein Distance with single deletion", () => {
-    let finder = new DupFinder();
-    //one insertion, one substitution
-    expect(finder.getLevenshteinDistance("testin", "testing")).toEqual(
-      DefaultParams.delCost
-    );
-  });
+    test("with single deletion", () => {
+      const finder = new DupFinder();
+      expect(finder.getLevenshteinDistance("testin", "testing")).toEqual(
+        DefaultParams.delCost
+      );
+    });
 
-  test("Levenshtein Distance with single addition", () => {
-    let finder = new DupFinder();
-    //one insertion, one substitution
-    expect(finder.getLevenshteinDistance("testing", "testin")).toEqual(
-      DefaultParams.insCost
-    );
-  });
+    test("with single addition", () => {
+      const finder = new DupFinder();
+      expect(finder.getLevenshteinDistance("testing", "testin")).toEqual(
+        DefaultParams.insCost
+      );
+    });
 
-  test("Levenshtein Distance with distinct Word", () => {
-    let finder = new DupFinder();
-    expect(
-      finder.getLevenshteinDistance(
-        "badger",
-        "testWordList[12].senses[0].glosses[0].def"
-      )
-    ).toEqual(39);
+    test("with distinct Word", () => {
+      const finder = new DupFinder();
+      expect(
+        finder.getLevenshteinDistance(
+          "badger",
+          "testWordList[12].senses[0].glosses[0].def"
+        )
+      ).toEqual(39);
+    });
   });
 });
