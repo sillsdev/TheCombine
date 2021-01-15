09edbb2bb09a5683d01e3651c623161e042a5d02
D. Ror <imnasnainaec@gmail.com>
2021-01-15 13:15:15 -0500

    GoalTimeline, Login, Temp, TreeView: use StoreStateDispatch type.

--- src/components/Temp/index.tsx
+++ src/components/Temp/index.tsx
@@ -1,9 +1,9 @@
-import Temp from "./TempComponent";
-import * as actions from "./TempActions";
-import { StoreState } from "../../types";
-
 import { connect } from "react-redux";
-import { ThunkDispatch } from "redux-thunk";
+
+import { StoreState } from "../../types";
+import { StoreStateDispatch } from "../../types/actions";
+import * as actions from "./TempActions";
+import Temp from "./TempComponent";
 
 //Temp Container Component
 
@@ -13,9 +13,7 @@ export function mapStateToProps(state: StoreState) {
   };
 }
 
-export function mapDispatchToProps(
-  dispatch: ThunkDispatch<StoreState, any, actions.TempAction>
-) {
+export function mapDispatchToProps(dispatch: StoreStateDispatch) {
   return {
     buttonClicked: () => {
       //console.log('clicked test!');
