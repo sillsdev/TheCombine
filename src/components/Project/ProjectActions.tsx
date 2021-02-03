77d649a0332bc2138ce29f26aa9ba9c012dddead
D. Ror <imnasnainaec@gmail.com>
2021-02-03 09:48:22 -0500

    fix @typescript-eslint/no-redeclare

--- src/components/Project/ProjectActions.tsx
+++ src/components/Project/ProjectActions.tsx
@@ -3,16 +3,13 @@ import { StoreStateDispatch } from "types/actions";
 import { Project } from "types/project";
 
 export const SET_CURRENT_PROJECT = "SET_CURRENT_PROJECT";
-export type SET_CURRENT_PROJECT = typeof SET_CURRENT_PROJECT;
 
-type ProjectType = SET_CURRENT_PROJECT;
-
-//action types
-
-export interface ProjectAction {
-  type: ProjectType;
+export interface SetCurrentProject {
+  type: typeof SET_CURRENT_PROJECT;
   payload: Project;
 }
+
+export type ProjectAction = SetCurrentProject;
 
 export function setCurrentProject(payload: Project): ProjectAction {
   return {
