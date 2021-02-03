24dab30b3d6c63c3f53ac85149ec75cac3cde634
D. Ror <imnasnainaec@gmail.com>
2021-02-03 09:52:52 -0500

    Improve for potential future expansion.

--- src/components/Project/ProjectActions.tsx
+++ src/components/Project/ProjectActions.tsx
@@ -4,12 +4,12 @@ import { Project } from "types/project";
 
 export const SET_CURRENT_PROJECT = "SET_CURRENT_PROJECT";
 
-export interface SetCurrentProject {
-  type: typeof SET_CURRENT_PROJECT;
+type ProjectActionType = typeof SET_CURRENT_PROJECT;
+
+export interface ProjectAction {
+  type: ProjectActionType;
   payload: Project;
 }
-
-export type ProjectAction = SetCurrentProject;
 
 export function setCurrentProject(payload: Project): ProjectAction {
   return {
