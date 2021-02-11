fbadf4a0d431a47337a7be1b2a306f6504b4cbad
D. Ror <imnasnainaec@gmail.com>
2021-02-11 16:20:10 -0500

    CharInv refactor: implement function to compute changes, fix non-distinct keys error, fix sort menu icons, etc.

--- src/goals/CharInventoryCreation/components/CharacterDetail/CharacterStatusControl/index.tsx
+++ src/goals/CharInventoryCreation/components/CharacterDetail/CharacterStatusControl/index.tsx
@@ -3,17 +3,18 @@ import { connect } from "react-redux";
 import { StoreStateDispatch } from "types/actions";
 import { setCharacterStatus } from "goals/CharInventoryCreation/CharacterInventoryActions";
 import CharacterStatusControl from "goals/CharInventoryCreation/components/CharacterDetail/CharacterStatusControl/CharacterStatusControl";
+import { CharacterStatus } from "goals/CharInventoryCreation/CharacterInventoryReducer";
 
 function mapDispatchToProps(dispatch: StoreStateDispatch) {
   return {
     accept: (character: string) => {
-      dispatch(setCharacterStatus(character, "accepted"));
+      dispatch(setCharacterStatus(character, CharacterStatus.Accepted));
     },
     reject: (character: string) => {
-      dispatch(setCharacterStatus(character, "rejected"));
+      dispatch(setCharacterStatus(character, CharacterStatus.Rejected));
     },
     unset: (character: string) => {
-      dispatch(setCharacterStatus(character, "undecided"));
+      dispatch(setCharacterStatus(character, CharacterStatus.Undecided));
     },
   };
 }
