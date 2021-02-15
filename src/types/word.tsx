sillsdev/TheCombine

commit f4e86d719c7b0e7c38dcb003d6485aa3d21bc480

Fix frontend Word and Project data types to match the backend (#1019)

src/types/word.tsx
@@ -16,8 +16,11 @@ export interface Gloss {
 export interface SemanticDomain {
   name: string;
   id: string;
+  // TODO: The backend also stores a description field. Should that be
+  //    exposed?
 }
 export class Sense {
+  guid?: string;
   glosses: Gloss[];
   semanticDomains: SemanticDomain[] = [];
   accessibility?: State;
@@ -31,8 +34,8 @@ export class Sense {
 }
 
 export class Note {
-  text: string;
   language: string; // bcp-47 code
+  text: string;
 
   constructor(text: string = "", lang: string = "") {
     this.text = text;
@@ -42,27 +45,25 @@ export class Note {
 
 export class Word {
   id: string = "";
+  guid: string = "";
   vernacular: string = "";
+  plural: string = "";
   senses: Sense[] = [];
   audio: string[] = [];
   created: string = "";
   modified: string = "";
+  accessibility: string = "";
   history: string[] = [];
   partOfSpeech: string = "";
   editedBy: string[] = [];
   otherField: string = "";
-  plural: string = "";
+  projectId: string = "";
   note: Note = new Note();
 }
 
 export interface MergeWord {
   wordID: string;
   senses: State[];
-}
-
-export interface MergeWords {
-  parent: Word;
-  children: MergeWord[];
 }
 
 //used in ExistingDataTable

src/types/project.tsx
@@ -21,24 +21,22 @@ export interface Project {
   isActive: boolean;
   liftImported: boolean;
   semanticDomains: SemanticDomain[];
-  userRoles: string;
   vernacularWritingSystem: WritingSystem;
   analysisWritingSystems: WritingSystem[];
   validCharacters: string[];
   rejectedCharacters: string[];
+  autocompleteSetting: AutoComplete;
+  customFields: CustomField[];
   wordFields: string[];
   partsOfSpeech: string[];
-  customFields: CustomField[];
-  autocompleteSetting: AutoComplete;
 }
 
-export const defaultProject = {
+export const defaultProject: Project = {
   id: "",
   name: "",
   isActive: true,
   liftImported: false,
   semanticDomains: [],
-  userRoles: "",
   vernacularWritingSystem: { name: "", bcp47: "", font: "" },
   analysisWritingSystems: [{ name: "", bcp47: "", font: "" }],
   validCharacters: [],
@@ -47,7 +45,7 @@ export const defaultProject = {
   wordFields: [],
   partsOfSpeech: [],
   autocompleteSetting: AutoComplete.On,
-} as Project;
+};
 
 // Randomize properties as needed for tests.
 export function randomProject(): Project {

commit eb5b9159c066319d090edc9acb09f8112de37590

Fix type mismatch between frontend and backend. (#1023)
Fix for bug introduced in #1019 preventing new words from being created.

src/types/word.tsx
@@ -52,7 +52,7 @@ export class Word {
   audio: string[] = [];
   created: string = "";
   modified: string = "";
-  accessibility: string = "";
+  accessibility: State = State.Active;
   history: string[] = [];
   partOfSpeech: string = "";
   editedBy: string[] = [];