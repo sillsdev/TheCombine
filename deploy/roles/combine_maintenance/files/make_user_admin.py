06796fef574da43cf0bfe89c956fb107b40cc981
Jim Grady <jimgrady.jg@gmail.com>
2021-02-17 17:45:02 -0500

    Add stub script to make user a site admino

---
+++ deploy/roles/combine_maintenance/files/make_user_admin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Make a user a site administrator."""
+
+
+def main():
+    """Make a user a site administrator."""
+    print("Make admin")
+
+
+if __name__ == "__main__":
+    main()
