29262730b2360da41a1d31f0f99c7e29f4a9a1b3
D. Ror <imnasnainaec@gmail.com>
2021-03-05 10:08:35 -0500

    Use InsertManyAsync() on import to reduce connections to mongo. (#1054)

--- Backend/Interfaces/ILiftService.cs
+++ Backend/Interfaces/ILiftService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using BackendFramework.Models;
 using SIL.Lift.Parsing;
 
@@ -6,16 +7,21 @@ namespace BackendFramework.Interfaces
 {
     public interface ILiftService
     {
-        ILexiconMerger<LiftObject, LiftEntry, LiftSense, LiftExample> GetLiftImporterExporter(
+        ILiftMerger GetLiftImporterExporter(
             string projectId, IProjectService projectService, IWordRepository wordRepo);
         void LdmlImport(string filePath, string langTag, IProjectService projectService, Project project);
         Task<string> LiftExport(string projectId, IWordRepository wordRepo, IProjectService projectService);
 
-        // Methods to store, retrieve, and delete an export string in a common dictionary
+        // Methods to store, retrieve, and delete an export string in a common dictionary.
         void StoreExport(string key, string filePath);
         string? RetrieveExport(string key);
         bool DeleteExport(string key);
         void SetExportInProgress(string key, bool isInProgress);
         bool IsExportInProgress(string key);
+    }
+
+    public interface ILiftMerger : ILexiconMerger<LiftObject, LiftEntry, LiftSense, LiftExample>
+    {
+        Task<List<Word>> SaveImportEntries();
     }
 }
--- Backend/Services/LiftApiServices.cs
+++ Backend/Services/LiftApiServices.cs
@@ -474,7 +474,7 @@ namespace BackendFramework.Services
             return SecurityElement.Escape(sInput);
         }
 
-        public ILexiconMerger<LiftObject, LiftEntry, LiftSense, LiftExample> GetLiftImporterExporter(
+        public ILiftMerger GetLiftImporterExporter(
             string projectId, IProjectService projectService, IWordRepository wordRepo)
         {
             return new LiftMerger(projectId, projectService, wordRepo);
@@ -511,12 +511,12 @@ namespace BackendFramework.Services
             liftRangesWriter.WriteEndElement(); //end range element
         }
 
-
-        private sealed class LiftMerger : ILexiconMerger<LiftObject, LiftEntry, LiftSense, LiftExample>
+        private sealed class LiftMerger : ILiftMerger
         {
             private readonly string _projectId;
             private readonly IProjectService _projectService;
             private readonly IWordRepository _wordRepo;
+            private readonly List<Word> _importEntries = new List<Word>();
 
             public LiftMerger(string projectId, IProjectService projectService, IWordRepository wordRepo)
             {
@@ -526,9 +526,23 @@ namespace BackendFramework.Services
             }
 
             /// <summary>
-            /// The meat of lift import is done here.
-            /// This reads in all necessary attributes of a word and adds it to the database.
+            /// <see cref="Word"/>s are added to the private field <see cref="_importEntries"/>
+            /// during lift import. This saves the contents of _importEntries to the database.
             /// </summary>
+            /// <returns> The words saved. </returns>
+            public async Task<List<Word>> SaveImportEntries()
+            {
+                var savedWords = new List<Word>(await _wordRepo.Create(_importEntries));
+                _importEntries.Clear();
+                return savedWords;
+            }
+
+            /// <summary>
+            /// A significant portion of lift import is done here. This reads in all necessary
+            /// attributes to create a word from <paramref name="entry"/>, adding the result
+            /// to <see cref="_importEntries"/> to be saved to the database.
+            /// </summary>
+            /// <param name="entry"></param>
             public async void FinishEntry(LiftEntry entry)
             {
                 var newWord = new Word
@@ -616,7 +630,8 @@ namespace BackendFramework.Services
                     {
                         // Splits on the space between the number and name of the semantic domain
                         var splitSemDom = semanticDomainString.Split(" ", 2);
-                        newSense.SemanticDomains.Add(new SemanticDomain { Id = splitSemDom[0], Name = splitSemDom[1] });
+                        newSense.SemanticDomains.Add(
+                            new SemanticDomain { Id = splitSemDom[0], Name = splitSemDom[1] });
                     }
 
                     newWord.Senses.Add(newSense);
@@ -652,7 +667,7 @@ namespace BackendFramework.Services
                 }
 
                 newWord.ProjectId = _projectId;
-                await _wordRepo.Create(newWord);
+                _importEntries.Add(newWord);
             }
 
             /// <summary> Creates the object to transfer all the data from a word </summary>
