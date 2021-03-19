bba6a19c29e0954ac0d4f225494d36152ebe8765
Jim Grady <jimgrady.jg@gmail.com>
2021-03-19 08:56:13 -0400

    Untrack test.py

--- deploy/roles/combine_maintenance/files/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from combine_env import get_setting, print_step
-
-print(f"Verbose is set to {get_setting('VERBOSE')}")
-print_step("The first step.")
-print_step("The second step.")
-print_step("The third step.")
