eca2d1c06f676de5ab10fab39db01b65e172c1f0
D. Ror <imnasnainaec@gmail.com>
2021-03-26 14:06:55 -0400

    Add depenencies to prevent perpetual rerender console error when saving CharInv changes.

--- src/goals/DefaultGoal/BaseGoalScreen.tsx
+++ src/goals/DefaultGoal/BaseGoalScreen.tsx
@@ -41,7 +41,7 @@ export default function BaseGoalScreen() {
       dispatch(setCurrentGoal());
       dispatch(clearTree());
     };
-  });
+  }, [dispatch, setCurrentGoal, clearTree]);
   return (
     <React.Fragment>
       <DisplayProgress />
