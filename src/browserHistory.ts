163f6d2193431c5f39718f53905eed823c77de50
Jason Naylor <jason_naylor@sil.org>
2021-04-13 14:02:20 -0700

    Add segment.com analytics tracking to The Combine

--- src/browserHistory.ts
+++ src/browserHistory.ts
@@ -8,6 +8,17 @@ import { createBrowserHistory } from "history";
  * information.
  */
 let history = createBrowserHistory();
+// set up analytics for page navigation
+let prevPath = "";
+history.listen((location) => {
+  if (location.pathname !== prevPath) {
+    analytics.track("navigate", {
+      source: prevPath,
+      destination: location.pathname,
+    });
+    prevPath = location.pathname;
+  }
+});
 export default history;
 
 export enum Path {
