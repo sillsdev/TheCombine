ddf7b645fa5df3c410d0b56325a7db441a0b13b2
Jim Grady <jimgrady.jg@gmail.com>
2021-04-14 17:21:58 -0400

    Backout check for empty analytics key

--- src/components/Login/LoginActions.tsx
+++ src/components/Login/LoginActions.tsx
@@ -46,14 +46,12 @@ export function asyncLogin(username: string, password: string) {
       .then(async (user: User) => {
         LocalStorage.setCurrentUser(user);
         dispatch(loginSuccess(user.username));
-        if (analytics.invoked) {
-          // hash the user name and use it in analytics.identify
-          const analyticsId = hash
-            .createHash("sha256")
-            .update(user.id)
-            .digest("hex");
-          analytics.identify(analyticsId);
-        }
+        // hash the user name and use it in analytics.identify
+        const analyticsId = hash
+          .createHash("sha256")
+          .update(user.id)
+          .digest("hex");
+        analytics.identify(analyticsId);
         if (user.hasAvatar) {
           backend.avatarSrc(user.id).then((avatar) => {
             LocalStorage.setAvatar(avatar);
