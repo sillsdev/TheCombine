c296b441d926ca81bd2454cdcd511af770d2f6cc
D. Ror <imnasnainaec@gmail.com>
2021-04-23 14:55:59 -0400

    MergeBlacklist collection, context, model.

---
+++ Backend/Models/MergeBlacklistEntry.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BackendFramework.Models
+{
+    public class MergeBlacklistEntry
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("projectId")]
+        public readonly string ProjectId;
+
+        [BsonElement("userId")]
+        public string UserId { get; set; }
+
+        [BsonElement("wordIds")]
+        public List<string> WordIds { get; set; }
+
+        public MergeBlacklistEntry(string projectId, string userId, List<string> wordIds)
+        {
+            Id = "";
+            ProjectId = projectId;
+            UserId = userId;
+            WordIds = wordIds;
+        }
+
+        public MergeBlacklistEntry Clone()
+        {
+            var ClonedWordIds = new List<string>();
+            foreach (var id in WordIds)
+            {
+                ClonedWordIds.Add((string)id.Clone());
+            }
+            return new MergeBlacklistEntry(ProjectId, UserId, ClonedWordIds) { Id = (string)Id.Clone() };
+        }
+    }
+}
