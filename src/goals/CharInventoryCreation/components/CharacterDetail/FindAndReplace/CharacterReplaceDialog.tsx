c632c32871887ac950c7b0fde485218b08bca7f7
JacobHaimes <42851286+JacobHaimes@users.noreply.github.com>
2021-06-24 18:44:21 -0400

    Created warning box when finding and reaplacing characters

---
+++ src/goals/CharInventoryCreation/components/CharacterDetail/FindAndReplace/CharacterReplaceDialog.tsx
@@ -0,0 +1,54 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import { Translate } from "react-localize-redux";
+
+interface ReplaceDialogProps {
+  open: boolean;
+  textId?: string;
+  dialogFindValue: string;
+  dialogReplaceValue: string;
+  handleAccept: () => void;
+  handleCancel: () => void;
+}
+
+/**
+ * Dialog to confirm replacement
+ */
+export default function CharacterReplaceDialog(props: ReplaceDialogProps) {
+  return (
+    <Dialog
+      open={props.open}
+      onClose={props.handleCancel}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        <Translate id="buttons.proceedWithCaution" />
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Replace all occurrences of "<strong>{props.dialogFindValue}</strong>"
+          with "<strong>{props.dialogReplaceValue}</strong>" ?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={props.handleCancel} variant="outlined" color="primary">
+          <Translate id="buttons.cancel" />
+        </Button>
+        <Button
+          onClick={props.handleAccept}
+          variant="contained"
+          color="primary"
+        >
+          <Translate id="buttons.confirm" />
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
