9c74a42f1d8353cdc86780e751548aea113ff5f4
jzwu <jzwu@uw.edu>
2021-07-14 14:25:14 -0700

    Implement adding/removing admin privileges from users

--- src/components/ProjectSettings/ProjectUsers/ActiveUsers.tsx
+++ src/components/ProjectSettings/ProjectUsers/ActiveUsers.tsx
@@ -2,27 +2,35 @@ import {
   Avatar,
   FormControl,
   IconButton,
-  IconButtonProps,
   InputLabel,
   List,
   ListItem,
   ListItemProps,
   ListItemText,
+  Menu,
   MenuItem,
+  MenuItemProps,
   Select,
+  Tooltip,
 } from "@material-ui/core";
 import DeleteDialog from "components/Buttons/DeleteDialog";
+import MakeAdminDialog from "components/Buttons/MakeAdminDialog";
+import RemoveAdminDialog from "components/Buttons/RemoveAdminDialog";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+
 import { toast } from "react-toastify";
 //styles the ToastContainer so that it appears on the upper right corner with the message.
 import "react-toastify/dist/ReactToastify.min.css";
 import { Translate } from "react-localize-redux";
-import React from "react";
+import React, { ElementType } from "react";
 
-import { Permission, Project, User } from "api/models";
+import { Permission, Project, User, UserRole } from "api/models";
 import {
+  addOrUpdateUserRole,
   avatarSrc,
   getAllUsersInCurrentProject,
+  getUserRoles,
   removeUserRole,
 } from "backend";
 import theme from "types/theme";
@@ -38,9 +46,13 @@ interface UserProps {
 
 interface UserState {
   projUsers: User[];
+  projUserRoles: UserRole[];
   userAvatar: { [key: string]: string };
   userOrder: UserOrder;
-  dialogOpen: boolean;
+  removeUserDialogOpen: boolean;
+  makeAdminDialogOpen: boolean;
+  removeAdminDialogOpen: boolean;
+  anchorEl: Element | undefined;
 }
 
 export default class ActiveUsers extends React.Component<UserProps, UserState> {
@@ -48,9 +60,13 @@ export default class ActiveUsers extends React.Component<UserProps, UserState> {
     super(props);
     this.state = {
       projUsers: [],
+      projUserRoles: [],
       userAvatar: {},
       userOrder: UserOrder.Username,
-      dialogOpen: false,
+      removeUserDialogOpen: false,
+      makeAdminDialogOpen: false,
+      removeAdminDialogOpen: false,
+      anchorEl: undefined,
     };
   }
 
@@ -68,9 +84,12 @@ export default class ActiveUsers extends React.Component<UserProps, UserState> {
   private removeUser() {
     removeUserRole(
       [Permission.DeleteEditSettingsAndUsers],
-      (arguments[0] as unknown) as string
+      arguments[0] as string
     )
-      .then(this.handleClose.bind(this))
+      .then(this.handleRemoveUserDialogClose.bind(this))
+      .then(() => {
+        this.setState({ anchorEl: undefined });
+      })
       .then(() => {
         toast(<Translate id="projectSettings.invite.toastSuccess" />);
       })
@@ -80,7 +99,51 @@ export default class ActiveUsers extends React.Component<UserProps, UserState> {
       });
   }
 
-  private populateUsers() {
+  // TODO: Add the correct text for toast
+  private makeAdmin() {
+    addOrUpdateUserRole(
+      [
+        Permission.WordEntry,
+        Permission.Unused,
+        Permission.MergeAndCharSet,
+        Permission.ImportExport,
+        Permission.DeleteEditSettingsAndUsers,
+      ],
+      arguments[0] as string
+    )
+      .then(this.handleMakeAdminDialogClose.bind(this))
+      .then(() => {
+        this.setState({ anchorEl: undefined });
+      })
+      .then(() => {
+        toast(<Translate id="projectSettings.invite.toastSuccess" />);
+      })
+      .catch((err: string) => {
+        console.log(err);
+        toast(<Translate id="projectSettings.invite.toastFail" />);
+      });
+  }
+
+  // TODO: Add the correct text for toast
+  private removeAdmin() {
+    addOrUpdateUserRole(
+      [Permission.MergeAndCharSet, Permission.Unused, Permission.WordEntry],
+      arguments[0] as string
+    )
+      .then(this.handleRemoveAdminDialogClose.bind(this))
+      .then(() => {
+        this.setState({ anchorEl: undefined });
+      })
+      .then(() => {
+        toast(<Translate id="projectSettings.invite.toastSuccess" />);
+      })
+      .catch((err: string) => {
+        console.log(err);
+        toast(<Translate id="projectSettings.invite.toastFail" />);
+      });
+  }
+
+  private async populateUsers() {
     console.log("populated users");
     getAllUsersInCurrentProject()
       .then(async (projUsers) => {
@@ -94,6 +157,9 @@ export default class ActiveUsers extends React.Component<UserProps, UserState> {
         await Promise.all(promises);
         this.setState({ userAvatar });
       })
+      .catch((err) => console.error(err));
+    getUserRoles()
+      .then((projUserRoles) => this.setState({ projUserRoles }))
       .catch((err) => console.error(err));
   }
 
@@ -114,17 +180,49 @@ export default class ActiveUsers extends React.Component<UserProps, UserState> {
     });
   }
 
-  private handleOpen() {
-    this.setState({ dialogOpen: true });
+  private isProjectAdmin(userRoleId: string): boolean {
+    console.log(this.state.projUserRoles);
+    console.log(userRoleId);
+    const userRole = this.state.projUserRoles.find(
+      (role) => role.id === userRoleId
+    );
+    if (userRole) {
+      return (
+        userRole.permissions.includes(Permission.DeleteEditSettingsAndUsers) &&
+        userRole.permissions.includes(Permission.ImportExport)
+      );
+    }
+    return false;
   }
 
-  private handleClose() {
-    this.setState({ dialogOpen: false });
+  private handleRemoveUserDialogOpen() {
+    this.setState({ removeUserDialogOpen: true });
+  }
+
+  private handleRemoveUserDialogClose() {
+    this.setState({ removeUserDialogOpen: false });
+  }
+
+  private handleMakeAdminDialogOpen() {
+    this.setState({ makeAdminDialogOpen: true });
+  }
+
+  private handleMakeAdminDialogClose() {
+    this.setState({ makeAdminDialogOpen: false });
+  }
+
+  private handleRemoveAdminDialogOpen() {
+    this.setState({ removeAdminDialogOpen: true });
+  }
+
+  private handleRemoveAdminDialogClose() {
+    this.setState({ removeAdminDialogOpen: false });
   }
 
   render() {
     var userList: React.ReactElement<ListItemProps>[] = [];
-    var removeUser: React.ReactElement<IconButtonProps>;
+    var manageUser: React.ReactElement<ElementType>;
+    var adminOption: React.ReactElement<MenuItemProps>;
     const currentUser = localStorage.getItem("user");
     const currentProjectId = localStorage.getItem("projectId");
     const sortedUserList = this.getSortedUsers();
@@ -138,25 +236,78 @@ export default class ActiveUsers extends React.Component<UserProps, UserState> {
         user.id !== JSON.parse(currentUser).id &&
         !user.isAdmin
       ) {
-        removeUser = (
+        var hi = this.isProjectAdmin(user.projectRoles[currentProjectId]);
+        console.log(hi);
+        if (hi) {
+          adminOption = (
+            <MenuItem onClick={this.handleRemoveAdminDialogOpen.bind(this)}>
+              <Translate id="buttons.removeAdmin" />
+            </MenuItem>
+          );
+        } else {
+          adminOption = (
+            <MenuItem onClick={this.handleMakeAdminDialogOpen.bind(this)}>
+              <Translate id="buttons.makeAdmin" />
+            </MenuItem>
+          );
+        }
+        manageUser = (
           <div>
-            <IconButton onClick={this.handleOpen.bind(this)}>
-              <DeleteForeverIcon />
-            </IconButton>
             <DeleteDialog
-              open={this.state.dialogOpen}
-              // TODO: Add the correct text
-              textId={"reviewEntries.deleteWordWarning"}
-              handleCancel={this.handleClose.bind(this)}
+              open={this.state.removeUserDialogOpen}
+              textId={"projectSettings.userManagement.removeUserWarning"}
+              handleCancel={this.handleRemoveUserDialogClose.bind(this)}
               handleAccept={this.removeUser.bind(this, [user.id])}
             />
+            <MakeAdminDialog
+              open={this.state.makeAdminDialogOpen}
+              textId={"projectSettings.userManagement.makeAdminWarning"}
+              handleCancel={this.handleMakeAdminDialogClose.bind(this)}
+              handleAccept={this.makeAdmin.bind(this, [user.id])}
+            />
+            <RemoveAdminDialog
+              open={this.state.removeAdminDialogOpen}
+              textId={"projectSettings.userManagement.removeAdminWarning"}
+              handleCancel={this.handleRemoveAdminDialogClose.bind(this)}
+              handleAccept={this.removeAdmin.bind(this, [user.id])}
+            />
+            <Tooltip title="Manage User" placement="right">
+              <IconButton
+                id="user-options"
+                onClick={(event) => {
+                  this.setState({
+                    anchorEl: event.currentTarget,
+                  });
+                }}
+              >
+                <MoreVertIcon />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              id="simple-menu"
+              anchorEl={this.state.anchorEl}
+              keepMounted
+              open={Boolean(this.state.anchorEl)}
+              onClose={() => {
+                this.setState({
+                  anchorEl: undefined,
+                });
+              }}
+            >
+              <MenuItem onClick={this.handleRemoveUserDialogOpen.bind(this)}>
+                <Translate id="buttons.removeFromProject" />
+              </MenuItem>
+              {adminOption}
+            </Menu>
           </div>
         );
       } else {
-        removeUser = (
-          <IconButton disabled>
-            <DeleteForeverIcon />
-          </IconButton>
+        manageUser = (
+          <Tooltip title="You cannot edit this user">
+            <IconButton disabled>
+              <MoreVertIcon />
+            </IconButton>
+          </Tooltip>
         );
       }
       userList.push(
@@ -167,7 +318,7 @@ export default class ActiveUsers extends React.Component<UserProps, UserState> {
             style={{ marginRight: theme.spacing(1) }}
           />
           <ListItemText primary={`${user.name} (${user.username})`} />
-          {removeUser}
+          {manageUser}
         </ListItem>
       );
     }
