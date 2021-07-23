e4090d3457f2108e75c9fb3df615a1dc204a5800
johnthagen <johnthagen@gmail.com>
2021-07-23 14:17:43 -0400

    Add FileStorage tests

---
+++ Backend.Tests/Helper/FileStorageTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BackendFramework.Helper;
+using NUnit.Framework;
+
+namespace Backend.Tests.Helper
+{
+    public class FileStorageTests
+    {
+        [Test]
+        public void TestInvalidFileTypeExtension()
+        {
+            Assert.Throws<NotImplementedException>(() => { FileStorage.FileTypeExtension((FileStorage.FileType) 99); });
+        }
+    }
+}
