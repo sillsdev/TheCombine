e37e4e0c5f8bebfb1c157eabf5b1708aabd7145d
johnthagen <johnthagen@gmail.com>
2021-07-24 10:07:46 -0400

    Add round trip unit tests for PasswordHash class

---
+++ Backend.Tests/Helper/PasswordHashTests.cs
@@ -0,0 +1,34 @@
+﻿using BackendFramework.Helper;
+using NUnit.Framework;
+
+namespace Backend.Tests.Helper
+{
+    public class PasswordHashTests
+    {
+        private const string Password = "password123";
+
+        [Test]
+        public void HashPasswordValidRoundtrip()
+        {
+            var hash = PasswordHash.HashPassword(Password);
+            Assert.That(PasswordHash.ValidatePassword(hash, Password));
+        }
+
+        [Test]
+        public void HashPasswordInvalidHashByteRoundtrip()
+        {
+            var hash = PasswordHash.HashPassword(Password);
+            // Change a single byte of the hash and validate that the hash fails.
+            hash[0] ^= 0xff;
+            Assert.IsFalse(PasswordHash.ValidatePassword(hash, Password));
+        }
+
+        [Test]
+        public void HashPasswordInvalidPasswordCharacterRoundtrip()
+        {
+            var hash = PasswordHash.HashPassword(Password);
+            var mutatedPassword = $"Z{Password}";
+            Assert.IsFalse(PasswordHash.ValidatePassword(hash, mutatedPassword));
+        }
+    }
+}
