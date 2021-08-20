d04150709896251599fb3e6aa4f3fafeac98aca1
johnthagen <johnthagen@gmail.com>
2021-08-20 15:08:11 -0400

    Add a basic banner model

---
+++ Backend/Models/Banner.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BackendFramework.Models
+{
+    public class Banner
+    {
+        /// <summary>
+        /// Banner shown in the initial landing page of the app long-term to personalize the deployment.
+        /// </summary>
+        [Required]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Login { get; set; }
+
+        /// <summary>
+        /// Banner used for short-term announcements related to the specific deployment.
+        /// </summary>
+        [Required]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Announcement { get; set; }
+    }
+}
