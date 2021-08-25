691d7e3a3b4b0730b12aa77aa48e64b19dc59264
Jim Grady <jimgrady.jg@gmail.com>
2021-08-25 15:22:52 -0400

    Clean out old backups

--- maintenance/scripts/combine-backup-job.sh
+++ maintenance/scripts/combine-backup-job.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 if [ -n "$clean_files_select" ]; then
   echo "kubectl exec deployment/backend -- find /home/app/${backend_files_subdir} ${clean_files_select} -delete"
 fi
 
 # Backup the CombineDatabase and the .CombineFiles in the backend
-#combine_backup.py
+combine_backup.py
 # Cleanup the old backups stored in AWS S3 service
-#combine-clean-aws.sh
+combine-clean-aws.sh
