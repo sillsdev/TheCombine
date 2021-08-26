cc40dc59ff3ddf4d49b35bb7815fd8b4466500af
Jim Grady <jimgrady.jg@gmail.com>
2021-08-26 08:38:00 -0400

    Removed clean up of backend files
    This is no longer required.  The app no longer leaves .zip files around 
    after import/export.

--- maintenance/scripts/combine-backup-job.sh
+++ maintenance/scripts/combine-backup-job.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-if [ -n "$clean_files_select" ]; then
-  "kubectl exec deployment/backend -- find /home/app/${backend_files_subdir} ${clean_files_select} -delete"
-fi
-
 # Backup the CombineDatabase and the .CombineFiles in the backend
 combine_backup.py
 # Cleanup the old backups stored in AWS S3 service
