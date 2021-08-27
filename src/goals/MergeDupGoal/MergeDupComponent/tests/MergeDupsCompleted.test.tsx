d8cdddc3fb8377f61673d8b94a855e09cfe276e7
jzwu <jzwu12@gmail.com>
2021-08-27 09:48:53 -0700

    Cleanup syntax and style in doWordsIncludeMerges unit tests

--- src/goals/MergeDupGoal/MergeDupComponent/tests/MergeDupsCompleted.test.tsx
+++ src/goals/MergeDupGoal/MergeDupComponent/tests/MergeDupsCompleted.test.tsx
@@ -1,9 +1,9 @@
+import { MergeUndoIds, Word } from "api/models";
 import { doWordsIncludeMerges } from "goals/MergeDupGoal/MergeDupComponent/MergeDupsCompleted";
-import { MergeUndoIds, State, Word } from "api/models";
+import { newWord } from "types/word";
 
-describe("convertGoalToEdit, convertEditToGoal", () => {
+describe("doWordsIncludeMerges", () => {
   it("should return false since words don't contain each of the parentIds in merges", () => {
-    const merges: MergeUndoIds[] = [];
     const merge1: MergeUndoIds = {
       parentIds: ["merge1PId", "merge1PId2"],
       childIds: ["merge1CId1"],
@@ -12,58 +12,17 @@ describe("convertGoalToEdit, convertEditToGoal", () => {
       parentIds: ["merge2PId", "merge2PId2"],
       childIds: ["merge2CId1"],
     };
-    merges.push(merge1);
-    merges.push(merge2);
+    const merges: MergeUndoIds[] = [merge1, merge2];
 
-    const words: Word[] = [];
-    const word1: Word = {
-      id: "merge1PId",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    const word2: Word = {
-      id: "merge1PId2",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    const word3: Word = {
-      id: "merge2PId",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    words.push(word1);
-    words.push(word2);
-    words.push(word3);
+    const words: Word[] = [newWord(), newWord(), newWord()];
+    words[0].id = "merge1PId";
+    words[1].id = "merge1PId2";
+    words[2].id = "merge2PId";
 
     expect(doWordsIncludeMerges(words, merges)).toBe(false);
   });
 
   it("should return true since words contain each of the parentIds in merges", () => {
-    const merges: MergeUndoIds[] = [];
     const merge1: MergeUndoIds = {
       parentIds: ["merge1PId", "merge1PId2"],
       childIds: ["merge1CId1"],
@@ -72,136 +31,28 @@ describe("convertGoalToEdit, convertEditToGoal", () => {
       parentIds: ["merge2PId", "merge2PId2"],
       childIds: ["merge2CId1"],
     };
-    merges.push(merge1);
-    merges.push(merge2);
+    const merges: MergeUndoIds[] = [merge1, merge2];
 
-    const words: Word[] = [];
-    const word1: Word = {
-      id: "merge1PId",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    const word2: Word = {
-      id: "merge1PId2",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    const word3: Word = {
-      id: "merge2PId",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    const word4: Word = {
-      id: "merge2PId2",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    words.push(word1);
-    words.push(word2);
-    words.push(word3);
-    words.push(word4);
+    const words: Word[] = [newWord(), newWord(), newWord(), newWord()];
+    words[0].id = "merge1PId";
+    words[1].id = "merge1PId2";
+    words[2].id = "merge2PId";
+    words[3].id = "merge2PId2";
 
     expect(doWordsIncludeMerges(words, merges)).toBe(true);
   });
 
   it("should return true if merges is empty and words is non-empty", () => {
-    const merges: MergeUndoIds[] = [];
+    const words: Word[] = [newWord(), newWord(), newWord(), newWord()];
+    words[0].id = "merge1PId";
+    words[1].id = "merge1PId2";
+    words[2].id = "merge2PId";
+    words[3].id = "merge2PId2";
 
-    const words: Word[] = [];
-    const word1: Word = {
-      id: "merge1PId",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    const word2: Word = {
-      id: "merge1PId2",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    const word3: Word = {
-      id: "merge2PId",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    const word4: Word = {
-      id: "merge2PId2",
-      guid: "",
-      vernacular: "",
-      senses: [],
-      audio: [],
-      created: "",
-      modified: "",
-      accessibility: State.Active,
-      history: [],
-      projectId: "",
-      note: { language: "", text: "" },
-    };
-    words.push(word1);
-    words.push(word2);
-    words.push(word3);
-    words.push(word4);
-
-    expect(doWordsIncludeMerges(words, merges)).toBe(true);
+    expect(doWordsIncludeMerges(words, [])).toBe(true);
   });
 
   it("should return false if words is empty and merges is non-empty", () => {
-    const merges: MergeUndoIds[] = [];
     const merge1: MergeUndoIds = {
       parentIds: ["merge1PId", "merge1PId2"],
       childIds: ["merge1CId1"],
@@ -210,29 +61,12 @@ describe("convertGoalToEdit, convertEditToGoal", () => {
       parentIds: ["merge2PId", "merge2PId2"],
       childIds: ["merge2CId1"],
     };
-    merges.push(merge1);
-    merges.push(merge2);
+    const merges: MergeUndoIds[] = [merge1, merge2];
 
-    const words: Word[] = [];
-
-    expect(doWordsIncludeMerges(words, merges)).toBe(false);
+    expect(doWordsIncludeMerges([], merges)).toBe(false);
   });
 
   it("should return true if both words and merges are empty", () => {
-    const merges: MergeUndoIds[] = [];
-    const merge1: MergeUndoIds = {
-      parentIds: ["merge1PId", "merge1PId2"],
-      childIds: ["merge1CId1"],
-    };
-    const merge2: MergeUndoIds = {
-      parentIds: ["merge2PId", "merge2PId2"],
-      childIds: ["merge2CId1"],
-    };
-    merges.push(merge1);
-    merges.push(merge2);
-
-    const words: Word[] = [];
-
-    expect(doWordsIncludeMerges(words, merges)).toBe(false);
+    expect(doWordsIncludeMerges([], [])).toBe(true);
   });
 });
