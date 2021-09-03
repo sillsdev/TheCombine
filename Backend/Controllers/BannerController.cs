218573dd5670e91f466c6ac100b5cc75dc7fee98
johnthagen <johnthagen@gmail.com>
2021-09-03 09:49:15 -0400

    Add banner repository

---
+++ Backend/Controllers/BannerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendFramework.Controllers
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("v1/banner")]
+    public class BannerController
+    {
+
+    }
+}
---
+++ Backend/Repositories/BannerRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using BackendFramework.Helper;
+using BackendFramework.Interfaces;
+using BackendFramework.Models;
+using MongoDB.Driver;
+
+namespace BackendFramework.Repositories
+{
+    /// <summary> Atomic database functions for <see cref="Banner"/> singleton. </summary>
+    [ExcludeFromCodeCoverage]
+    public class BannerRepository
+    {
+        private readonly IBannerContext _bannerDatabase;
+
+        public BannerRepository(IBannerContext collectionSettings)
+        {
+            _bannerDatabase = collectionSettings;
+        }
+
+        private async Task<Banner> CreateEmptyBanner()
+        {
+            var emptyBanner = new Banner();
+            await _bannerDatabase.Banners.InsertOneAsync(emptyBanner);
+            return emptyBanner;
+        }
+
+        public async Task<Banner> Get()
+        {
+            var bannerList = await _bannerDatabase.Banners.FindAsync(b => true);
+            try
+            {
+                return await bannerList.FirstAsync();
+            }
+            catch (InvalidOperationException)
+            {
+                return await CreateEmptyBanner();
+            }
+        }
+
+        public async Task<ResultOfUpdate> Update(Banner banner)
+        {
+            var existingBanner = await Get();
+            var filter = Builders<Banner>.Filter.Eq(b => b.Id, existingBanner.Id);
+            var updateDef = Builders<Banner>.Update.Set(b => banner, banner);
+            var updateResult = await _bannerDatabase.Banners.UpdateOneAsync(filter, updateDef);
+
+            // The Banner singleton should always exist, so this case should never happen.
+            if (!updateResult.IsAcknowledged || updateResult.ModifiedCount != 1)
+            {
+                throw new BannerSingletonNotFound();
+            }
+
+            return ResultOfUpdate.Updated;
+        }
+
+        [Serializable]
+        private class BannerSingletonNotFound : Exception
+        {
+        }
+    }
+}
