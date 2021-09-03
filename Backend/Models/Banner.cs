9151941e259384c0b263894669d033052d480105
johnthagen <johnthagen@gmail.com>
2021-09-03 07:35:55 -0400

    Update model definition

--- Backend/Models/Banner.cs
+++ Backend/Models/Banner.cs
@@ -6,20 +6,28 @@ namespace BackendFramework.Models
 {
     public class Banner
     {
+        [Required]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
         /// <summary>
         /// Banner shown in the initial landing page of the app long-term to personalize the deployment.
         /// </summary>
         [Required]
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("login")]
         public string? Login { get; set; }
 
         /// <summary>
         /// Banner used for short-term announcements related to the specific deployment.
         /// </summary>
         [Required]
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("announcement")]
         public string? Announcement { get; set; }
+
+        public Banner()
+        {
+            Id = "";
+        }
     }
 }
