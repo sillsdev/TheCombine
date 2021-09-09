d196358c2268c15c2d4d03b3a3ad2277b6b7e99b
JacobHaimes <42851286+JacobHaimes@users.noreply.github.com>
2021-09-09 15:45:44 -0400

    Changed wording of Step to Merge Set (#1365)

--- src/goals/DefaultGoal/DisplayProgress.tsx
+++ src/goals/DefaultGoal/DisplayProgress.tsx
@@ -3,25 +3,36 @@ import { Translate } from "react-localize-redux";
 import { useSelector } from "react-redux";
 
 import { StoreState } from "types";
+import { GoalType } from "types/goals";
 
 /**
  * Displays how much progress has been made in a goal
  */
 export default function DisplayProgress() {
+  /* We cannot use a single selector for state.goalsState.currentGoal and define everything on that;
+ currentStep needs its own targeted selector for it to re-render as the user progresses. */
   const currentStep = useSelector(
     (state: StoreState) => state.goalsState.currentGoal.currentStep
   );
   const numSteps = useSelector(
     (state: StoreState) => state.goalsState.currentGoal.numSteps
   );
+  const goalType = useSelector(
+    (state: StoreState) => state.goalsState.currentGoal.goalType
+  );
   const percentComplete = (currentStep / numSteps) * 100;
+
+  const stepTranslateId =
+    goalType === GoalType.MergeDups
+      ? "goal.progress.stepMerge"
+      : "goal.progress.step";
 
   return numSteps > 1 ? (
     <Paper key={currentStep}>
       <Grid container direction="column">
         <Grid item xs>
           <Typography variant={"h4"}>
-            <Translate id="goal.progress.step" />
+            <Translate id={stepTranslateId} />
             {` ${currentStep + 1} `}
             <Translate id="goal.progress.of" />
             {` ${numSteps}`}
