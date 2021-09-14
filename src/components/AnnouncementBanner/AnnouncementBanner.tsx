c3b4a4f2597e3085af9b2d321f0083f3a2214a65
D. Ror <imnasnainaec@gmail.com>
2021-09-14 15:20:31 -0400

    Allow user to close banner; lasts until logout or banner update.

--- src/components/AnnouncementBanner/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner/AnnouncementBanner.tsx
@@ -1,31 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { TextField, Toolbar } from "@material-ui/core";
+import { IconButton, TextField, Toolbar } from "@material-ui/core";
+import { Cancel } from "@material-ui/icons";
 
 import * as backend from "backend";
+import { getClosedBanner, setClosedBanner } from "backend/localStorage";
 import { topBarHeight } from "components/LandingPage/TopBar";
+import { themeColors } from "types/theme";
 
 export default function AnnouncementBanner() {
   const [banner, setBanner] = useState<string>("");
 
   useEffect(() => {
     const getBanner = async () => {
-      const banner = await backend.getBanner();
-      setBanner(banner.announcement);
+      const text = (await backend.getBanner()).announcement;
+      if (text && text !== getClosedBanner()) {
+        setBanner(text);
+      }
     };
     getBanner();
   }, []);
+
+  function closeBanner() {
+    setClosedBanner(banner);
+    setBanner("");
+  }
 
   return (
     <React.Fragment>
       {banner !== "" && (
         <div style={{ marginTop: topBarHeight }}>
-          <Toolbar style={{ background: "#ffc046" }}>
+          <Toolbar>
+            <IconButton onClick={closeBanner}>
+              <Cancel />
+            </IconButton>
             <TextField
               id="login.banner"
               variant="outlined"
               multiline
               disabled
-              style={{ width: "100%" }}
+              style={{ width: "100%", backgroundColor: themeColors.warn }}
               value={banner}
             />
           </Toolbar>
