3ccc6cf172da90f3eae56bf798d67321c95febf3
D. Ror <imnasnainaec@gmail.com>
2021-09-22 16:02:24 -0400

    Look for new banner on refresh and navigation

--- src/components/AnnouncementBanner/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner/AnnouncementBanner.tsx
@@ -12,6 +12,14 @@ import theme, { themeColors } from "types/theme";
 export default function AnnouncementBanner() {
   const [banner, setBanner] = useState<string>("");
 
+  // Adust the margins depending on whether there is an AppBar.
+  const loc = useLocation().pathname;
+  const isBelowAppBar = loc === Path.Root || loc.startsWith(Path.ProjScreen);
+  const margins = isBelowAppBar
+    ? { marginTop: topBarHeight, marginBottom: -topBarHeight }
+    : {};
+
+  // Check for banner on (re)load or navigation to a new page.
   useEffect(() => {
     getBanner().then((banner) => {
       const text = banner.announcement;
@@ -19,19 +27,12 @@ export default function AnnouncementBanner() {
         setBanner(text);
       }
     });
-  }, []);
+  }, [loc]);
 
   function closeBanner() {
     setClosedBanner(banner);
     setBanner("");
   }
-
-  // Adust the margins depending on whether there is an AppBar.
-  const loc = useLocation().pathname;
-  const isBelowAppBar = loc === Path.Root || loc.startsWith(Path.ProjScreen);
-  const margins = isBelowAppBar
-    ? { marginTop: topBarHeight, marginBottom: -topBarHeight }
-    : {};
 
   return (
     <React.Fragment>
