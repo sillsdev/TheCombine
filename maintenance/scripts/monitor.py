e4354f28aa158ffd3a5e8bcee7cb41ea2bd13f86
Jim Grady <jimgrady.jg@gmail.com>
2021-10-05 08:08:46 -0400

    Add cert-proxy-server deployment to k8s_cert_proxy_server role

--- maintenance/scripts/monitor.py
+++ maintenance/scripts/monitor.py
@@ -2,17 +2,28 @@
 """
 Monitor TLS secrets for changes and push changes to AWS S3.
 
-monitor.py will monitor the secrets specified in the NUC_CERTIFICATES
+monitor.py will monitor the secrets specified in the CERT_PROXY_CERTIFICATES
 environment variable.  When a secret is updated, the updated secret is pushed
 to the AWS_S3_BUCKET.
 """
 
 from kubernetes import client, config, watch
+from typing import List
+
+
+def get_secret_names() -> List[str]:
+    """
+    Create the list of secrets to monitor.
+
+    Create the list of secrets to monitor from the CERT_PROXY_CERTIFICATES
+    environment variable.  This function assumes that the secret name is '-tls'
+    appened to the dns name of the server (with '.' converted to '-').
+    """
 
 
 def main() -> None:
     config.load_incluster_config()
-
+    secrets_list = get_secret_names()
     v1 = client.CoreV1Api()
     print("Listing all configmaps in the default namespace:")
     w = watch.Watch()
