4b5831dc5db904239ee4e842c7e8c723980a9249
johnthagen <johnthagen@gmail.com>
2021-11-05 10:26:28 -0400

    Add tests for FileStorage ID sanitization

--- Backend.Tests/Helper/FileStorageTests.cs
+++ Backend.Tests/Helper/FileStorageTests.cs
@@ -13,5 +13,28 @@ namespace Backend.Tests.Helper
             Assert.AreEqual(FileStorage.FileTypeExtension(FileStorage.FileType.Avatar), ".jpg");
             Assert.Throws<NotImplementedException>(() => { FileStorage.FileTypeExtension((FileStorage.FileType)99); });
         }
+
+        [Test]
+        public void TestFilePathIdSanitization()
+        {
+            const string invalidId = "@";
+            const string validId = "a";
+            Assert.Throws<FileStorage.InvalidIdException>(
+                () => FileStorage.GenerateAudioFilePathForWord(invalidId, validId));
+            Assert.Throws<FileStorage.InvalidIdException>(
+                () => FileStorage.GenerateAudioFilePathForWord(validId, invalidId));
+            Assert.Throws<FileStorage.InvalidIdException>(
+                () => FileStorage.GenerateAudioFilePath(invalidId, "file.mp3"));
+            Assert.Throws<FileStorage.InvalidIdException>(
+                () => FileStorage.GenerateAudioFileDirPath(invalidId));
+            Assert.Throws<FileStorage.InvalidIdException>(
+                () => FileStorage.GenerateImportExtractedLocationDirPath(invalidId));
+            Assert.Throws<FileStorage.InvalidIdException>(
+                () => FileStorage.GenerateLiftImportDirPath(invalidId));
+            Assert.Throws<FileStorage.InvalidIdException>(
+                () => FileStorage.GenerateAvatarFilePath(invalidId));
+            Assert.Throws<FileStorage.InvalidIdException>(
+                () => FileStorage.GetProjectDir(invalidId));
+        }
     }
 }
