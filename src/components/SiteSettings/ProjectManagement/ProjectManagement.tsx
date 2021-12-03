146ec0c02278bef2f32752d018b2053d17187d1b
D. Ror <imnasnainaec@gmail.com>
2021-12-03 12:48:15 -0500

    Convert to functional component

--- src/components/SiteSettings/ProjectManagement/ProjectManagement.tsx
+++ src/components/SiteSettings/ProjectManagement/ProjectManagement.tsx
@@ -1,6 +1,5 @@
 import { List, ListItem, Typography } from "@material-ui/core";
-import React from "react";
-import { LocalizeContextProps, withLocalize } from "react-localize-redux";
+import { useEffect, useState } from "react";
 
 import { Project } from "api/models";
 import { getAllProjects } from "backend";
@@ -8,68 +7,64 @@ import ExportButton from "components/ProjectExport/ExportButton";
 import ProjectButtonWithConfirmation from "components/SiteSettings/ProjectManagement/ProjectButtonWithConfirmation";
 import theme from "types/theme";
 
-interface ProjectManagementState {
-  activeProjects: Project[];
-  archivedProjects: Project[];
+export default function ProjectManagement() {
+  const [allProjects, setAllProjects] = useState<Project[]>([]);
+  const [activeProjects, setActiveProjects] = useState<Project[]>([]);
+  const [archivedProjects, setArchivedProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    updateProjectList();
+  }, [setAllProjects]);
+
+  async function updateProjectList() {
+    await getAllProjects().then(setAllProjects);
+  }
+
+  // Sort projects into active vs archived as conditional effect here rather than inside
+  // the ProjectList function to avoid react mounting/rendering error in console.
+  useEffect(() => {
+    setActiveProjects(allProjects.filter((project) => project.isActive));
+    setArchivedProjects(allProjects.filter((project) => !project.isActive));
+  }, [allProjects, setActiveProjects, setArchivedProjects]);
+
+  return ProjectList(activeProjects, archivedProjects, updateProjectList);
 }
 
-export class ProjectManagement extends React.Component<
-  LocalizeContextProps,
-  ProjectManagementState
-> {
-  constructor(props: LocalizeContextProps) {
-    super(props);
-
-    this.state = {
-      activeProjects: [],
-      archivedProjects: [],
-    };
-    this.updateProjectList();
+// Extract and export for unit testing.
+export function ProjectList(
+  activeProjects: Project[],
+  archivedProjects: Project[],
+  updateProjects: () => Promise<void>
+) {
+  function getListItems(projects: Project[]) {
+    return projects.map((project) => (
+      <ListItem key={project.id}>
+        <Typography
+          variant="h6"
+          color={project.isActive ? "inherit" : "textSecondary"}
+          style={{ marginRight: theme.spacing(1) }}
+        >
+          {project.name}
+        </Typography>
+        {/* Export Lift file */}
+        <ExportButton
+          projectId={project.id}
+          buttonProps={{ style: { marginRight: theme.spacing(1) } }}
+        />
+        {/* Archive active project or restore archived project. */}
+        <ProjectButtonWithConfirmation
+          archive={project.isActive}
+          projectId={project.id}
+          updateParent={updateProjects}
+        />
+      </ListItem>
+    ));
   }
 
-  updateProjectList = () => {
-    getAllProjects().then((projects) => {
-      const activeProjects = projects.filter((project) => project.isActive);
-      const archivedProjects = projects.filter((project) => !project.isActive);
-      this.setState({ activeProjects, archivedProjects });
-    });
-  };
-
-  getListItems(projects: Project[]) {
-    return projects.map((project) => {
-      return (
-        <ListItem key={project.id}>
-          <Typography
-            variant="h6"
-            color={project.isActive ? "inherit" : "textSecondary"}
-            style={{ marginRight: theme.spacing(1) }}
-          >
-            {project.name}
-          </Typography>
-          {/* Export Lift file */}
-          <ExportButton
-            projectId={project.id}
-            buttonProps={{ style: { marginRight: theme.spacing(1) } }}
-          />
-          {/* Archive active project or restore archived project. */}
-          <ProjectButtonWithConfirmation
-            archive={project.isActive}
-            projectId={project.id}
-            updateParent={this.updateProjectList}
-          />
-        </ListItem>
-      );
-    });
-  }
-
-  render() {
-    return (
-      <List>
-        {this.getListItems(this.state.activeProjects)}
-        {this.getListItems(this.state.archivedProjects)}
-      </List>
-    );
-  }
+  return (
+    <List>
+      {getListItems(activeProjects)}
+      {getListItems(archivedProjects)}
+    </List>
+  );
 }
-
-export default withLocalize(ProjectManagement);
