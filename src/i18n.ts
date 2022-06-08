815d1f2ea4b750fe6a259edbc01260c329bcaa2b
D. Ror <imnasnainaec@gmail.com>
2022-06-08 09:50:19 -0400

    Connect to translation files

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,13 @@
 // https://dev.to/adrai/how-to-properly-internationalize-a-react-application-using-i18next-3hdb#getting-started
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 import { Bcp47Code } from "types/writingSystem";
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
