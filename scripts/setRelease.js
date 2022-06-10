1e8ae5fc490d229af3b91dc332631d2bab301d52
Jim Grady <jimgrady.jg@gmail.com>
2022-06-10 15:02:25 -0400

    Remove dead code

--- scripts/setRelease.js
+++ scripts/setRelease.js
@@ -3,27 +3,6 @@
 //
 
 const spawnSync = require("child_process").spawnSync;
-const { log } = require("console");
-
-function quote_value() {
-  if (arguments.length > 0) {
-    const value = arguments[0];
-    // is value a number?
-    if (/^[+-]?[0-9]+([.][0-9]+)?$/.test(value)) {
-      return `${value}`;
-    }
-    // is value already quoted?
-    if (/^".*"$/.test(value)) {
-      return `${value}`;
-    }
-    // is value a boolean?
-    if (/^(true|false)?$/.test(value)) {
-      return `${value}`;
-    }
-    return `"${value}"`;
-  }
-  return "";
-}
 
 function runPy() {
   if (arguments.length > 0) {
