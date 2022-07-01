41433e6071ead0d881ef49de6473143ddc7ade6f
D. Ror <imnasnainaec@gmail.com>
2022-07-01 11:09:19 -0400

    Add comments to i18n mock files

--- src/tests/i18nMock.ts
+++ src/tests/i18nMock.ts
@@ -1,11 +1,18 @@
-// https://react.i18next.com/misc/testing
+/* Adapted from https://react.i18next.com/misc/testing
+ * (In most cases, this isn't needed.
+ * Instead you can use tests/mockReactI18next.ts to mock out i18n completely.)
+ * For a simple i18next wrapper, add the following to a test file:
+ * * import { I18nextProvider } from "react-i18next";
+ * * import i18n from "tests/i18nMock";
+ * Then wrap the component being rendered with <I18nextProvider i18n={i18n}> */
+
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import { Bcp47Code } from "types/writingSystem";
 
 i18n.use(initReactI18next).init({
-  //debug: true, // uncomment for troubleshooting tests
+  //debug: true, // Uncomment to troubleshoot tests
   fallbackLng: Bcp47Code.Default,
   interpolation: { escapeValue: false },
   resources: {
--- src/tests/mockReactI18next.ts
+++ src/tests/mockReactI18next.ts
@@ -1,3 +1,8 @@
+/* Use `import "tests/mockReactI18next.ts";` in a test file to mock i18next.
+ * (For testing components with `Trans`, see tests/i18nMock.ts instead.)
+ * This import should be placed before the other internal imports.
+ * It must come before any file that imports `react-i18next`. */
+
 jest.mock("react-i18next", () => ({
   useTranslation: () => ({ t: (str: string) => str }),
   withTranslation: () => (Component: any) => {
