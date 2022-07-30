sillsdev/TheCombine

commit a9bfe6a2451192b5fc65f0f81212002b8c580cee

Simplify Release Process (#1677)
* Get Release version from git history rather than package.json
* Update release process documentation
* Update Rancher version for install
* User build.py in github action instead of docker/build-push-action
* update combine version in package-lock.json
* Remove printVersion.js - superceded by app_release.py
* Update CI/CD actions
* Deploy release build to QA server as well as Live server
* Pin frontend_builder to 'node:16.14'
* import future annotations for Python 3.8
* Replace os.system call with subprocess.run in build.py
- push releases to public AWS ECR repo
- build frontend/backend/maintenance in parallel
- pin node to 16.14 for lint_test_build
* Update version of sillsdev/aws-kubectl

deploy/scripts/build.py
@@ -8,11 +8,17 @@ images.  'nerdctl' is recommended when using Rancher Desktop for the development
 environment and 'docker' is recommended when using Docker Desktop.
 """
 
+from __future__ import annotations
+
 import argparse
 from dataclasses import dataclass
+import logging
 import os
 from pathlib import Path
 from typing import Dict, Optional
+
+from app_release import get_release, set_release
+from utils import run_cmd
 
 
 @dataclass(frozen=True)
@@ -66,18 +72,24 @@ def parse_args() -> argparse.Namespace:
         help="Namespace for 'nerdctl' when building images.",
         default="k8s.io",
     )
+    parser.add_argument(
+        "--output-level",
+        "-o",
+        choices=["none", "progress", "all"],
+        help="Specify the amount of output desired during the build.",
+    )
     return parser.parse_args()
-
-
-def run(cmd: str) -> None:
-    """Print a command and run it"""
-    print(cmd)
-    os.system(cmd)
 
 
 def main() -> None:
     """Build the Docker images for The Combine."""
     args = parse_args()
+
+    if args.output_level != "none":
+        log_level = logging.INFO
+    else:
+        log_level = logging.WARNING
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=log_level)
 
     if args.nerdctl:
         build_cmd = f"nerdctl -n {args.namespace}"
@@ -95,13 +107,31 @@ def main() -> None:
         "frontend": BuildSpec(project_dir, "frontend"),
     }
 
+    # Create the version file
+    release_file = project_dir / "public" / "scripts" / "release.js"
+
+    set_release(get_release(), release_file)
+
+    # Build each of the component images
     for component in to_do:
         spec = build_specs[component]
         os.chdir(spec.dir)
         image_name = get_image_name(args.repo, spec.name, args.tag)
-        run(f"{build_cmd} build -t {image_name} -f Dockerfile .")
+        logging.info(f"Building component {component}")
+        print_all = args.output_level == "all"
+        run_cmd(
+            [build_cmd, "build", "-t", image_name, "-f", "Dockerfile", "."],
+            print_cmd=print_all,
+            print_output=print_all,
+        )
+        logging.info(f"{component} build complete")
         if args.repo is not None:
-            run(f"{build_cmd} push {image_name}")
+            run_cmd([build_cmd, "push", image_name])
+            logging.info(f"{image_name} pushed to {args.repo}")
+
+    # Remove the version file
+    if release_file.exists():
+        release_file.unlink()
 
 
 if __name__ == "__main__":

.github/actions/combine-build/action.yml
@@ -1,13 +1,13 @@
 name: "Combine Build"
-description: "Build Docker images for The Combine and push to AWS ECR"
+description: "Build a single Docker image for The Combine and push it to AWS ECR"
 inputs:
-  tag_suffix:
-    description: "Suffix for image tag"
+  image_registry:
+    description: "Docker Image Registry"
     required: true
+  image_registry_alias:
+    description: "Custom or default alias for AWS Public ECR registries.  Blank for AWS ECR Private registries."
+    required: false
     default: ""
-  image_repo:
-    description: "Docker Image Repository"
-    required: true
   aws_access_key_id:
     description: "Access key id for AWS ECR - must have read/write access"
     required: true
@@ -16,6 +16,9 @@ inputs:
     required: true
   aws_default_region:
     description: "Default accessibility zone for AWS ECR images"
+    required: true
+  build_component:
+    description: "Component to build"
     required: true
 outputs:
   image_tag:
@@ -26,7 +29,7 @@ runs:
   steps:
     - name: Create Image Tag
       run: |
-        echo "IMAGE_TAG="`node scripts/printVersion.js`${{ inputs.tag_suffix }} >> $GITHUB_ENV
+        echo "IMAGE_TAG="`deploy/scripts/app_release.py --get` >> $GITHUB_ENV
       shell: bash
 
     - name: Save Image Tag
@@ -41,27 +44,14 @@ runs:
         aws-secret-access-key: ${{ inputs.aws_secret_access_key }}
         aws-region: ${{ inputs.aws_default_region }}
 
-    - name: Login to Amazon ECR
-      id: login-ecr
-      uses: aws-actions/amazon-ecr-login@v1
-
-    - name: Build Frontend
-      uses: docker/build-push-action@v2
+    - name: Login to AWS ECR
+      uses: docker/login-action@v2
       with:
-        context:
-        push: true
-        tags: ${{ inputs.image_repo }}/combine_frontend:${{ env.IMAGE_TAG }}
+        registry: ${{ inputs.image_registry }}
+        username: ${{ inputs.aws_access_key_id }}
+        password: ${{ inputs.aws_secret_access_key }}
 
-    - name: Build Backend
-      uses: docker/build-push-action@v2
-      with:
-        context: Backend
-        push: true
-        tags: ${{ inputs.image_repo }}/combine_backend:${{ env.IMAGE_TAG }}
-
-    - name: Build Maintenance
-      uses: docker/build-push-action@v2
-      with:
-        context: maintenance
-        push: true
-        tags: ${{ inputs.image_repo }}/combine_maint:${{ env.IMAGE_TAG }}
+    - name: Build The Combine
+      run: |
+        deploy/scripts/build.py --tag ${{ env.IMAGE_TAG }} --repo ${{ inputs.image_registry }}${{ inputs.image_registry_alias}}
+      shell: bash

.github/actions/combine-deploy-update/action.yml
@@ -21,7 +21,7 @@ runs:
         cd
         curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
         sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-        kubectl version
+        kubectl version --short
       shell: bash
     - name: Deploy updated images
       run: echo "Update images with version ${{ inputs.image_tag }}"

.github/workflows/backend.yml
@@ -13,7 +13,7 @@ jobs:
       matrix:
         dotnet: ["6.0.x"]
     steps:
-      - uses: actions/checkout@v2
+      - uses: actions/checkout@v3
       - name: Setup dotnet
         uses: actions/setup-dotnet@v1
         with:
@@ -43,7 +43,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - name: Checkout repository
-        uses: actions/checkout@v2
+        uses: actions/checkout@v3
       # Manually install .NET to work around:
       # https://github.com/github/codeql-action/issues/757
       - name: Setup .NET
@@ -67,16 +67,18 @@ jobs:
 
   docker_build:
     runs-on: ubuntu-latest
+    if: ${{ github.event.type }} == "PullRequest"
     steps:
       # For subfolders, currently a full checkout is required.
       # See: https://github.com/marketplace/actions/build-and-push-docker-images#path-context
-      - name: Checkout
-        uses: actions/checkout@v2
-      - name: Build
-        id: docker_build
-        uses: docker/build-push-action@v2
+      - uses: actions/checkout@v3
         with:
-          context: Backend
-          push: false
+          fetch-depth: 0
+      - name: Build backend
+        run: |
+          deploy/scripts/build.py --components backend
+        shell: bash
       - name: Image digest
-        run: echo ${{ steps.docker_build.outputs.digest }}
+        run: |
+          docker image inspect combine_backend:latest -f '{{json .Id}}'
+        shell: bash

.github/workflows/deploy_live.yml
@@ -1,33 +1,0 @@
-name: "Deploy Update to Live Server"
-
-on:
-  push:
-    branches: [live]
-
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    outputs:
-      image_tag: ${{ steps.build_combine.outputs.image_tag }}
-    steps:
-      - uses: actions/checkout@v2
-      - name: Build The Combine
-        id: build_combine
-        uses: ./.github/actions/combine-build
-        with:
-          image_repo: ${{ secrets.AWS_ACCOUNT }}.dkr.ecr.${{ secrets.AWS_DEFAULT_REGION }}.amazonaws.com
-          aws_access_key_id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          aws_secret_access_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-          aws_default_region: ${{ secrets.AWS_DEFAULT_REGION }}
-  deploy_update:
-    needs: build
-    runs-on: [self-hosted, thecombine]
-    steps:
-      - uses: actions/checkout@v2
-      - name: Deploy The Combine Update
-        uses: ./.github/actions/combine-deploy-update
-        with:
-          image_repo: ${{ secrets.AWS_ACCOUNT }}.dkr.ecr.${{ secrets.AWS_DEFAULT_REGION }}.amazonaws.com
-          image_tag: ${{ needs.build.outputs.image_tag }}
-          kube_context: ${{ secrets.LTOPS_K8S_PRODUCTION_CONTEXT }}
-          update_cert_proxy: true

.github/workflows/deploy_qa.yml
@@ -6,39 +6,50 @@ on:
 
 jobs:
   build:
-    env:
-      QA_IMAGE_PATTERN: \\d+\\.\\d+\\.\\d+-[a-z]+\\.\\d+-build\\.\\d+
+    strategy:
+      matrix:
+        component: [frontend, backend, maintenance]
     runs-on: ubuntu-latest
     outputs:
       image_tag: ${{ steps.build_combine.outputs.image_tag }}
     steps:
-      - uses: actions/checkout@v2
-
-      - name: Create tag suffix
-        run: |
-          echo "TAG_SUFFIX=-build.$GITHUB_RUN_NUMBER" >> $GITHUB_ENV
-
+      - uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
       - name: Build The Combine
         id: build_combine
         uses: ./.github/actions/combine-build
         with:
-          image_repo: ${{ secrets.AWS_ACCOUNT }}.dkr.ecr.${{ secrets.AWS_DEFAULT_REGION }}.amazonaws.com
-          tag_suffix: ${{ env.TAG_SUFFIX }}
+          image_registry: ${{ secrets.AWS_ACCOUNT }}.dkr.ecr.${{ secrets.AWS_DEFAULT_REGION }}.amazonaws.com
           aws_access_key_id: ${{ secrets.AWS_ACCESS_KEY_ID }}
           aws_secret_access_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
           aws_default_region: ${{ secrets.AWS_DEFAULT_REGION }}
-
+          build_component: ${{ matrix.component }}
+  clean_ecr_repo:
+    needs: build
+    env:
+      RM_PATTERN_1: \d+\.\d+\.\d+-master\.\d+
+      RM_PATTERN_2: \d+\.\d+\.\d+-[a-z]+\.\d+-master\.\d+
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Configure AWS credentials
+        uses: aws-actions/configure-aws-credentials@v1
+        with:
+          aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          aws-region: ${{ secrets.AWS_DEFAULT_REGION }}
       - name: Remove old AWS ECR images
-        run: scripts/clean_aws_repo.py combine_frontend combine_backend combine_maint --keep ${{ env.IMAGE_TAG }} --remove ${{ env.QA_IMAGE_PATTERN }}
+        run: scripts/clean_aws_repo.py combine_frontend combine_backend combine_maint --keep ${{ needs.build.outputs.image_tag }} --remove "${{ env.RM_PATTERN_1 }}" "${{ env.RM_PATTERN_2 }}" --verbose
   deploy_update:
     needs: build
     runs-on: [self-hosted, thecombine]
     steps:
-      - uses: actions/checkout@v2
+      - uses: actions/checkout@v3
       - name: Deploy The Combine Update
         uses: ./.github/actions/combine-deploy-update
         with:
-          image_repo: ${{ secrets.AWS_ACCOUNT }}.dkr.ecr.${{ secrets.AWS_DEFAULT_REGION }}.amazonaws.com
+          image_registry: ${{ secrets.AWS_ACCOUNT }}.dkr.ecr.${{ secrets.AWS_DEFAULT_REGION }}.amazonaws.com
           image_tag: ${{ needs.build.outputs.image_tag }}
           kube_context: ${{ secrets.LTOPS_K8S_STAGING_CONTEXT }}
           update_cert_proxy: false

.github/workflows/deploy_release.yml
@@ -1,0 +1,46 @@
+name: "Deploy Update to Live Server"
+
+on:
+  release:
+    branches: [master]
+    types: [published]
+
+jobs:
+  build:
+    strategy:
+      matrix:
+        component: [frontend, backend, maintenance]
+    runs-on: ubuntu-latest
+    outputs:
+      image_tag: ${{ steps.build_combine.outputs.image_tag }}
+    steps:
+      - uses: actions/checkout@v3
+      - name: Build The Combine
+        id: build_combine
+        uses: ./.github/actions/combine-build
+        with:
+          image_registry: public.ecr.aws
+          image_registry_alias: "/x1l7u0y0"
+          aws_access_key_id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          aws_secret_access_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          # Note that the region for the public registries is always us-east-1 regardless of
+          # the account's default region. See the section "To authenticate Docker to an Amazon
+          # ECR registry with get-login-password in
+          # https://docs.aws.amazon.com/AmazonECR/latest/public/public-registries.html
+          aws_default_region: us-east-1
+          build_component: ${{ matrix.component }}
+  deploy_update:
+    needs: build
+    runs-on: [self-hosted, thecombine]
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
+      - name: Deploy The Combine Update
+        uses: ./.github/actions/combine-deploy-update
+        with:
+          image_registry: public.ecr.aws
+          image_registry_alias: "/x1l7u0y0"
+          image_tag: ${{ needs.build.outputs.image_tag }}
+          kube_context: ${{ secrets.LTOPS_K8S_STAGING_CONTEXT }}
+          update_cert_proxy: false

.github/workflows/frontend.yml
@@ -11,9 +11,9 @@ jobs:
     runs-on: ubuntu-latest
     strategy:
       matrix:
-        node-version: [16.x]
+        node-version: [16.14]
     steps:
-      - uses: actions/checkout@v2
+      - uses: actions/checkout@v3
       - name: Use Node.js ${{ matrix.node-version }}
         uses: actions/setup-node@v1
         with:
@@ -39,11 +39,16 @@ jobs:
 
   docker_build:
     runs-on: ubuntu-latest
+    if: ${{ github.event.type }} == "PullRequest"
     steps:
-      - name: Build
-        id: docker_build
-        uses: docker/build-push-action@v2
+      - uses: actions/checkout@v3
         with:
-          push: false
+          fetch-depth: 0
+      - name: Build frontend
+        run: |
+          deploy/scripts/build.py --components frontend
+        shell: bash
       - name: Image digest
-        run: echo ${{ steps.docker_build.outputs.digest }}
+        run: |
+          docker image inspect combine_frontend:latest -f '{{json .Id}}'
+        shell: bash

commit a8f8ffc223f4d57e4a3a0a6300d0cb3d675a08fa

Fix build command for Rancher Desktop
Fix error introduced in PR #1677.  Previous implementation used
os.system which takes a string.  #1677 changed to use subprocess.run
which takes a List.  When building for Rancher Desktop, the namespace
was part of the build_cmd string instead of separate elements in the
list.

deploy/scripts/build.py
@@ -92,9 +92,9 @@ def main() -> None:
     logging.basicConfig(format="%(levelname)s:%(message)s", level=log_level)
 
     if args.nerdctl:
-        build_cmd = f"nerdctl -n {args.namespace}"
+        build_cmd = ["nerdctl", "-n", args.namespace]
     else:
-        build_cmd = "docker"
+        build_cmd = ["docker"]
 
     if args.components is not None:
         to_do = args.components
@@ -120,13 +120,13 @@ def main() -> None:
         logging.info(f"Building component {component}")
         print_all = args.output_level == "all"
         run_cmd(
-            [build_cmd, "build", "-t", image_name, "-f", "Dockerfile", "."],
+            build_cmd + ["build", "-t", image_name, "-f", "Dockerfile", "."],
             print_cmd=print_all,
             print_output=print_all,
         )
         logging.info(f"{component} build complete")
         if args.repo is not None:
-            run_cmd([build_cmd, "push", image_name])
+            run_cmd(build_cmd + ["push", image_name])
             logging.info(f"{image_name} pushed to {args.repo}")
 
     # Remove the version file