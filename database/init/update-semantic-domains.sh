721a3ee4feab00a8e2d794bc140f8c9b2d12c4ac
Jim Grady <jimgrady.jg@gmail.com>
2022-10-18 17:12:47 -0400

    Change update mode for Semantic Domain database items (#1746)
    * Change `mongoimport` command in database initialization scripts to use `--mode=upsert` instead of `--mode=merge`.

--- database/init/update-semantic-domains.sh
+++ database/init/update-semantic-domains.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
-mongoimport -d CombineDatabase -c SemanticDomainTree /data/semantic-domains/tree.json --mode=merge --upsertFields=id,guid,lang
-mongoimport -d CombineDatabase -c SemanticDomains /data/semantic-domains/nodes.json --mode=merge --upsertFields=id,guid,lang
+mongoimport -d CombineDatabase -c SemanticDomainTree /data/semantic-domains/tree.json --mode=upsert --upsertFields=id,guid,lang
+mongoimport -d CombineDatabase -c SemanticDomains /data/semantic-domains/nodes.json --mode=upsert --upsertFields=id,guid,lang
