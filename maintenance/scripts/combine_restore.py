9a5bfeb672a521c1af10c3ee2398874876d68d07
Jim Grady <jimgrady.jg@gmail.com>
2023-01-09 14:12:41 -0500

    Add type hints

--- maintenance/scripts/combine_restore.py
+++ maintenance/scripts/combine_restore.py
@@ -123,26 +123,23 @@ def main() -> None:
         step.print("Unpack the backup.")
         os.chdir(restore_dir)
         with tarfile.open(restore_file, "r:gz") as tar:
-            def is_within_directory(directory, target):
-                
-                abs_directory = os.path.abspath(directory)
-                abs_target = os.path.abspath(target)
-            
-                prefix = os.path.commonprefix([abs_directory, abs_target])
-                
+
+            def is_within_directory(directory: Path, target: Path) -> bool:
+
+                abs_directory = Path(directory).absolute()
+                abs_target = Path(target).absolute()
+
+                prefix = Path(os.path.commonprefix([abs_directory, abs_target]))
+
                 return prefix == abs_directory
-            
-            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-            
-                for member in tar.getmembers():
-                    member_path = os.path.join(path, member.name)
-                    if not is_within_directory(path, member_path):
-                        raise Exception("Attempted Path Traversal in Tar File")
-            
-                tar.extractall(path, members, numeric_owner) 
-                
-            
-            safe_extract(tar)
+
+            path = Path(".")
+            for member in tar.getmembers():
+                member_path = path / member.name
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+
+            tar.extractall(path, None, numeric_owner=False)
 
         step.print("Restore the database.")
         db_pod = combine.get_pod_id(CombineApp.Component.Database)
