f1f8b1e2a7ba3d041e9a1cc1b1316376db7d834d
fuencui <cui.fu@northeastern.edu>
2023-02-15 09:17:53 -0800

    Fix front-end test

--- src/components/DataEntry/DataEntryTable/DataEntryTable.tsx
+++ src/components/DataEntry/DataEntryTable/DataEntryTable.tsx
@@ -168,12 +168,14 @@ export class DataEntryTable extends React.Component<
 
     const addedWord = await backend.createWord(wordToAdd);
     const wordId = await this.addAudiosToBackend(addedWord.id, audioURLs);
-    await this.updateExisting();
-
     const word = await backend.getWord(wordId);
     //update userId to new word before check ignoreRecent
-    word.senses[0].semanticDomains[0].userId = getCurrentUser()?.id;
+    if (word && word.senses && word.senses[0].semanticDomains.length) {
+      word.senses[0].semanticDomains[0].userId = getCurrentUser()?.id;
+    }
     await backend.updateWord(word);
+
+    await this.updateExisting();
 
     if (ignoreRecent) {
       return;
