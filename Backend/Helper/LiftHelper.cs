b2f1cd39a8129593f3c2221ea05736a0c0282c83
D. Ror <imnasnainaec@gmail.com>
2023-04-17 17:12:23 -0400

    Adjust protections

--- Backend/Helper/LiftHelper.cs
+++ Backend/Helper/LiftHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using SIL.Lift.Parsing;
 
 namespace BackendFramework.Helper
@@ -11,8 +12,9 @@ namespace BackendFramework.Helper
         public static bool IsProtected(LiftEntry entry)
         {
             return entry.Annotations.Count > 0 || entry.Etymologies.Count > 0 || entry.Fields.Count > 0 ||
-                entry.Pronunciations.Count > 0 || entry.Relations.Count > 0 || entry.Traits.Count > 0 ||
-                entry.Variants.Count > 0;
+                (entry.Notes.Count == 1 && String.IsNullOrEmpty(entry.Notes.First().Type)) ||
+                entry.Notes.Count > 1 || entry.Pronunciations.Count > 0 || entry.Relations.Count > 0 ||
+                entry.Traits.Any(t => !t.Value.Equals("stem")) || entry.Variants.Count > 0;
         }
 
         /// <summary>
@@ -21,8 +23,9 @@ namespace BackendFramework.Helper
         public static bool IsProtected(LiftSense sense)
         {
             return sense.Examples.Count > 0 || sense.Fields.Count > 0 || sense.GramInfo != null ||
-                sense.Illustrations.Count > 0 || sense.Relations.Count > 0 || sense.Reversals.Count > 0 ||
-                sense.Subsenses.Count > 0 || (sense.Traits.Find(t => !t.Name.StartsWith("semantic-domain")) != null);
+                sense.Illustrations.Count > 0 || sense.Notes.Count > 0 || sense.Relations.Count > 0 ||
+                sense.Reversals.Count > 0 || sense.Subsenses.Count > 0 ||
+                (sense.Traits.Any(t => !t.Name.StartsWith("semantic-domain")));
         }
     }
 }
