fdd6f3f6df2616b6f3fc8f8522ceb5c408e0a107
Jim Grady <jimgrady.jg@gmail.com>
2023-05-05 10:56:38 -0400

    Python formatting

--- scripts/generate_rtk_query.py
+++ scripts/generate_rtk_query.py
@@ -76,9 +76,9 @@ def main() -> None:
     if args.backend:
         logging.info("Starting backend process")
         backend_job = subprocess.Popen(
-                    ["dotnet", "run"],
-                    cwd=project_dir / "Backend",
-                )
+            ["dotnet", "run"],
+            cwd=project_dir / "Backend",
+        )
         # replace with a more elegant solution, i.e. read stdout/stderr to see
         # if process started or if it exited
         time.sleep(5)
@@ -89,29 +89,29 @@ def main() -> None:
     logging.info(f"Controllers:\n{controllers}")
     # create the configuration file for openapi-codegen
     with open(openapi_config, "w") as output_file:
-        output_file.writelines([
-            'import type { ConfigFile } from "@rtk-query/codegen-openapi";\n\n',
-            'const config: ConfigFile = {\n',
-            f'  schemaFile: "{config["schemaFile"]}",\n',
-            f'  apiFile: "{config["apiFile"]}",\n',
-            f'  apiImport: "{config["apiImport"]}",\n',
-            '  outputFiles: {\n',
-        ])
+        output_file.writelines(
+            [
+                'import type { ConfigFile } from "@rtk-query/codegen-openapi";\n\n',
+                "const config: ConfigFile = {\n",
+                f'  schemaFile: "{config["schemaFile"]}",\n',
+                f'  apiFile: "{config["apiFile"]}",\n',
+                f'  apiImport: "{config["apiImport"]}",\n',
+                "  outputFiles: {\n",
+            ]
+        )
         for controller in controllers:
             api_filename = f"./api/{controller}-api.ts"
-            output_file.writelines([
-                f'    "{api_filename}": {{\n',
-                f'      filterEndpoints: {controllers[controller]},\n'
-                 '    },\n',
-            ])
-        output_file.writelines([
-            '  },\n',
-            '  hooks: true\n',
-            '};\n\n',
-            'export default config\n'
-        ])
-    openapi_filename = f"./{openapi_config.relative_to(project_dir)}"
-    subprocess.run(["npx", "@rtk-query/codegen-openapi", openapi_filename], cwd=project_dir)
+            output_file.writelines(
+                [
+                    f'    "{api_filename}": {{\n',
+                    f"      filterEndpoints: {controllers[controller]},\n" "    },\n",
+                ]
+            )
+        output_file.writelines(["  },\n", "  hooks: true\n", "};\n\n", "export default config\n"])
+    subprocess.run(
+        ["npx", "@rtk-query/codegen-openapi", openapi_config.relative_to(project_dir)],
+        cwd=project_dir,
+    )
     subprocess.run(["npm", "run", "lint:fix-layout"], cwd=project_dir)
     if backend_job is not None:
         backend_status = backend_job.poll()
@@ -120,6 +120,7 @@ def main() -> None:
             logging.info("Backend terminated.")
         else:
             logging.info(f"Backend exited with return code: {backend_status}.")
+
 
 if __name__ == "__main__":
     main()
