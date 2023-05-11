b1dcaf6711a5b653d1a14bdf58771df892879f1f
D. Ror <imnasnainaec@gmail.com>
2023-05-11 17:15:23 -0400

    Remove extraneous Fragment wrapper.

--- src/components/DataEntry/DataEntryTable/EntryCellComponents/VernWithSuggestions.tsx
+++ src/components/DataEntry/DataEntryTable/EntryCellComponents/VernWithSuggestions.tsx
@@ -36,40 +36,38 @@ export default function VernWithSuggestions(
   });
 
   return (
-    <React.Fragment>
-      <Autocomplete
-        id={props.textFieldId}
-        disabled={props.isDisabled}
-        // freeSolo allows use of a typed entry not available as a drop-down option
-        freeSolo
-        value={props.vernacular}
-        options={props.suggestedVerns ?? []}
-        onBlur={props.onBlur}
-        onChange={(_e, value) => {
-          // onChange is triggered when an option is selected
-          props.updateVernField(value ?? "", true);
-        }}
-        onInputChange={(_e, value) => {
-          // onInputChange is triggered by typing
-          props.updateVernField(value);
-        }}
-        onKeyPress={(e: React.KeyboardEvent) => {
-          if (e.key === Key.Enter || e.key === Key.Tab) {
-            e.preventDefault();
-            props.handleEnterAndTab(e);
-          }
-        }}
-        onClose={props.onClose}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            fullWidth
-            inputRef={props.vernInput}
-            label={props.isNew ? props.vernacularLang.name : ""}
-            variant={props.isNew ? "outlined" : "standard"}
-          />
-        )}
-      />
-    </React.Fragment>
+    <Autocomplete
+      id={props.textFieldId}
+      disabled={props.isDisabled}
+      // freeSolo allows use of a typed entry not available as a drop-down option
+      freeSolo
+      value={props.vernacular}
+      options={props.suggestedVerns ?? []}
+      onBlur={props.onBlur}
+      onChange={(_e, value) => {
+        // onChange is triggered when an option is selected
+        props.updateVernField(value ?? "", true);
+      }}
+      onInputChange={(_e, value) => {
+        // onInputChange is triggered by typing
+        props.updateVernField(value);
+      }}
+      onKeyPress={(e: React.KeyboardEvent) => {
+        if (e.key === Key.Enter || e.key === Key.Tab) {
+          e.preventDefault();
+          props.handleEnterAndTab(e);
+        }
+      }}
+      onClose={props.onClose}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          fullWidth
+          inputRef={props.vernInput}
+          label={props.isNew ? props.vernacularLang.name : ""}
+          variant={props.isNew ? "outlined" : "standard"}
+        />
+      )}
+    />
   );
 }
