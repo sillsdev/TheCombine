f62db5f8891851c00826b9069f2660f172252eb8
Danny Rorabaugh <imnasnainaec@gmail.com>
2023-08-01 17:32:19 -0400

    Only download fonts for specified languages

--- deploy/scripts/fetch_fonts.py
+++ deploy/scripts/fetch_fonts.py
@@ -4,13 +4,10 @@ Generates font support for all fonts used in Mui-Language-Picker.
 """
 
 import argparse
-import json
 import logging
 import os
 from pathlib import Path
 from shutil import rmtree
-from typing import List
-import urllib.request
 
 import requests
 
@@ -100,119 +97,6 @@ def main() -> None:
             if line[:4] == "src:":
                 src_parts = line.split("'")
                 logging.info(f"Downloading {src_parts[1]}")
-
-        """
-
-        # Get font info from the file of font families.
-        if not nrsi_id in families.keys():
-            logging.warning(f"Font {nrsi_id} not in file {families_file_path}")
-            continue
-        font_info: dict = families[nrsi_id]
-        family: str = font_info["family"]
-
-        # Determine which format to use, with preference for woff2 if available.
-        defaults: dict = font_info["defaults"]
-        format = ""
-        for key in ["woff2", "woff", "ttf"]:
-            if key in defaults.keys():
-                format = key
-                break
-        if format == "":
-            logging.warning(f"{family}: 'ttf', 'woff', 'woff2' formats all unavailable")
-            continue
-        logging.info(f"{family}: Using format '{format}'")
-
-        # Determine the naming convention in this font's files.
-        default_name: str = defaults[format]
-        logging.info(f"{family}: default {default_name}")
-        if "Regular." in default_name:
-            # Most fonts
-            file_name_prefix = default_name.split("-Regular.")[0]
-            style_suffixes = ["-Bold", "-BoldItalic", "-Italic", "-Regular"]
-        elif "-R." in default_name:
-            # Khmer Mondulkiri
-            file_name_prefix = default_name.split("-R.")[0]
-            style_suffixes = ["-B", "-BI", "-I", "-R"]
-        elif "R." in default_name:
-            # Galatia SIL, Sophia Nubian
-            file_name_prefix = default_name.split("R.")[0]
-            style_suffixes = ["B", "BI", "I", "R"]
-        else:
-            # Ezra SIL
-            file_name_prefix = default_name.split(".")[0]
-            style_suffixes = [""]
-
-        if "files" not in font_info.keys():
-            logging.warning(f"{family}: no file list")
-            continue
-
-    
-        files: dict = font_info["files"]
-        for style_suffix in style_suffixes:
-            file_name = f"{file_name_prefix}{style_suffix}.{format}"
-            if file_name in files.keys():
-                # Build the css info for this font in this style.
-                css_lines.append("@font-face {\n")
-                css_lines.append("  font-display: swap;\n")
-                css_lines.append(f"  font-family: '{mlp_family}';\n")
-                variable_name = f"{file_name_prefix}_"
-                if style_suffix in ["-Regular", "-R", "R", ""]:
-                    css_lines.append("  font-style: normal;\n")
-                    css_lines.append("  font-weight: normal;\n")
-                    css_line_local = f"local('{family}'), local('{family} Regular'),"
-                    variable_name += "Regular"
-                elif style_suffix in ["-Bold", "-B", "B"]:
-                    css_lines.append("  font-style: normal;\n")
-                    css_lines.append("  font-weight: bold;\n")
-                    css_line_local = f"local('{family} Bold'),"
-                    variable_name += "Bold"
-                elif style_suffix in ["-Italic", "-I", "I"]:
-                    css_lines.append("  font-style: italic;\n")
-                    css_lines.append("  font-weight: normal;\n")
-                    css_line_local = f"local('{family} Italic'),"
-                    variable_name += "Italic"
-                elif style_suffix in ["-BoldItalic", "-BI", "BI"]:
-                    css_lines.append("  font-style: italic;\n")
-                    css_lines.append("  font-weight: bold;\n")
-                    css_line_local = f"local('{family} Bold Italic'),"
-                    variable_name += "BoldItalic"
-                else:
-                    logging.error(f"{family}: Impossible font style suffix: {style_suffix}")
-                    continue
-
-                # Build the url source, downloading if requested.
-                dest = Path.joinpath(subdir, file_name)
-                if args.download:
-                    if file_name not in files.keys():
-                        logging.error(f"{family}: No such file: {file_name}")
-                        continue
-                    file_info: dict = files[file_name]
-                    if "flourl" not in file_info.keys():
-                        logging.info(f"{file_name}: No 'flourl' for this file")
-                        if "url" not in file_info.keys():
-                            logging.warning(f"{file_name}: No 'flourl' or 'url' for this file")
-                            continue
-                        src = file_info["url"]
-                    else:
-                        src = file_info["flourl"]
-
-                    # With the https://fonts.languagetechnology.org "flourl" urls,
-                    # urllib.request.urlretrieve() is denied (403), but requests.get() works.
-                    req = requests.get(src)
-                    logging.info(f"Downloading {src} to {dest}")
-                    with open(dest, "wb") as out:
-                        out.write(req.content)
-
-                # Finish the css info for this font in this style.
-                css_lines.append(f"  src: {css_line_local} url('{dest}');\n")
-                css_lines.append("}\n")
-
-        # Create font override file
-        css_file_path = subdir.joinpath("fontFace.css")
-        logging.info(f"Writing css info for font family: {css_file_path}")
-        with open(css_file_path, "w") as css_file:
-            css_file.writelines(css_lines)
-        """
 
 
 if __name__ == "__main__":
--- deploy/scripts/get_fonts.py
+++ deploy/scripts/get_fonts.py
@@ -4,6 +4,7 @@ Generates font support for all SIL fonts used in Mui-Language-Picker.
 """
 
 import argparse
+from csv import reader
 import json
 import logging
 import os
@@ -22,7 +23,11 @@ def parse_args() -> argparse.Namespace:
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("-c", "--clean", action="store_true", help="Delete the fonts directory")
-    parser.add_argument("-d", "--download", action="store_true", help="Download fonts")
+    parser.add_argument(
+        "-l",
+        "--langs",
+        help="Comma-separated list of lang-tags of the languages for which fonts should be downloaded.",
+    )
     parser.add_argument(
         "-r",
         "--root",
@@ -39,14 +44,25 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def getFontDefault(defaults: dict) -> str:
+    """Determine which default file to use, with preference for woff2 if available."""
+    keys = ["woff2", "woff", "ttf"]
+    for key in keys:
+        if key in defaults.keys():
+            return defaults[key]
+    return ""
+
+
 def checkFontInfo(font_info: dict) -> bool:
     family: str = font_info["family"]
 
     # Check that the font is current and licensed as expected.
     if font_info["distributable"] != True:
         logging.warning(f"{family}: Not distributable")
+        return False
     if "license" not in font_info.keys():
         logging.warning(f"{family}: No license")
+        return False
     elif font_info["license"] != "OFL":
         logging.warning(f"{family}: Non-OFL license: {font_info['license']}")
     if "source" not in font_info.keys():
@@ -58,15 +74,85 @@ def checkFontInfo(font_info: dict) -> bool:
     elif font_info["status"] != "current":
         logging.warning(f"{family}: Non-current status: {font_info['status']}")
 
-    if "defaults" not in font_info.keys() or len(font_info["defaults"]) == 0:
+    if "defaults" not in font_info.keys() or getFontDefault(font_info["defaults"]) == "":
         logging.warning(f"{family}: No defaults")
         return False
 
     if "files" not in font_info.keys():
-        logging.warning(f"{family}: no file list")
+        logging.warning(f"{family}: No file list")
         return False
 
     return True
+
+
+def extractLangSubtags(langs: str, sep=",") -> List[str]:
+    """Given a string of (comma-separated) lang tags, return list of the initial language subtags."""
+    tags: List[str] = [lang.strip() for lang in langs.split(sep) if lang.strip() != ""]
+    subtags = [tag.split("-")[0].lower() for tag in tags]
+    langs = [subtag for subtag in set(subtags) if subtag != ""]
+    langs.sort()
+    return langs
+
+
+def fetchScriptsForLangs(langs: List[str]) -> List[str]:
+    """Given a list of language tags, look up and return all script tags used with the languages."""
+    langs = [lang.lower() for lang in langs]
+    scripts = []
+    ldml_langtags_source = "https://ldml.api.sil.org/en?query=langtags"
+    req = requests.get(ldml_langtags_source)
+    for line in req.iter_lines():
+        tags: List[str] = line.decode("UTF-8").split(" = ")
+        if len(tags) == 0 or tags[0].split("-")[0].strip("*") not in langs:
+            continue
+        for tag in tags:
+            for subtag in tag.split("-"):
+                if len(subtag) == 4 and subtag not in scripts:
+                    scripts.append(subtag)
+
+    scripts.sort()
+    return scripts
+
+
+def fetchFontsForScripts(scripts: List[str]) -> List[str]:
+    """Given a list of script tags, look up and return the default fonts used with the languages."""
+    scripts = [script.capitalize() for script in scripts]
+
+    # Always include the Mui-Language-Picker default/safe fonts (except "SimSun", which is proprietary).
+    fonts = ["AnnapurnaSIL", "CharisSIL", "DoulosSIL", "NotoSans", "ScheherazadeNew"]
+
+    script_font_source = (
+        "https://raw.githubusercontent.com/silnrsi/langfontfinder/main/data/script2font.csv"
+    )
+    req = requests.get(script_font_source)
+    script_font_table = reader(
+        req.content.decode("UTF-8").splitlines(), delimiter=",", quotechar='"'
+    )
+
+    # Get the font-column indices
+    header_row: List[str] = next(script_font_table)
+    font_index = header_row.index("Default Font")
+    font_indices = [
+        i for i in range(font_index, len(header_row)) if header_row[i] != "Default Features"
+    ]
+
+    for row in script_font_table:
+        if len(row) == 0 or row[0] not in scripts:
+            continue
+        for i in font_indices:
+            font = row[i].replace(" ", "")
+            if font != "" and font not in fonts:
+                fonts.append(font)
+
+    fonts.sort()
+    return fonts
+
+
+def fetchFontFamiliesInfo(targetPath: str) -> dict:
+    source_url = "https://github.com/silnrsi/fonts/raw/main/families.json"
+    urllib.request.urlretrieve(source_url, targetPath)
+    with open(targetPath, "r") as families_file:
+        families: dict = json.load(families_file)
+    return families
 
 
 def main() -> None:
@@ -80,9 +166,16 @@ def main() -> None:
     if not os.path.exists(root_dir):
         logging.error("Root directory specified with --root_dir not valid")
         exit(1)
-
-    # Assumes script is run from The Combine's root directory
     target_dir = root_dir.joinpath("fonts")
+
+    langs = extractLangSubtags(args.langs)
+    logging.info(f"Lang-tags to download fonts for: {', '.join(langs)}")
+
+    scripts = fetchScriptsForLangs(langs)
+    logging.info(f"Scripts used for specified lang-tags: {', '.join(scripts)}")
+
+    fonts = fetchFontsForScripts(scripts)
+    logging.info(f"Default fonts and fonts used for specified lang-tags: {', '.join(fonts)}")
 
     if args.clean:
         logging.info(f"Deleting {target_dir}")
@@ -93,154 +186,91 @@ def main() -> None:
         logging.info(f"Making {target_dir}")
         os.mkdir(target_dir)
 
-    source_url = "https://github.com/silnrsi/fonts/raw/main/families.json"
     families_file_path = target_dir.joinpath("families.json")
-    logging.info(f"Downloading {source_url} to {families_file_path}")
-    urllib.request.urlretrieve(source_url, families_file_path)
+    logging.info(f"Downloading font families info to {families_file_path}")
+    families = fetchFontFamiliesInfo(families_file_path)
 
     # Todo: Add info for these font's directly to this script.
-    mlp_families_not_in_file = {"simsun": "Sim Sun", "notosanstangut": "Noto Serif Tangut"}
+    # mlp_families_not_in_file = {"notosanstangut": "Noto Serif Tangut"}
+    # with open("deploy/scripts/font_lists/mui-language-picker-fonts.txt", "r") as mlp_families_file:
+    #    mlp_families = [f.strip() for f in mlp_families_file.readlines()]
+    # with open("deploy/scripts/font_lists/mlp-nrsi-font-map.json", "r") as mlp_nrsi_map_file:
+    #    mlp_nrsi_map: dict = json.load(mlp_nrsi_map_file)
 
-    with open(families_file_path, "r") as nrsi_families_file:
-        families: dict = json.load(nrsi_families_file)
+    with open("deploy/scripts/font_lists/nrsi-mlp-font-map.json", "r") as nrsi_mlp_map_file:
+        nrsi_mlp_map: dict = json.load(nrsi_mlp_map_file)
 
-    with open("deploy/scripts/font_lists/mui-language-picker-fonts.txt", "r") as mlp_families_file:
-        mlp_families = [f.strip() for f in mlp_families_file.readlines()]
-        mlp_ids = [f.strip().replace(" ", "").lower() for f in mlp_families_file.readlines()]
+    for font in fonts:
+        font_id = font.lower()
+        logging.info(f"Font: {font}")
 
-    with open("deploy/scripts/font_lists/mlp-nrsi-font-map.json", "r") as mlp_nrsi_map_file:
-        mlp_nrsi_map: dict = json.load(mlp_nrsi_map_file)
-
-    for mlp_family in mlp_families:
-        mlp_id = mlp_family.replace(" ", "").lower()
-        nrsi_id = mlp_nrsi_map[mlp_id] if mlp_id in mlp_nrsi_map.keys() else mlp_id
-        logging.info(f"Font: {mlp_id}/{nrsi_id}")
-
-        # Get font info from the file of font families, using fallback font if necessary
-        while nrsi_id != "" and nrsi_id in families.keys():
-            font_info: dict = families[nrsi_id]
+        # Get font family info from font families info, using fallback font if necessary
+        while font_id != "" and font_id in families.keys():
+            font_info: dict = families[font_id]
             family = font_info["family"]
             if checkFontInfo(font_info):
                 break
             if "fallback" in font_info.keys():
-                nrsi_id: str = font_info["fallback"]
-                logging.warning(f"{family}: Using fallback {nrsi_id}")
+                font_id: str = font_info["fallback"]
+                logging.warning(f"{family}: Using fallback {font_id}")
             else:
                 logging.warning(f"{family}: No fallback")
-                nrsi_id = ""
+                font_id = ""
         else:
-            if nrsi_id != "":
-                logging.warning(f"Font {nrsi_id} not in file {families_file_path}")
+            if font_id != "":
+                logging.warning(f"Font {font_id} not in file {families_file_path}")
             continue
 
-        # Determine which format to use, with preference for woff2 if available.
-        defaults: dict = font_info["defaults"]
-        format = ""
-        for key in ["woff2", "woff", "ttf"]:
-            if key in defaults.keys():
-                format = key
-                break
-        if format == "":
-            logging.warning(f"{family}: 'ttf', 'woff', 'woff2' formats all unavailable")
-            continue
-        logging.info(f"{family}: Using format '{format}'")
-
-        # Determine the naming convention in this font's files.
-        default_name: str = defaults[format]
-        logging.info(f"{family}: default {default_name}")
-        if "-Regular." in default_name:
-            # Most fonts
-            file_name_prefix = default_name.split("-Regular.")[0]
-            style_suffixes = ["-Bold", "-BoldItalic", "-Italic", "-Regular"]
-        elif "REGULAR." in default_name:
-            # Aboriginal Sans
-            file_name_prefix = default_name.split("REGULAR.")[0]
-            style_suffixes = ["BOLD", "BOLDITALIC", "ITALIC", "REGULAR"]
-        elif "-R." in default_name:
-            # Khmer Mondulkiri
-            file_name_prefix = default_name.split("-R.")[0]
-            style_suffixes = ["-B", "-BI", "-I", "-R"]
-        elif "R." in default_name:
-            # Galatia SIL, Sophia Nubian
-            file_name_prefix = default_name.split("R.")[0]
-            style_suffixes = ["B", "BI", "I", "R"]
-        else:
-            # Ezra SIL
-            file_name_prefix = default_name.split(".")[0]
-            style_suffixes = [""]
-
-        logging.info(f"{family}: Populating font subfolder '{mlp_id}'")
-        subdir = Path.joinpath(target_dir, mlp_id)
-        if not os.path.exists(subdir):
-            os.mkdir(subdir)
-        # To fill with the content of the font family's css file.
-        css_lines: List[str] = []
-
+        # Get the font's default file info.
+        file_name = getFontDefault(font_info["defaults"])
+        logging.info(f"{family}: default {file_name}")
         files: dict = font_info["files"]
-        for style_suffix in style_suffixes:
-            file_name = f"{file_name_prefix}{style_suffix}.{format}"
-            if file_name in files.keys():
-                # Build the css info for this font in this style.
-                css_lines.append("@font-face {\n")
-                css_lines.append("  font-display: swap;\n")
-                css_lines.append(f"  font-family: '{mlp_family}';\n")
-                variable_name = f"{file_name_prefix}_"
-                if style_suffix in ["-Regular", "REGULAR", "-R", "R", ""]:
-                    css_lines.append("  font-style: normal;\n")
-                    css_lines.append("  font-weight: normal;\n")
-                    css_line_local = f"local('{family}'), local('{family} Regular'),"
-                    variable_name += "Regular"
-                elif style_suffix in ["-Bold", "BOLD", "-B", "B"]:
-                    css_lines.append("  font-style: normal;\n")
-                    css_lines.append("  font-weight: bold;\n")
-                    css_line_local = f"local('{family} Bold'),"
-                    variable_name += "Bold"
-                elif style_suffix in ["-Italic", "ITALIC" "-I", "I"]:
-                    css_lines.append("  font-style: italic;\n")
-                    css_lines.append("  font-weight: normal;\n")
-                    css_line_local = f"local('{family} Italic'),"
-                    variable_name += "Italic"
-                elif style_suffix in ["-BoldItalic", "BOLDITALIC", "-BI", "BI"]:
-                    css_lines.append("  font-style: italic;\n")
-                    css_lines.append("  font-weight: bold;\n")
-                    css_line_local = f"local('{family} Bold Italic'),"
-                    variable_name += "BoldItalic"
-                else:
-                    logging.error(f"{family}: Impossible font style suffix: {style_suffix}")
-                    continue
+        if file_name not in files.keys():
+            logging.error(f"{family}: Default file not in file list")
+            continue
+        file_info: dict = files[file_name]
 
-                # Build the url source, downloading if requested.
-                dest = Path.joinpath(subdir, file_name)
-                if args.download:
-                    if file_name not in files.keys():
-                        logging.error(f"{family}: No such file: {file_name}")
-                        continue
-                    file_info: dict = files[file_name]
-                    if "flourl" not in file_info.keys():
-                        logging.info(f"{file_name}: No 'flourl' for this file")
-                        if "url" not in file_info.keys():
-                            logging.warning(f"{file_name}: No 'flourl' or 'url' for this file")
-                            continue
-                        src = file_info["url"]
-                    else:
-                        src = file_info["flourl"]
+        # Build the css info for this font in this style.
+        css_lines: List[str] = []
+        css_lines.append("@font-face {\n")
+        css_lines.append("  font-display: swap;\n")
+        css_lines.append(f"  font-family: '{font}';\n")
+        css_line_local = f"local('{family}'), local('{family} Regular'),"
 
-                    # With the https://fonts.languagetechnology.org "flourl" urls,
-                    # urllib.request.urlretrieve() is denied (403), but requests.get() works.
-                    req = requests.get(src)
-                    logging.info(f"Downloading {src} to {dest}")
-                    with open(dest, "wb") as out:
-                        out.write(req.content)
+        # Build the url source, downloading if requested.
+        if "flourl" not in file_info.keys():
+            logging.info(f"{file_name}: No 'flourl' for this file")
+            if "url" not in file_info.keys():
+                logging.warning(f"{file_name}: No 'flourl' or 'url' for this file")
+                continue
+            src = file_info["url"]
+        else:
+            src = file_info["flourl"]
 
-                # Finish the css info for this font in this style.
-                css_lines.append(f"  src: {css_line_local} url('{dest}');\n")
-                css_lines.append("}\n")
+        # With the https://fonts.languagetechnology.org "flourl" urls,
+        # urllib.request.urlretrieve() is denied (403), but requests.get() works.
+        req = requests.get(src)
+        dest = Path.joinpath(target_dir, file_name)
+        logging.info(f"Downloading {src} to {dest}")
+        with open(dest, "wb") as out:
+            out.write(req.content)
+
+        # Finish the css info for this font in this style.
+        css_lines.append(f"  src: {css_line_local} url('{dest}');\n")
+        css_lines.append("}\n")
 
         # Create font override file
-        css_file_path = subdir.joinpath("fontFace.css")
+        css_file_path = target_dir.joinpath(f"{font}.css")
         logging.info(f"Writing css info for font family: {css_file_path}")
         with open(css_file_path, "w") as css_file:
             css_file.writelines(css_lines)
+        if font in nrsi_mlp_map.keys():
+            font = nrsi_mlp_map[font]
+            css_file_path = target_dir.joinpath(f"{font}.css")
+            css_lines[2] = f"  font-family: '{font}';\n"
+            logging.info(f"Writing css info for font family: {css_file_path}")
+            with open(css_file_path, "w") as css_file:
+                css_file.writelines(css_lines)
 
 
 if __name__ == "__main__":
