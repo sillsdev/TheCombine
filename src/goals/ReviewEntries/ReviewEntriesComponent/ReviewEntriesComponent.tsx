0a6c1d8ecf28999b82d6ff86092c26305dd582ed
Danny Rorabaugh <imnasnainaec@gmail.com>
2023-08-16 16:32:31 -0400

    Revert ReviewEntriesComponent.tsx

--- src/goals/ReviewEntries/ReviewEntriesComponent/ReviewEntriesComponent.tsx
+++ src/goals/ReviewEntries/ReviewEntriesComponent/ReviewEntriesComponent.tsx
@@ -3,8 +3,6 @@ import { ReactElement, useEffect, useState } from "react";
 import { getFrontierWords } from "backend";
 import ReviewEntriesTable from "goals/ReviewEntries/ReviewEntriesComponent/ReviewEntriesTable";
 import { ReviewEntriesWord } from "goals/ReviewEntries/ReviewEntriesComponent/ReviewEntriesTypes";
-import { StoreState } from "types";
-import { useAppSelector } from "types/hooks";
 
 // Component state/props
 interface ReviewEntriesProps {
@@ -19,23 +17,15 @@ interface ReviewEntriesProps {
 export default function ReviewEntriesComponent(
   props: ReviewEntriesProps
 ): ReactElement {
-  const proj = useAppSelector(
-    (state: StoreState) => state.currentProjectState.project
-  );
-
   const [loaded, setLoaded] = useState(false);
   const { updateAllWords, updateFrontierWord } = props;
 
-  const analysisLang = proj.analysisWritingSystems[0]?.bcp47;
-
   useEffect(() => {
     getFrontierWords().then((frontier) => {
-      updateAllWords(
-        frontier.map((w) => new ReviewEntriesWord(w, analysisLang))
-      );
+      updateAllWords(frontier.map((w) => new ReviewEntriesWord(w)));
       setLoaded(true);
     });
-  }, [analysisLang, proj.id, updateAllWords]);
+  }, [updateAllWords]);
 
   return loaded ? (
     <ReviewEntriesTable
