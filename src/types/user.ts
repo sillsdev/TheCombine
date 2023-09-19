6f5e2daf63e0ba29e9bdacc99314c5c10e207d22
Danny Rorabaugh <imnasnainaec@gmail.com>
2023-09-19 13:47:27 -0400

    Add comment before doesTextMatchUser

--- src/types/user.ts
+++ src/types/user.ts
@@ -17,6 +17,8 @@ export function newUser(name = "", username = "", password = ""): User {
   };
 }
 
+/** Returns whether the given `text` is a (case-insensitive) substring of the
+ * name, username, or email of the given `user`. */
 export function doesTextMatchUser(text: string, user: User): boolean {
   const lower = text.toLocaleLowerCase();
   return (
