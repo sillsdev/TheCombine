4e0e2575934e8af087be73220bd0ad86a76e6b23
D. Ror <imnasnainaec@gmail.com>
2023-12-07 11:45:32 -0500

    Consolidate buttons (#2826)

---
+++ src/components/Dialogs/UploadImageDialog.tsx
@@ -0,0 +1,30 @@
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import { ReactElement } from "react";
+import { useTranslation } from "react-i18next";
+
+import UploadImage from "components/Dialogs/UploadImage";
+
+interface UploadImageDialogProps {
+  close: () => void;
+  open: boolean;
+  titleId: string;
+  uploadImage: (imageFile: File) => Promise<void>;
+}
+
+export default function UploadImageDialog(
+  props: UploadImageDialogProps
+): ReactElement {
+  const { t } = useTranslation();
+
+  return (
+    <Dialog onClose={props.close} open={props.open}>
+      <DialogTitle>{t(props.titleId)}</DialogTitle>
+      <DialogContent>
+        <UploadImage
+          doneCallback={props.close}
+          uploadImage={props.uploadImage}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
