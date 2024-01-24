41c3ba602279883a82086e047639a665c96f2a48
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-01-24 17:46:31 -0500

    Only show distinct items in domains column, not sense separated

--- src/goals/ReviewEntries/ReviewEntriesTable/Cells/DomainsCell.tsx
+++ src/goals/ReviewEntries/ReviewEntriesTable/Cells/DomainsCell.tsx
@@ -1,36 +1,29 @@
-import { Chip, Grid, Typography } from "@mui/material";
+import { Chip, Grid } from "@mui/material";
 import { ReactElement } from "react";
 
+import { SemanticDomain, Sense } from "api/models";
 import { CellProps } from "goals/ReviewEntries/ReviewEntriesTable/Cells/CellTypes";
 
+/** Collect all distinct sense.semanticDomains entries. */
+function gatherDomains(senses: Sense[]): SemanticDomain[] {
+  return senses
+    .flatMap((s) => s.semanticDomains)
+    .reduce<SemanticDomain[]>((a, dom) => {
+      const { id, name } = dom;
+      return !id || a.some((d) => d.id === id && d.name === name)
+        ? a
+        : [...a, dom];
+    }, []);
+}
+
 export default function DomainsCell(props: CellProps): ReactElement {
-  const items: ReactElement[] = [];
-
-  props.rowData.senses.forEach((sense) => {
-    if (!sense.semanticDomains.length) {
-      return;
-    }
-
-    if (items.length) {
-      items.push(
-        <Grid item key={`${sense.guid}-sep`}>
-          <Typography>{"|"}</Typography>
-        </Grid>
-      );
-    }
-
-    items.push(
-      ...sense.semanticDomains.map((d) => (
-        <Grid item key={`${sense.guid}-${d.id}-${d.name}`}>
-          <Chip label={`${d.id}: ${d.name}`} />
-        </Grid>
-      ))
-    );
-  });
-
   return (
     <Grid container direction="row" spacing={1}>
-      {items}
+      {gatherDomains(props.rowData.senses).map((dom) => (
+        <Grid item key={`${dom.id}-${dom.name}`}>
+          <Chip label={`${dom.id}: ${dom.name}`} />
+        </Grid>
+      ))}
     </Grid>
   );
 }
