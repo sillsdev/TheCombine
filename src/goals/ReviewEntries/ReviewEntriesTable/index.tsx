2c5147e90deda20ce6af7062fc8e439bf6bc0b27
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-01-24 17:38:56 -0500

    Pluralize Parts of Speech

--- src/goals/ReviewEntries/ReviewEntriesTable/index.tsx
+++ src/goals/ReviewEntries/ReviewEntriesTable/index.tsx
@@ -164,7 +164,7 @@ export default function ReviewEntriesTable(): ReactElement {
         value: g,
       })),
       filterVariant: "select",
-      header: t("reviewEntries.columns.partOfSpeech"),
+      header: t("reviewEntries.columns.partsOfSpeech"),
       id: "partsOfSpeech",
       sortingFn: (rowA, rowB) =>
         compareWordGrammaticalInfo(rowA.original, rowB.original),
