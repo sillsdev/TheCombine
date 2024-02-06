e3a2779b6cc02a493d8532948ad6c6ececefb211
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-02-06 18:13:55 -0500

    Expand ReviewEntriesTable testing

--- src/goals/ReviewEntries/ReviewEntriesTable/tests/index.test.tsx
+++ src/goals/ReviewEntries/ReviewEntriesTable/tests/index.test.tsx
@@ -1,14 +1,14 @@
+import { MRT_TableHeadCell } from "material-react-table";
 import { Provider } from "react-redux";
-import { act, create } from "react-test-renderer";
+import { type ReactTestRenderer, act, create } from "react-test-renderer";
 import configureMockStore from "redux-mock-store";
 
 import "tests/reactI18nextMock";
 
-import { CurrentProjectState } from "components/Project/ProjectReduxTypes";
+import { defaultState } from "components/Project/ProjectReduxTypes";
 import ReviewEntriesTable from "goals/ReviewEntries/ReviewEntriesTable";
-import { newProject } from "types/project";
+import { type StoreState } from "types";
 import { newWord } from "types/word";
-import { defaultWritingSystem } from "types/writingSystem";
 
 const mockGetFrontierWords = jest.fn();
 const mockUuid = jest.fn();
@@ -19,51 +19,88 @@ jest.mock("uuid", () => ({ v4: () => mockUuid() }));
 jest.mock("backend", () => ({
   getAllSpeakers: () => Promise.resolve([]),
   getFrontierWords: (...args: any[]) => mockGetFrontierWords(...args),
+  getWord: () => jest.fn(),
 }));
 jest.mock("components/GoalTimeline/Redux/GoalActions", () => ({}));
+jest.mock("goals/ReviewEntries/ReviewEntriesTable/Cells/EditCell/EditDialog");
 jest.mock("types/hooks", () => ({
   ...jest.requireActual("types/hooks"),
   useAppDispatch: () => jest.fn(),
 }));
 
-// Mock store + axios
-const mockWords = [newWord()];
-const currentProjectState: Partial<CurrentProjectState> = {
-  project: {
-    ...newProject(),
-    analysisWritingSystems: [defaultWritingSystem],
-    definitionsEnabled: true,
-    grammaticalInfoEnabled: true,
-    vernacularWritingSystem: defaultWritingSystem,
+const mockState = (
+  definitionsEnabled = false,
+  grammaticalInfoEnabled = false
+): Partial<StoreState> => ({
+  currentProjectState: {
+    ...defaultState,
+    project: {
+      ...defaultState.project,
+      definitionsEnabled,
+      grammaticalInfoEnabled,
+    },
   },
+});
+const mockWords = [newWord()];
+
+let renderer: ReactTestRenderer;
+
+const renderReviewEntriesTable = async (
+  definitionsEnabled = false,
+  grammaticalInfoEnabled = false
+): Promise<void> => {
+  const mockStore = configureMockStore()(
+    mockState(definitionsEnabled, grammaticalInfoEnabled)
+  );
+  await act(async () => {
+    renderer = create(
+      <Provider store={mockStore}>
+        <ReviewEntriesTable />
+      </Provider>
+    );
+  });
 };
-const mockStore = configureMockStore()({ currentProjectState });
 
 function setMockFunctions(): void {
   jest.clearAllMocks();
   mockGetFrontierWords.mockResolvedValue(mockWords);
 }
 
-beforeEach(async () => {
-  // Prep for component creation
+beforeEach(() => {
   setMockFunctions();
   for (const word of mockWords) {
     for (const sense of word.senses) {
       mockUuid.mockImplementationOnce(() => sense.guid);
     }
   }
-
-  await act(async () => {
-    create(
-      <Provider store={mockStore}>
-        <ReviewEntriesTable />
-      </Provider>
-    );
-  });
 });
 
 describe("ReviewEntriesTable", () => {
-  it("fetches frontier when it initializes", () => {
+  it("fetches frontier when it initializes", async () => {
+    await renderReviewEntriesTable();
     expect(mockGetFrontierWords).toHaveBeenCalled();
+  });
+
+  describe("definitionsEnabled & grammaticalInfoEnabled", () => {
+    const definitionsId = "definitions";
+    const partOfSpeechId = "partOfSpeech";
+
+    test("show definitions when definitionsEnabled is true", async () => {
+      await renderReviewEntriesTable(true, false);
+      const colIds = renderer.root
+        .findAllByType(MRT_TableHeadCell)
+        .map((col) => col.props.header.id);
+      expect(colIds).toContain(definitionsId);
+      expect(colIds).not.toContain(partOfSpeechId);
+    });
+
+    test("show part of speech when grammaticalInfoEnabled is true", async () => {
+      await renderReviewEntriesTable(false, true);
+      const colIds = renderer.root
+        .findAllByType(MRT_TableHeadCell)
+        .map((col) => col.props.header.id);
+      expect(colIds).not.toContain(definitionsId);
+      expect(colIds).toContain(partOfSpeechId);
+    });
   });
 });
