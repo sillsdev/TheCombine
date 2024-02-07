dd6403e765e6db12416cd10da040dcc18103a744
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-02-06 19:04:09 -0500

    Mock out uuid and start developing mockWords for sort testing

--- src/goals/ReviewEntries/ReviewEntriesTable/tests/index.test.tsx
+++ src/goals/ReviewEntries/ReviewEntriesTable/tests/index.test.tsx
@@ -6,16 +6,26 @@ import configureMockStore from "redux-mock-store";
 
 import "tests/reactI18nextMock";
 
+import { type Word } from "api/models";
 import { defaultState } from "components/Project/ProjectReduxTypes";
 import ReviewEntriesTable from "goals/ReviewEntries/ReviewEntriesTable";
 import { type StoreState } from "types";
-import { newWord } from "types/word";
-
-const mockGetFrontierWords = jest.fn();
-const mockUuid = jest.fn();
+import { newSense, newWord } from "types/word";
 
 jest.mock("@mui/material/Grow"); // For `columnFilterDisplayMode: "popover",`
-jest.mock("uuid", () => ({ v4: () => mockUuid() }));
+jest.mock("uuid");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mockUuid = require("uuid") as { v4: jest.Mock };
+
+let uuidIndex = 0;
+// getMockUuid(false) gives the next uuid to be assigned by our mocked v4.
+function getMockUuid(increment = true): string {
+  const uuid = `mockUuid${uuidIndex}`;
+  if (increment) {
+    uuidIndex++;
+  }
+  return uuid;
+}
 
 jest.mock("backend", () => ({
   getAllSpeakers: () => Promise.resolve([]),
@@ -30,6 +40,7 @@ jest.mock("types/hooks", () => ({
 }));
 
 const mockClickEvent = { stopPropagation: jest.fn() };
+const mockGetFrontierWords = jest.fn();
 const mockState = (
   definitionsEnabled = false,
   grammaticalInfoEnabled = false
@@ -43,7 +54,23 @@ const mockState = (
     },
   },
 });
-const mockWords = [newWord()];
+const mockWords = (): Word[] => [
+  { ...newWord("Alfa"), senses: [newSense("Echo")] },
+  { ...newWord("Delta"), senses: [newSense("Foxtrot")] },
+  { ...newWord("Bravo"), senses: [newSense("Hotel")] },
+  { ...newWord("Charlie"), senses: [newSense("Golf")] },
+];
+/*const sortOrder = [
+  [0, 2, 3, 1], // Vernacular
+  [0, 1, 2, 3], // Senses
+  [0, 1, 2, 3], // Definitions
+  [0, 1, 3, 2], // Glosses
+  [0, 1, 2, 3], // PartOfSpeech
+  [0, 1, 2, 3], // Domains
+  [0, 1, 2, 3], // Pronunciations
+  [0, 1, 2, 3], // Note
+  [0, 1, 2, 3], // Flag
+];*/
 
 let renderer: ReactTestRenderer;
 
@@ -65,16 +92,12 @@ const renderReviewEntriesTable = async (
 
 function setMockFunctions(): void {
   jest.clearAllMocks();
-  mockGetFrontierWords.mockResolvedValue(mockWords);
+  mockUuid.v4.mockImplementation(getMockUuid);
+  mockGetFrontierWords.mockResolvedValue(mockWords());
 }
 
 beforeEach(() => {
   setMockFunctions();
-  for (const word of mockWords) {
-    for (const sense of word.senses) {
-      mockUuid.mockImplementationOnce(() => sense.guid);
-    }
-  }
 });
 
 describe("ReviewEntriesTable", () => {
