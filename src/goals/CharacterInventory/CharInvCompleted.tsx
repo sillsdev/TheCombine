2b2953846837e6283f687a821692874880996f70
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-02-19 16:25:35 -0500

    Add changed words to CharInvCompleted

--- src/goals/CharacterInventory/CharInvCompleted.tsx
+++ src/goals/CharacterInventory/CharInvCompleted.tsx
@@ -1,22 +1,27 @@
 import { ArrowRightAlt } from "@mui/icons-material";
-import { Typography } from "@mui/material";
-import { ReactElement } from "react";
+import { List, ListItem, Typography } from "@mui/material";
+import { type ReactElement, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useSelector } from "react-redux";
 
+import { type Word } from "api/models";
+import { getWord } from "backend";
+import WordCard from "components/WordCard";
 import CharacterStatusText from "goals/CharacterInventory/CharInv/CharacterList/CharacterStatusText";
 import {
-  CharacterChange,
-  CharInvChanges,
+  type CharInvChanges,
+  type CharacterChange,
 } from "goals/CharacterInventory/CharacterInventoryTypes";
-import { StoreState } from "types";
+import { type StoreState } from "types";
+import { useAppSelector } from "types/hooks";
 
 export default function CharInvCompleted(): ReactElement {
-  const changes = useSelector(
+  const changes = useAppSelector(
     (state: StoreState) =>
       state.goalsState.currentGoal.changes as CharInvChanges
   );
   const { t } = useTranslation();
+
+  const entryChanges = Object.entries(changes.wordChanges ?? {});
 
   return (
     <>
@@ -24,9 +29,22 @@ export default function CharInvCompleted(): ReactElement {
         {t("charInventory.title")}
       </Typography>
       {changes.charChanges?.length ? (
-        changes.charChanges.map((c) => <CharInvChange change={c} key={c[0]} />)
+        changes.charChanges.map((c) => <CharChange change={c} key={c[0]} />)
       ) : (
-        <Typography>{t("charInventory.changes.noChanges")}</Typography>
+        <Typography>{t("charInventory.changes.noCharChanges")}</Typography>
+      )}
+      <br />
+      {entryChanges.length ? (
+        <>
+          <Typography>{t("charInventory.changes.wordChanges")}</Typography>
+          <List>
+            {entryChanges.map(([oldId, newId]) => (
+              <WordChangeListItem key={newId} oldId={oldId} newId={newId} />
+            ))}
+          </List>
+        </>
+      ) : (
+        <Typography>{t("charInventory.changes.noWordChanges")}</Typography>
       )}
     </>
   );
@@ -36,20 +54,35 @@ export function CharInvChangesGoalList(changes: CharInvChanges): ReactElement {
   const { t } = useTranslation();
   const changeLimit = 3;
 
+  const wordsChanged = Object.keys(changes.wordChanges ?? {}).length;
+  const wordChangesTypography = wordsChanged ? (
+    <Typography>{`${t(
+      "charInventory.changes.wordChanges"
+    )} ${wordsChanged}`}</Typography>
+  ) : (
+    <Typography>{t("charInventory.changes.noWordChanges")}</Typography>
+  );
+
   if (!changes.charChanges?.length) {
-    return <Typography>{t("charInventory.changes.noChanges")}</Typography>;
+    return (
+      <>
+        <Typography>{t("charInventory.changes.noCharChanges")}</Typography>
+        {wordChangesTypography}
+      </>
+    );
   }
   if (changes.charChanges.length > changeLimit) {
     return (
       <>
         <Typography />
         {changes.charChanges.slice(0, changeLimit - 1).map((c) => (
-          <CharInvChange change={c} key={c[0]} />
+          <CharChange change={c} key={c[0]} />
         ))}
         <Typography>
           {`+${changes.charChanges.length - 3} `}
           {t("charInventory.changes.more")}
         </Typography>
+        {wordChangesTypography}
       </>
     );
   }
@@ -57,13 +90,14 @@ export function CharInvChangesGoalList(changes: CharInvChanges): ReactElement {
     <>
       <Typography />
       {changes.charChanges.map((c) => (
-        <CharInvChange change={c} key={c[0]} />
+        <CharChange change={c} key={c[0]} />
       ))}
+      {wordChangesTypography}
     </>
   );
 }
 
-function CharInvChange(props: { change: CharacterChange }): ReactElement {
+function CharChange(props: { change: CharacterChange }): ReactElement {
   return (
     <>
       <Typography display="inline">{`${props.change[0]}: `}</Typography>
@@ -72,5 +106,28 @@ function CharInvChange(props: { change: CharacterChange }): ReactElement {
       <CharacterStatusText status={props.change[2]} inline />
       <Typography />
     </>
+  );
+}
+
+function WordChangeListItem(props: {
+  newId: string;
+  oldId: string;
+}): ReactElement {
+  const [oldVern, setOldVern] = useState("");
+  const [newWord, setNewWord] = useState<Word | undefined>();
+
+  useEffect(() => {
+    getWord(props.oldId).then((w) => setOldVern(w.vernacular));
+    getWord(props.newId).then(setNewWord);
+  }, [props]);
+
+  const vernacular = `${oldVern}  →  ${newWord?.vernacular}`;
+
+  return (
+    <ListItem>
+      {oldVern && newWord ? (
+        <WordCard word={{ ...newWord, vernacular }} />
+      ) : null}
+    </ListItem>
   );
 }
