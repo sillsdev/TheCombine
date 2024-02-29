5d13a71c1e7e89bccbb596aa2c12294386ecb298
D. Ror <imnasnainaec@gmail.com>
2024-02-29 10:52:24 -0500

    Update i18n settings

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,7 @@
 // https://dev.to/adrai/how-to-properly-internationalize-a-react-application-using-i18next-3hdb#getting-started
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+import Backend, { type HttpBackendOptions } from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 import { getCurrentUser } from "backend/localStorage";
@@ -18,17 +18,16 @@ i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init(
+  .init<HttpBackendOptions>(
     {
-      //debug: true, // Uncomment to troubleshoot
+      debug: true, // Uncomment to troubleshoot
       returnNull: false,
-      // detection: options,
       // ignoring localStorage and cookies for the detection order lets the user change languages
       // more easily (just switch in the browser and reload, instead of clearing all site data)
       detection: { order: ["queryString", "path", "navigator"] },
       supportedLngs: i18nLangs,
-      // nonExplicitSupportedLngs will (e.g.) use 'es' if the browser is 'es-MX'
-      nonExplicitSupportedLngs: true,
+      // "languageOnly" will (e.g.) only load 'es' if the browser is 'es-MX'
+      load: "languageOnly",
       fallbackLng: i18nFallbacks,
       interpolation: { escapeValue: false },
     },
