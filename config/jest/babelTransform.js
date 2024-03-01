792b89ce9cde57bbccbe498e5dcd90b54a7b89f2
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-03-01 12:01:25 -0500

    Remove unneeded babel preset

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -3,10 +3,7 @@
 const babelJest = require("babel-jest").default;
 
 module.exports = babelJest.createTransformer({
-  presets: [
-    require.resolve("@babel/preset-env"),
-    require.resolve("@babel/preset-typescript"),
-  ],
+  presets: [require.resolve("@babel/preset-env")],
   babelrc: false,
   configFile: false,
 });
