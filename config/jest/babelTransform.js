3de9d73dfdfd283c448cabd599a0a8b38003aa1d
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-03-01 10:46:59 -0500

    Remove babel conflict

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -2,23 +2,10 @@
 
 const babelJest = require("babel-jest").default;
 
-const hasJsxRuntime = (() => {
-  try {
-    require.resolve("react/jsx-runtime");
-    return true;
-  } catch (e) {
-    return false;
-  }
-})();
-
 module.exports = babelJest.createTransformer({
   presets: [
-    [
-      require.resolve("babel-preset-react-app"),
-      {
-        runtime: hasJsxRuntime ? "automatic" : "classic",
-      },
-    ],
+    require.resolve("@babel/preset-env"),
+    require.resolve("@babel/preset-typescript"),
   ],
   babelrc: false,
   configFile: false,
