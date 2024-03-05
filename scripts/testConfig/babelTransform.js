323a550347f9d423749c24435ff856f15511f6c0
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-03-05 16:25:41 -0500

    Try babel-preset-react-app settings

--- scripts/testConfig/babelTransform.js
+++ scripts/testConfig/babelTransform.js
@@ -4,13 +4,11 @@ const babelJest = require("babel-jest").default;
 
 module.exports = babelJest.createTransformer({
   presets: [
-    require.resolve("@babel/preset-env"),
     [
-      require.resolve("@babel/preset-react"),
-      {
-        runtime: "automatic",
-      },
+      require.resolve("@babel/preset-env"),
+      { useBuiltIns: "entry", corejs: 3, exclude: ["transform-typeof-symbol"] },
     ],
+    [require.resolve("@babel/preset-react"), { runtime: "automatic" }],
     require.resolve("@babel/preset-typescript"),
   ],
   babelrc: false,
