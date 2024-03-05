a5076682dbf9b4d680a750dba65c0f32ebc5f279
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-03-05 16:26:06 -0500

    Revert

--- scripts/testConfig/babelTransform.js
+++ scripts/testConfig/babelTransform.js
@@ -4,10 +4,7 @@ const babelJest = require("babel-jest").default;
 
 module.exports = babelJest.createTransformer({
   presets: [
-    [
-      require.resolve("@babel/preset-env"),
-      { useBuiltIns: "entry", corejs: 3, exclude: ["transform-typeof-symbol"] },
-    ],
+    require.resolve("@babel/preset-env"),
     [require.resolve("@babel/preset-react"), { runtime: "automatic" }],
     require.resolve("@babel/preset-typescript"),
   ],
