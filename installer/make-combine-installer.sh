84f8944501ee47c1a3135a4d63155e74364defd4
Jim Grady <jimgrady.jg@gmail.com>
2024-03-18 14:51:55 -0400

    Pass Combine version to installer

--- installer/make-combine-installer.sh
+++ installer/make-combine-installer.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
 
-makeself --tar-quietly ../deploy ./combine-installer.run "Combine Installer" scripts/install-combine
+if [[ $# -gt 0 ]] ; then
+  COMBINE_VERSION=$1
+fi
+if [ -z "${COMBINE_VERSION}" ] ; then
+  read -p "Enter Combine version to install: " COMBINE_VERSION
+fi
+makeself --tar-quietly ../deploy ./combine-installer.run "Combine Installer" scripts/install-combine.sh ${COMBINE_VERSION}
