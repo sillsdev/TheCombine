c6c5e860b41d42b1b49b6ef254fa681cbe2f8c21
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-03-20 11:34:49 -0400

    Fix line missed in merge

--- src/components/WordCard/tests/SenseCard.test.tsx
+++ src/components/WordCard/tests/SenseCard.test.tsx
@@ -2,8 +2,6 @@ import { Provider } from "react-redux";
 import { type ReactTestRenderer, act, create } from "react-test-renderer";
 import configureMockStore from "redux-mock-store";
 
-import "tests/reactI18nextMock";
-
 import { GramCatGroup, type Sense } from "api";
 import { PartOfSpeechButton } from "components/Buttons";
 import { defaultState } from "components/Project/ProjectReduxTypes";
