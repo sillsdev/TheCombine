cfe09e1abe7e9c44f5b6b4c02fd477c0a0720a32
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-03-28 09:30:58 -0400

    Trim scripts/test* files

--- scripts/test.js
+++ scripts/test.js
@@ -4,18 +4,17 @@
 process.env.BABEL_ENV = "test";
 process.env.NODE_ENV = "test";
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+// Make the script crash on unhandled rejections instead of silently ignoring them.
 process.on("unhandledRejection", (err) => {
   throw err;
 });
 
 // Ensure environment variables are read.
-require("./testConfig/env");
+require("./testConfig/envTest");
 
 const jest = require("jest");
-let argv = process.argv.slice(2);
+
+const argv = process.argv.slice(2);
 
 // Watch unless on CI or explicitly running all tests
 if (
--- scripts/testConfig/envTest.js
+++ scripts/testConfig/envTest.js
@@ -3,28 +3,17 @@
 const fs = require("fs");
 const path = require("path");
 
-const NODE_ENV = process.env.NODE_ENV;
-if (!NODE_ENV) {
-  throw new Error(
-    "The NODE_ENV environment variable is required but was not specified."
-  );
-}
-
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebook/create-react-app/pull/648
 const appDirectory = fs.realpathSync(process.cwd());
 const dotenv = path.resolve(appDirectory, ".env");
 
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
-const dotenvFiles = [
-  `${dotenv}.${NODE_ENV}.local`,
-  `${dotenv}.${NODE_ENV}`,
-  dotenv,
-].filter(Boolean);
+const dotenvFiles = [`${dotenv}.test.local`, `${dotenv}.test`, dotenv];
 
-// Load environment variables from .env* files. Suppress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.  Variable expansion is supported in .env files.
+// Load environment variables from .env* files, ignoring missing files.
+// `dotenv` won't modify environment variables that are already set.
+// Variable expansion is supported in .env files.
 // https://github.com/motdotla/dotenv
 // https://github.com/motdotla/dotenv-expand
 dotenvFiles.forEach((dotenvFile) => {
