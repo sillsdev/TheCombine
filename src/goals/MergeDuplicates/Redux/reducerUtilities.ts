5ca7f0a835391e945a66b396195a054269f0a9b3
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-04-03 14:07:13 -0400

    Restore reducerUtilities summary comment

--- src/goals/MergeDuplicates/Redux/reducerUtilities.ts
+++ src/goals/MergeDuplicates/Redux/reducerUtilities.ts
@@ -14,6 +14,8 @@ import { newMergeWords } from "goals/MergeDuplicates/MergeDupsTypes";
 import { type Hash } from "types/hash";
 import { compareFlags } from "utilities/wordUtilities";
 
+// A collection of helper/utility functions only for use in the MergeDupsReducer.
+
 /** Generate dictionary of MergeTreeSense arrays:
  * - key: word id
  * - value: all merge senses of the word, with Deleted/Separate accessibility */
