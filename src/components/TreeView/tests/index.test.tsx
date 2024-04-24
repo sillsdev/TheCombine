9b356d276ba7ff887723f0626a3641655521fa3e
D. Ror <imnasnainaec@gmail.com>
2024-04-24 15:31:27 -0400

    Fix tests

--- src/components/TreeView/tests/index.test.tsx
+++ src/components/TreeView/tests/index.test.tsx
@@ -1,3 +1,4 @@
+import { match } from "css-mediaquery";
 import { Provider } from "react-redux";
 import renderer from "react-test-renderer";
 import configureMockStore from "redux-mock-store";
@@ -38,10 +39,28 @@ const mockStore = configureMockStore([thunk])({
 const findById = (id: string): renderer.ReactTestInstance =>
   treeMaster.root.findByProps({ id });
 
+// Modified from mui.com/material-ui/react-use-media-query/#testing
+// For <Hidden> elements to show up: window.matchMedia = createMatchMedia(...)
+const matchMedia = (width: number): ((query: string) => MediaQueryList) => {
+  return (query: string) =>
+    ({ matches: match(query, { width }), addListener: jest.fn() }) as any;
+};
+
+const muiSM = 600;
+
 describe("TreeView", () => {
-  it("renders with top button and no exit button by default", async () => {
-    await renderTree();
+  it("renders without top button in xs windows", async () => {
+    await renderTree(undefined, muiSM - 1);
+    expect(() => findById(topButtonId)).toThrow();
+  });
+
+  it("renders with top button in sm+ windows", async () => {
+    await renderTree(undefined, muiSM);
     expect(() => findById(topButtonId)).not.toThrow();
+  });
+
+  it("renders with no exit button by default", async () => {
+    await renderTree();
     expect(() => findById(exitButtonId)).toThrow();
   });
 
@@ -66,7 +85,10 @@ describe("TreeView", () => {
   });
 });
 
-async function renderTree(exit?: () => void): Promise<void> {
+async function renderTree(exit?: () => void, width?: number): Promise<void> {
+  // Required for <Hidden> elements to show up
+  window.matchMedia = matchMedia(width ?? window.innerWidth);
+
   await renderer.act(async () => {
     treeMaster = renderer.create(
       <Provider store={mockStore}>
