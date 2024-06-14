66fb47cdc520c4045f869b8858ef255c79e09dcb
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-06-14 13:47:34 -0400

    Add missing parenteses

--- Backend/Services/LiftService.cs
+++ Backend/Services/LiftService.cs
@@ -527,7 +527,7 @@ namespace BackendFramework.Services
                 {
                     var orc = new OptionRefCollection();
                     semDomNames.TryGetValue(semDom.Id, out string? name);
-                    orc.Add(semDom.Id + " " + name ?? semDom.Name);
+                    orc.Add(semDom.Id + " " + (name ?? semDom.Name));
                     lexSense.Properties.Add(new KeyValuePair<string, IPalasoDataObjectProperty>(
                         LexSense.WellKnownProperties.SemanticDomainDdp4, orc));
                 }
