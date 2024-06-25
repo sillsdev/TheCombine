9dc36390c992751c5f77427394f75826bb2bc318
Andra Constantin <andrac@princeton.edu>
2024-06-25 15:12:17 -0400

    OtelKernel formatting

--- Backend/Otel/OtelKernel.cs
+++ Backend/Otel/OtelKernel.cs
@@ -1,6 +1,10 @@
+// using System;
 using System.Diagnostics;
-using System.Diagnostics.Metrics;
+// using System.Diagnostics.Metrics;
+// using System.Net.Http;
+// using System.Net.Http.Json;
 using System.Security.Claims;
+// using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry.Metrics;
@@ -20,85 +24,87 @@ namespace BackendFramework.Otel
             var appResourceBuilder = ResourceBuilder.CreateDefault().AddService(ServiceName);
             // todo: include version 
             services.AddOpenTelemetry().WithTracing(tracerProviderBuilder => tracerProviderBuilder
-                        .SetResourceBuilder(appResourceBuilder)
-                        .AddSource(ServiceName)
-                        .AddAspNetCoreInstrumentation(options =>
-                        {
-                            options.RecordException = true;
-                            options.EnrichWithHttpRequest = (activity, request) =>
-                            {
-                                var contentLength = request.Headers.ContentLength;
-                                if (contentLength.HasValue)
-                                {
-                                    activity.SetTag("inbound.http.request.body.size", contentLength.Value);
-                                }
-                                else
-                                {
-                                    activity.SetTag("inbound.http.request.body.size", "no content");
-                                }
-                                activity.EnrichWithUser(request.HttpContext);
-                            };
-                            options.EnrichWithHttpResponse = (activity, response) =>
-                            {
-                                var contentLength = response.Headers.ContentLength;
-                                if (contentLength.HasValue)
-                                {
-                                    activity.SetTag("inbound.http.response.body.size", contentLength.Value);
-                                }
-                                else
-                                {
-                                    activity.SetTag("inbound.http.response.body.size", "no content");
-                                }
-                                activity.EnrichWithUser(response.HttpContext);
-                            };
-                        })
-                        .AddHttpClientInstrumentation(options =>
-                        {
-                            options.EnrichWithHttpRequestMessage = (activity, request) =>
-                            {
-                                var contentLength = request.Content?.Headers.ContentLength;
-                                if (contentLength.HasValue)
-                                {
-                                    activity.SetTag("outbound.http.request.body.size", contentLength.Value);
-                                }
-                                else
-                                {
-                                    activity.SetTag("outbound.http.request.body.size", "no content");
-                                }
-
-                                if (request.RequestUri is not null)
-                                {
-                                    activity.SetTag("url.path", request.RequestUri.AbsolutePath);
-                                    if (!string.IsNullOrEmpty(request.RequestUri.Query))
-                                        activity.SetTag("url.query", request.RequestUri.Query);
-                                }
-                                else
-                                {
-                                    activity.SetTag("outbound.http.response.body.size", "no content");
-                                }
-                            };
-                            options.EnrichWithHttpResponseMessage = (activity, response) =>
-                        {
-                            var contentLength = response.Content.Headers.ContentLength;
-                            if (contentLength.HasValue)
-                            {
-                                activity.SetTag("outbound.http.response.body.size", contentLength.Value);
-                            }
-                        };
-                        })
-                            // .AddSource("MongoDB.Driver.Core.Extensions.DiagnosticSources")
-                            .AddConsoleExporter()
-                            .AddOtlpExporter()
-                            );
-
-            var meter = new Meter(ServiceName);
-            var counter = meter.CreateUpDownCounter<int>("word-count");
-            services.AddOpenTelemetry().WithMetrics(metricProviderBuilder => metricProviderBuilder
                 .SetResourceBuilder(appResourceBuilder)
-                .AddMeter(meter.Name)
-                .AddConsoleExporter()
-                .AddOtlpExporter()
-                );
+                .AddSource(ServiceName)
+                .AddAspNetCoreInstrumentation(options =>
+                {
+                    options.RecordException = true;
+                    options.EnrichWithHttpRequest = (activity, request) =>
+                    {
+                        var contentLength = request.Headers.ContentLength;
+                        if (contentLength.HasValue)
+                        {
+                            activity.SetTag("inbound.http.request.body.size", contentLength.Value);
+                        }
+                        else
+                        {
+                            activity.SetTag("inbound.http.request.body.size", "no content");
+                        }
+                        activity.EnrichWithUser(request.HttpContext);
+                    };
+                    options.EnrichWithHttpResponse = (activity, response) =>
+                    {
+                        var contentLength = response.Headers.ContentLength;
+                        if (contentLength.HasValue)
+                        {
+                            activity.SetTag("inbound.http.response.body.size", contentLength.Value);
+                        }
+                        else
+                        {
+                            activity.SetTag("inbound.http.response.body.size", "no content");
+                        }
+                        activity.EnrichWithUser(response.HttpContext);
+                    };
+                })
+                .AddHttpClientInstrumentation(options =>
+                {
+                    options.EnrichWithHttpRequestMessage = (activity, request) =>
+                    {
+                        var contentLength = request.Content?.Headers.ContentLength;
+                        if (contentLength.HasValue)
+                        {
+                            activity.SetTag("outbound.http.request.body.size", contentLength.Value);
+                        }
+                        else
+                        {
+                            activity.SetTag("outbound.http.request.body.size", "no content");
+                        }
+
+                        if (request.RequestUri is not null)
+                        {
+                            activity.SetTag("url.path", request.RequestUri.AbsolutePath);
+                            if (!string.IsNullOrEmpty(request.RequestUri.Query))
+                                activity.SetTag("url.query", request.RequestUri.Query);
+                        }
+                        else
+                        {
+                            activity.SetTag("outbound.http.response.body.size", "no content");
+                        }
+                    };
+                    options.EnrichWithHttpResponseMessage = (activity, response) =>
+                    {
+                        var contentLength = response.Content.Headers.ContentLength;
+                        if (contentLength.HasValue)
+                        {
+                            activity.SetTag("outbound.http.response.body.size", contentLength.Value);
+                        }
+                    };
+                })
+                    // .AddSource("MongoDB.Driver.Core.Extensions.DiagnosticSources")
+                    .AddConsoleExporter()
+                    .AddOtlpExporter()
+            );
+
+            // var meter = new Meter(ServiceName);
+            // // var counter = meter.CreateCounter<int>("words-created");
+            // services.AddOpenTelemetry().WithMetrics(metricProviderBuilder => metricProviderBuilder
+            //     .SetResourceBuilder(appResourceBuilder)
+            //     .AddMeter(meter.Name)
+            //     .AddAspNetCoreInstrumentation()
+            //     .AddHttpClientInstrumentation()
+            //     .AddConsoleExporter()
+            //     .AddOtlpExporter()
+            // );
         }
 
         private static void EnrichWithUser(this Activity activity, HttpContext httpContext)
