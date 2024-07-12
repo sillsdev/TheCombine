a364a4d0be2625756850820d76ed41a70517098b
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-07-12 10:42:53 -0400

    Fix tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -8,6 +8,13 @@ global.console.warn = (message) => {
   throw message;
 };
 
+// Mock all permissions as granted
+Object.defineProperty(navigator, "permissions", {
+  get() {
+    return { query: () => Promise.resolve({ state: "granted" }) };
+  },
+});
+
 // Mock the audio components
 jest
   .spyOn(window.HTMLMediaElement.prototype, "pause")
