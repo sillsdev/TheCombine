2022e48e340a4582fe08db2cbb047535cd870285
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-07-12 13:17:40 -0400

    Tidy renderOption

--- src/components/DataEntry/DataEntryTable/EntryCellComponents/GlossWithSuggestions.tsx
+++ src/components/DataEntry/DataEntryTable/EntryCellComponents/GlossWithSuggestions.tsx
@@ -78,18 +78,16 @@ export default function GlossWithSuggestions(
           variant={(props.isNew ? "outlined" : "standard") as any}
         />
       )}
-      renderOption={(liProps, option, { selected }) => {
-        return (
-          <LiWithFont
-            {...liProps}
-            analysis
-            aria-selected={selected}
-            lang={props.analysisLang.bcp47}
-          >
-            {SpellChecker.replaceAllButLastWordWithEllipses(option)}
-          </LiWithFont>
-        );
-      }}
+      renderOption={(liProps, option, { selected }) => (
+        <LiWithFont
+          {...liProps}
+          analysis
+          aria-selected={selected}
+          lang={props.analysisLang.bcp47}
+        >
+          {SpellChecker.replaceAllButLastWordWithEllipses(option)}
+        </LiWithFont>
+      )}
       onKeyPress={(e: KeyboardEvent) => {
         if (e.key === Key.Enter) {
           props.handleEnter();
