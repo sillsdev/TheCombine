b9c5f7efd0c5035aae748fdab37a62a5278164f6
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-08-08 15:36:09 -0400

    Ask users for cookie consent

--- src/components/App/component.tsx
+++ src/components/App/component.tsx
@@ -3,12 +3,15 @@ import { RouterProvider } from "react-router-dom";
 
 import AnnouncementBanner from "components/AnnouncementBanner/AnnouncementBanner";
 import UpperRightToastContainer from "components/Toast/UpperRightToastContainer";
+import useCookieConsent from "cookies/useCookieConsent";
 import router from "router/browserRouter";
 
 /**
  * The top-level component
  */
 export default function App(): ReactElement {
+  useCookieConsent();
+
   return (
     <div className="App">
       <Suspense fallback={<div />}>
---
+++ src/cookies/useCookieConsent.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { run } from "vanilla-cookieconsent";
+
+import "vanilla-cookieconsent/dist/cookieconsent.css";
+
+import { defaultWritingSystem } from "types/writingSystem";
+
+export default function useCookieConsent(): void {
+  useEffect(() => {
+    console.info("C is for Cookie");
+    run({
+      categories: {
+        analytics: {
+          /* Coming soon */
+        },
+        necessary: {},
+      },
+      guiOptions: {
+        consentModal: { layout: "bar inline" },
+      },
+      language: {
+        default: defaultWritingSystem.bcp47,
+        translations: {
+          en: {
+            consentModal: {
+              title: "Cookies on The Combine",
+              description:
+                "The Combine stores basic info about your current session on your device. This info is necessary for The Combine to function and is not shared with anybody or any other programs. The Combine also uses analytics cookies, which are only for us to fix bugs and compile anonymized statistics. Do you consent to our usage of analytics cookies?",
+              acceptAllBtn: "Yes, allow analytics cookies",
+              acceptNecessaryBtn: "No, reject analytics cookies",
+            },
+            preferencesModal: { sections: [] },
+          },
+        },
+      },
+    });
+  }, []);
+}
