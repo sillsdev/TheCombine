db48f3f975725023970e4f1cedced17a4f133af0
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-08-16 15:18:24 -0400

    Set version to default (-1)

--- src/rootRedux/testTypes.ts
+++ src/rootRedux/testTypes.ts
@@ -6,5 +6,5 @@ import { defaultState } from "rootRedux/types";
 /** Preloaded values for store when testing */
 export const persistedDefaultState: PreloadedState<RootState> = {
   ...defaultState,
-  _persist: { rehydrated: false, version: 1 },
+  _persist: { rehydrated: false, version: -1 },
 };
