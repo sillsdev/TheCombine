372118da28df3576cdf70a0805475c9b3ec61873
Andra Constantin <andrac@princeton.edu>
2024-09-18 12:16:55 -0400

    check cov with tracerprovider

--- Backend.Tests/Controllers/WordControllerTests.cs
+++ Backend.Tests/Controllers/WordControllerTests.cs
@@ -6,8 +6,10 @@ using Backend.Tests.Mocks;
 using BackendFramework.Controllers;
 using BackendFramework.Interfaces;
 using BackendFramework.Models;
+using BackendFramework.Otel;
 using BackendFramework.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 
 namespace Backend.Tests.Controllers
@@ -389,6 +391,9 @@ namespace Backend.Tests.Controllers
         [Test]
         public async Task TestCreateWord()
         {
+            var service = new ServiceCollection();
+            service.AddOpenTelemetryInstrumentation();
+
             var word = Util.RandomWord(_projId);
 
             var id = (string)((ObjectResult)await _wordController.CreateWord(_projId, word)).Value!;
@@ -399,6 +404,8 @@ namespace Backend.Tests.Controllers
 
             var frontier = await _wordRepo.GetFrontier(_projId);
             Assert.That(frontier[0], Is.EqualTo(word));
+
+
         }
 
         [Test]
