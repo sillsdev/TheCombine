8867a83bd9bd8a9fdba1af3e7ea8cd8a8f2c0e1a
Andra Constantin <andrac@princeton.edu>
2024-09-23 14:52:30 -0400

    save location test draft
    save location test draft

---
+++ Backend.Tests/Otel/LocationProviderTest.cs
@@ -0,0 +1,61 @@
+// using System.Net.Http;
+// using System.Runtime;
+// using System.Threading.Tasks;
+// using BackendFramework.Otel;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.Extensions.Caching.Memory;
+// using NUnit.Framework;
+// using Org.BouncyCastle.Crypto.Operators;
+
+// namespace Backend.Tests.Otel
+// {
+//     public class LocationProviderTests
+//     {
+//         public const string locationGetterUri = "http://ip-api.com/json/";
+//         private IHttpContextAccessor? _contextAccessor;
+//         private IMemoryCache? _memoryCache;
+//         private IHttpClientFactory? _httpClientFactory;
+//         private DraftLocationProvider _locationProvider;
+//         public void Setup()
+//         {
+//             _contextAccessor = new HttpContextAccessor();
+//             _memoryCache = memoryCache;
+//             _httpClientFactory = httpClientFactory;
+//             _locationProvider = new DraftLocationProvider(_contextAccessor, _memoryCache, _httpClientFactory);
+
+//         }
+
+//         [Test]
+//         public void TestGetLocation()
+//         {
+
+
+//         }
+
+//         [Test]
+//         public void TestGetLocationWithCaching()
+//         {
+//             // create a mock context with a set IP
+//             var httpContext = new DefaultHttpContext();
+
+//             // call GetLocation() 
+
+
+//         }
+
+//         [Test]
+//         public void TestGetLocationFromIp()
+//         {
+//             var ipAddressWithoutPort = "100.0.0.0";
+
+//             // how to set up locationProvider
+//             var location = _locationProvider.GetLocationFromIp(ipAddressWithoutPort);
+
+//             // ideally, would create a locationAPI and check that values match
+//             // (but that would also then be quite dependent on implementation, 
+//             // e.g. which fields we are returning)
+//             Assert.That(location, Is.Not.Null);
+
+//         }
+//     }
+// }
