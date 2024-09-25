ad17c22dc31fc23cbd86a44f4b19bd2109a64d21
Andra Constantin <andrac@princeton.edu>
2024-09-25 10:52:14 -0400

    clean otelservice

--- Backend/Otel/OtelService.cs
+++ Backend/Otel/OtelService.cs
@@ -7,9 +7,6 @@ public class OtelService
 {
     public static void AddOtelTag(string key, object? value)
     {
-        // return new ActivitySource("service");
-        // return new ActivitySource(OtelKernel.SourceName);
-
         using var activity = new ActivitySource(OtelKernel.SourceName).StartActivity();
         activity?.AddTag(key, value);
     }
