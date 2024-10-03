51c20c51f5ae75431906a30eb3574a0bbc8b970d
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-10-03 10:46:59 -0400

    Add missing script

---
+++ scripts/createBackendLicenses.js
@@ -0,0 +1,33 @@
+// To be run after generating a JSON license file with nuget-license
+
+const fs = require("fs");
+
+const fileNameNoExt = "docs/user_guide/assets/licenses/backend_licenses";
+const fileNameIn = `${fileNameNoExt}.json`;
+const fileNameOut = `${fileNameNoExt}.txt`;
+
+console.log(`Reading JSON data from ${fileNameIn}`);
+const licenseData = JSON.parse(fs.readFileSync(fileNameIn));
+
+const infoKeys = [
+  "PackageId",
+  "PackageVersion",
+  "PackageProjectUrl",
+  "Authors",
+  "License",
+  "LicenseUrl",
+];
+const sep =
+  "####################################################################################################\n";
+let licensesString = "";
+
+for (const data of licenseData) {
+  licensesString += sep;
+  for (const key of infoKeys) {
+    licensesString += `${key}:${data[key]}\n`;
+  }
+  licensesString += "\n";
+}
+
+console.log(`Writing license data to ${fileNameOut}`);
+fs.writeFileSync(fileNameOut, licensesString);
