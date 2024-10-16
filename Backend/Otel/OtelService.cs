bb874637d5e4f3d5103f9168f7aaac8f9d932104
Andra Constantin <andrac@princeton.edu>
2024-10-16 07:56:40 -0400

    DisplayName should specify function

--- Backend/Otel/OtelService.cs
+++ Backend/Otel/OtelService.cs
@@ -1,19 +1,18 @@
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BackendFramework.Otel
 {
     public class OtelService
     {
-        public static Activity? AddOtelTag(string key, object? value)
+        public static Activity? AddOtelTag(string key, object? value, [CallerMemberName] string activityName = "")
         {
             var activitySource = new ActivitySource(OtelKernel.SourceName);
+            using var activity = activitySource.StartActivity(activityName);
+            activity?.AddTag(key, value);
+            return activity;
 
-            using (var activity = activitySource.StartActivity())
-            {
-                activity?.AddTag(key, value);
-                return activity;
-            }
         }
 
         public static void AddOtelInstrumentation(IServiceCollection services)
