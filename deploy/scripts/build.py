84acfc661a31b65be3b703df70b16edeacd62e91
Danny Rorabaugh <imnasnainaec@gmail.com>
2024-10-24 16:04:31 -0400

    Fix build.py docker debugging

--- deploy/scripts/build.py
+++ deploy/scripts/build.py
@@ -50,7 +50,8 @@ class Job:
 class JobQueue:
     """Class to manage a queue of jobs."""
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, debug: bool = False) -> None:
+        self.debug = debug
         self.name = name
         self.status = JobStatus.RUNNING
         self.job_list: List[Job] = []
@@ -88,6 +89,18 @@ class JobQueue:
         logging.debug(f"{self.name}.start_next(): no more jobs to run.")
         return False
 
+    def print_out(self) -> None:
+        logging.debug("####################")
+        logging.debug("Printing the stdout:\n")
+        self.output_stream.print()
+        logging.debug("####################")
+
+    def print_err(self) -> None:
+        logging.debug("####################")
+        logging.debug("Printing the stderr:\n")
+        self.error_stream.print()
+        logging.debug("####################")
+
     def check_jobs(self) -> JobStatus:
         """
         Check if all jobs in the queue have completed.
@@ -104,12 +117,14 @@ class JobQueue:
             # Current job has finished
             if self.curr_job.returncode == 0:
                 logging.info(f"{self.name} job has finished.")
-                self.output_stream.print()
+                self.print_out()
+                if self.debug:
+                    self.print_err()
             else:
                 logging.error(f"{self.name} job failed.")
-                self.error_stream.print()
+                self.print_err()
                 self.returncode = self.curr_job.returncode
-                # skip remaining jobs
+                # Skip remaining jobs
                 self.job_list = []
                 self.status = JobStatus.ERROR
                 return self.status
@@ -245,13 +260,15 @@ def main() -> None:
 
     # Setup required build engine - docker or nerdctl
     container_cmd = [os.getenv("CONTAINER_CLI", "docker")]
-    if args.debug:
-        container_cmd.append("--debug")
     match container_cmd[0]:
         case "nerdctl":
+            if args.debug:
+                container_cmd.append("--debug-full")
             build_cmd = container_cmd + ["-n", args.namespace, "build"]
             push_cmd = container_cmd + ["-n", args.namespace, "push"]
         case "docker":
+            if args.debug:
+                container_cmd.extend(["-D", "-l", "debug"])
             build_cmd = container_cmd + ["buildx", "build"]
             push_cmd = container_cmd + ["push"]
         case _:
@@ -286,7 +303,7 @@ def main() -> None:
         spec.pre_build()
         image_name = get_image_name(args.repo, spec.name, args.tag)
         job_opts = ["-t", image_name, "-f", "Dockerfile", "."]
-        job_set[component] = JobQueue(component)
+        job_set[component] = JobQueue(component, debug=args.debug)
         logging.debug(f"Adding job {build_cmd + job_opts}")
         job_set[component].add_job(Job(build_cmd + job_opts, spec.dir))
         if args.repo is not None:
