9a91fc86daca5d45e6611fd5a0211cd15e9d3943
Andra Constantin <andrac@princeton.edu>
2024-12-13 16:14:06 -0500

    add MUI drawer component progress

--- src/components/AnalyticsConsent/AnalyticsConsent.tsx
+++ src/components/AnalyticsConsent/AnalyticsConsent.tsx
@@ -1,20 +1,34 @@
-import { ReactElement } from "react";
+import { Button } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+import { ReactElement, useState } from "react";
 
 interface ConsentProps {
   onChangeConsent: (consentVal: boolean) => void;
 }
 
 export function AnalyticsConsent(props: ConsentProps): ReactElement {
+
+  const [responded, setResponded] = useState(false);
   const acceptAnalytics = (): void => {
+
+    setResponded(true);
     props.onChangeConsent(true);
   };
   const rejectAnalytics = (): void => {
+
+    setResponded(false);
     props.onChangeConsent(false);
   };
+
+
   return (
     <div>
-      <button onClick={acceptAnalytics}>Accept</button>
-      <button onClick={rejectAnalytics}>Reject</button>
+      <Drawer anchor={"bottom"} open={!responded}>
+        MyDrawer!
+        <button onClick={acceptAnalytics}>Accept</button>
+        <button onClick={rejectAnalytics}>Reject</button>
+      </Drawer>
+      
     </div>
   );
 }
