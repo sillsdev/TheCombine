f95269119adf7afae6551dcbea982bbd5f7408b7
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-01-23 16:17:40 -0500

    Tweek ip checking

--- Backend/Otel/LocationProvider.cs
+++ Backend/Otel/LocationProvider.cs
@@ -27,24 +27,22 @@ namespace BackendFramework.Otel
             // because OtelKernel calls the function for each activity
             if (_contextAccessor.HttpContext is { } context)
             {
-                var ipAddress = context.GetServerVariable("HTTP_X_FORWARDED_FOR") ?? context.Connection.RemoteIpAddress?.ToString();
+                var ipAddress = context.GetServerVariable("HTTP_X_FORWARDED_FOR")
+                    ?? context.GetServerVariable("REMOTE_ADDR")
+                    ?? context.GetServerVariable("HTTP_CLIENT_IP")
+                    ?? context.Connection.RemoteIpAddress?.ToString();
                 var ipAddressWithoutPort = ipAddress?.Split(':')[0];
+                if (string.IsNullOrEmpty(ipAddressWithoutPort))
+                {
+                    return null;
+                }
 
                 return await _memoryCache.GetOrCreateAsync(
                     "location_" + ipAddressWithoutPort,
                     async (cacheEntry) =>
                     {
                         cacheEntry.SlidingExpiration = TimeSpan.FromHours(1);
-                        try
-                        {
-                            return await GetLocationFromIp(ipAddressWithoutPort);
-                        }
-                        catch
-                        {
-                            // TODO consider what to have in catch
-                            Console.WriteLine("Attempted to get location but exception");
-                            throw;
-                        }
+                        return await GetLocationFromIp(ipAddressWithoutPort);
                     }
                 );
             }
