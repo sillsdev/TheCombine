8dbcd52c50621cc650bc34dbd75a4d9758231312
D. Ror. <imnasnainaec@gmail.com>
2025-03-04 14:02:03 -0500

    Convert many easy tests to testing-library (#3617)

--- src/components/TreeView/TreeDepiction/tests/DomainTileButton.test.tsx
+++ src/components/TreeView/TreeDepiction/tests/DomainTileButton.test.tsx
@@ -1,23 +1,16 @@
-import { Button } from "@mui/material";
-import { ReactTestRenderer, act, create } from "react-test-renderer";
+import { act, render, screen } from "@testing-library/react";
 
 import DomainTileButton from "components/TreeView/TreeDepiction/DomainTileButton";
 import { Direction } from "components/TreeView/TreeDepiction/TreeDepictionTypes";
 import domMap, { mapIds } from "components/TreeView/tests/SemanticDomainMock";
 
-let tileMaster: ReactTestRenderer;
 const MOCK_ANIMATE = jest.fn();
 
 describe("DomainTileButton", () => {
-  it("renders tile and matches the latest snapshot", async () => {
-    await createTile();
-    snapTest();
-  });
-
   it("calls function on click", async () => {
     await createTile();
     expect(MOCK_ANIMATE).toHaveBeenCalledTimes(0);
-    tileMaster.root.findByType(Button).props.onClick();
+    screen.getByRole("button").click();
     expect(MOCK_ANIMATE).toHaveBeenCalledTimes(1);
   });
 });
@@ -25,7 +18,7 @@ describe("DomainTileButton", () => {
 // Creates the tile
 async function createTile(direction = Direction.Next): Promise<void> {
   await act(async () => {
-    tileMaster = create(
+    render(
       <DomainTileButton
         direction={direction}
         domain={domMap[mapIds.parent]}
@@ -33,9 +26,4 @@ async function createTile(direction = Direction.Next): Promise<void> {
       />
     );
   });
-}
-
-// Perform a snapshot test
-function snapTest(): void {
-  expect(tileMaster.toJSON()).toMatchSnapshot();
 }
--- src/components/UserSettings/tests/UserSettings.test.tsx
+++ src/components/UserSettings/tests/UserSettings.test.tsx
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom";
-import { cleanup, render, screen } from "@testing-library/react";
+import { act, cleanup, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { act } from "react";
 
 import { User } from "api/models";
 import UserSettingsGetUser, {
