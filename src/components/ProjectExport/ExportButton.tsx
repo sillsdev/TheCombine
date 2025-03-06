db478f9e730f0a117dc8408453aef860c37df1d3
Andra Constantin <andrac@princeton.edu>
2025-03-05 23:11:28 -0500

    export cancellation simple button

--- src/components/ProjectExport/ExportButton.tsx
+++ src/components/ProjectExport/ExportButton.tsx
@@ -1,11 +1,15 @@
+import { Cancel } from "@mui/icons-material";
 import { Tooltip } from "@mui/material";
-import { ButtonProps } from "@mui/material/Button";
+import Button, { ButtonProps } from "@mui/material/Button";
 import { ReactElement, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { isFrontierNonempty } from "backend";
 import { LoadingButton } from "components/Buttons";
-import { asyncExportProject } from "components/ProjectExport/Redux/ExportProjectActions";
+import {
+  asyncExportProject,
+  asyncResetExport,
+} from "components/ProjectExport/Redux/ExportProjectActions";
 import { ExportStatus } from "components/ProjectExport/Redux/ExportProjectReduxTypes";
 import { useAppDispatch, useAppSelector } from "rootRedux/hooks";
 import { type StoreState } from "rootRedux/types";
@@ -20,9 +24,14 @@ export default function ExportButton(props: ExportButtonProps): ReactElement {
   const dispatch = useAppDispatch();
   const [exports, setExports] = useState(false);
   const { t } = useTranslation();
+  // const [canceling, setCanceling] = useState(false);
 
   async function exportProj(): Promise<void> {
     await dispatch(asyncExportProject(props.projectId));
+  }
+
+  async function resetExport(): Promise<void> {
+    await dispatch(asyncResetExport);
   }
 
   const exportResult = useAppSelector(
@@ -38,20 +47,27 @@ export default function ExportButton(props: ExportButtonProps): ReactElement {
   }, [props.projectId]);
 
   return (
-    <Tooltip title={!exports ? t("projectExport.cannotExportEmpty") : ""}>
-      <span>
-        <LoadingButton
-          loading={loading}
-          disabled={loading || !exports}
-          buttonProps={{
-            ...props.buttonProps,
-            onClick: exportProj,
-            id: `project-${props.projectId}-export`,
-          }}
-        >
-          {t("buttons.export")}
-        </LoadingButton>
-      </span>
-    </Tooltip>
+    <>
+      <Tooltip title={!exports ? t("projectExport.cannotExportEmpty") : ""}>
+        <span>
+          <LoadingButton
+            loading={loading}
+            disabled={loading || !exports}
+            buttonProps={{
+              ...props.buttonProps,
+              onClick: exportProj,
+              id: `project-${props.projectId}-export`,
+            }}
+          >
+            {t("buttons.export")}
+          </LoadingButton>
+          {loading && (
+            <Button onClick={resetExport}>
+              <Cancel />
+            </Button>
+          )}
+        </span>
+      </Tooltip>
+    </>
   );
 }
