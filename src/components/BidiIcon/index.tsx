ce69e5fa13ab43165b3421e3a7244d9c098c1d03
D. Ror <imnasnainaec@gmail.com>
2025-03-17 09:51:58 -0400

    Add warning; Flip another icon

--- src/components/BidiIcon/index.tsx
+++ src/components/BidiIcon/index.tsx
@@ -1,14 +1,15 @@
-import { SvgIconComponent } from "@mui/icons-material";
-import { SvgIconOwnProps } from "@mui/material";
-import { CommonProps } from "@mui/material/OverridableComponent";
-import { ReactElement } from "react";
+import { type SvgIconComponent } from "@mui/icons-material";
+import { type CommonProps } from "@mui/material/OverridableComponent";
+import { type SvgIconOwnProps } from "@mui/material/SvgIcon";
+import { type ReactElement } from "react";
 
 interface BidiIconProps {
   icon: SvgIconComponent;
   iconProps?: CommonProps & SvgIconOwnProps;
 }
 
-/** Icon that will flip horizontally if the document is rtl. */
+/** Icon that will flip horizontally if the document is rtl.
+ * Warning: overrides `props.iconProps.style.transform`. */
 export default function BidiIcon(props: BidiIconProps): ReactElement {
   return (
     <props.icon
