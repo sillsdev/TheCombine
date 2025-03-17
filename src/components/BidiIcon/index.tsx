97b77dfd29c87b131dda3c2eb4247af2dbc8ea93
D. Ror <imnasnainaec@gmail.com>
2025-03-17 10:23:03 -0400

    Flip some project-settings icons

--- src/components/BidiIcon/index.tsx
+++ src/components/BidiIcon/index.tsx
@@ -5,11 +5,13 @@ import { type ReactElement } from "react";
 
 interface BidiIconProps {
   icon: SvgIconComponent;
-  iconProps?: CommonProps & SvgIconOwnProps;
+  iconProps?: CommonProps & SvgIconOwnProps & { "data-testid"?: string };
 }
 
 /** Icon that will flip horizontally if the document is rtl.
- * Warning: overrides `props.iconProps.style.transform`. */
+ * Warning: overrides `props.iconProps.style.transform`.
+ * Also, cannot receive a ref until we move to React 19
+ * (https://react.dev/blog/2024/12/05/react-19#ref-as-a-prop). */
 export default function BidiIcon(props: BidiIconProps): ReactElement {
   return (
     <props.icon
