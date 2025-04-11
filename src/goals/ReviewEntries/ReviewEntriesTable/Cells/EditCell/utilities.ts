8241309060689bd409e0f62663c17f65babd7ce5
D. Ror <imnasnainaec@gmail.com>
2025-04-11 09:39:44 -0400

    Remove redundant normalization

--- src/goals/ReviewEntries/ReviewEntriesTable/Cells/EditCell/utilities.ts
+++ src/goals/ReviewEntries/ReviewEntriesTable/Cells/EditCell/utilities.ts
@@ -12,14 +12,14 @@ import { newFlag, newNote } from "types/word";
 /** Trim whitespace off all definition texts, then remove those with empty text.  */
 function trimDefinitions(definitions: Definition[]): Definition[] {
   return definitions
-    .map((d) => ({ ...d, text: d.text.trim().normalize("NFC") }))
+    .map((d) => ({ ...d, text: d.text.trim() }))
     .filter((d) => d.text.length);
 }
 
 /** Trim whitespace off all gloss defs, then remove those with empty def.  */
 function trimGlosses(glosses: Gloss[]): Gloss[] {
   return glosses
-    .map((g) => ({ ...g, def: g.def.trim().normalize("NFC") }))
+    .map((g) => ({ ...g, def: g.def.trim() }))
     .filter((g) => g.def.length);
 }
 
@@ -131,7 +131,7 @@ export function cleanSense(
  * - all senses are empty/deleted */
 export function cleanWord(word: Word, options?: CleanOptions): Word | string {
   // Make sure vernacular isn't empty.
-  const vernacular = word.vernacular.trim().normalize("NFC");
+  const vernacular = word.vernacular.trim();
   if (!vernacular.length) {
     return "reviewEntries.error.vernacular";
   }
@@ -155,14 +155,12 @@ export function cleanWord(word: Word, options?: CleanOptions): Word | string {
   }
 
   // Clear note language if text empty.
-  const noteText = word.note.text.trim().normalize("NFC");
+  const noteText = word.note.text.trim();
   const note = newNote(noteText, noteText ? word.note.language : "");
 
   // Clear flag text if flag not active.
   const flagActive = word.flag.active;
-  const flag = newFlag(
-    flagActive ? word.flag.text.trim().normalize("NFC") : undefined
-  );
+  const flag = newFlag(flagActive ? word.flag.text.trim() : undefined);
   flag.active = flagActive;
 
   return { ...word, flag, note, senses, vernacular };
