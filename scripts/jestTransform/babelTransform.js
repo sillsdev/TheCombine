87b0a07444f98f3b1029fc4014270bb47b3168a6
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-05-07 08:41:01 -0400

    Fix tests

--- scripts/jestTransform/babelTransform.js
+++ scripts/jestTransform/babelTransform.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // File transformer to transform all .jsx/.ts/.tsx files into plain javascript for Jest.
+// Not in a babel.config.js file since it's only used for Jest.
 // Modified from:
 // https://github.com/facebook/create-react-app/blob/main/packages/react-scripts/config/jest/babelTransform.js
 
@@ -12,6 +13,7 @@ module.exports = createTransformer({
     [require.resolve("@babel/preset-react"), { runtime: "automatic" }],
     require.resolve("@babel/preset-typescript"),
   ],
+  plugins: [require.resolve("babel-plugin-transform-import-meta")],
   babelrc: false,
   configFile: false,
 });
