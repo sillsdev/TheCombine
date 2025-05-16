d397f1d419ee22a692cc110ad1c7726e598edaab
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-05-16 10:35:21 -0400

    Migrate buttons

--- src/components/Buttons/tests/DeleteButtonWithDialog.test.tsx
+++ src/components/Buttons/tests/DeleteButtonWithDialog.test.tsx
@@ -1,24 +1,22 @@
-import { ReactTestRenderer, act, create } from "react-test-renderer";
+import {
+  act,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import DeleteButtonWithDialog from "components/Buttons/DeleteButtonWithDialog";
-import { CancelConfirmDialog } from "components/Dialogs";
 
-// Dialog uses portals, which are not supported in react-test-renderer.
-jest.mock("@mui/material/Dialog", () =>
-  jest.requireActual("@mui/material/Container")
-);
-
-const mockDelete = jest.fn();
 const buttonId = "button-id";
 const buttonIdCancel = "button-id-cancel";
 const buttonIdConfirm = "button-id-confirm";
+const mockDelete = jest.fn();
 const textId = "text-id";
-
-let cellHandle: ReactTestRenderer;
 
 const renderDeleteCell = async (): Promise<void> => {
   await act(async () => {
-    cellHandle = create(
+    render(
       <DeleteButtonWithDialog
         buttonId={buttonId}
         buttonIdCancel={buttonIdCancel}
@@ -37,43 +35,37 @@ beforeEach(async () => {
 
 describe("DeleteCell", () => {
   it("has working dialog buttons", async () => {
-    const dialog = cellHandle.root.findByType(CancelConfirmDialog);
-    const deleteButton = cellHandle.root.findByProps({ id: buttonId });
+    // Renders with dialog closed.
+    expect(screen.queryByRole("dialog")).toBeNull();
 
-    expect(dialog.props.open).toBeFalsy();
-    await act(async () => {
-      deleteButton.props.onClick();
-    });
-    expect(dialog.props.open).toBeTruthy();
-    const cancelButton = cellHandle.root.findByProps({ id: buttonIdCancel });
-    await act(async () => {
-      cancelButton.props.onClick();
-    });
-    expect(dialog.props.open).toBeFalsy();
-    await act(async () => {
-      deleteButton.props.onClick();
-    });
-    expect(dialog.props.open).toBeTruthy();
-    const confButton = cellHandle.root.findByProps({ id: buttonIdConfirm });
-    await act(async () => {
-      await confButton.props.onClick();
-    });
-    expect(dialog.props.open).toBeFalsy();
+    // Delete button opens the dialog.
+    await userEvent.click(screen.getByTestId(buttonId));
+    expect(screen.queryByRole("dialog")).not.toBeNull();
+
+    // Cancel button closes the dialog.
+    await userEvent.click(screen.getByTestId(buttonIdCancel));
+    await waitForElementToBeRemoved(() => screen.queryByRole("dialog")); // else it's just hidden
+    expect(screen.queryByRole("dialog")).toBeNull();
+
+    // Delete button opens the dialog.
+    await userEvent.click(screen.getByTestId(buttonId));
+    expect(screen.queryByRole("dialog")).not.toBeNull();
+
+    // Confirm button closes the dialog.
+    await userEvent.click(screen.getByTestId(buttonIdConfirm));
+    await waitForElementToBeRemoved(() => screen.queryByRole("dialog")); // else it's just hidden
+    expect(screen.queryByRole("dialog")).toBeNull();
   });
 
   it("only deletes after confirmation", async () => {
-    const deleteButton = cellHandle.root.findByProps({ id: buttonId });
-
-    await act(async () => {
-      deleteButton.props.onClick();
-      cellHandle.root.findByProps({ id: buttonIdCancel }).props.onClick();
-      deleteButton.props.onClick();
-    });
+    // Delete function not called by clicking delete and cancel.
+    await userEvent.click(screen.getByTestId(buttonId));
+    await userEvent.click(screen.getByTestId(buttonIdCancel));
+    await userEvent.click(screen.getByTestId(buttonId));
     expect(mockDelete).not.toHaveBeenCalled();
-    const confButton = cellHandle.root.findByProps({ id: buttonIdConfirm });
-    await act(async () => {
-      await confButton.props.onClick();
-    });
+
+    // Delete function called upon clicking confirm.
+    await userEvent.click(screen.getByTestId(buttonIdConfirm));
     expect(mockDelete).toHaveBeenCalled();
   });
 });
