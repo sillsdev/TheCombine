37c26ccf862198da174d1dfb3fca3867202d4ba4
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-05-20 08:36:01 -0400

    Revert StyledMenuItem change

--- src/components/DataEntry/DataEntryTable/NewEntry/StyledMenuItem.ts
+++ src/components/DataEntry/DataEntryTable/NewEntry/StyledMenuItem.ts
@@ -1,15 +1,10 @@
 import { MenuItem } from "@mui/material";
 import { styled } from "@mui/system";
-import { CSSProperties } from "react";
-
-export const styledBorder: CSSProperties = {
-  border: "1px solid gray",
-  borderRadius: "8px",
-};
 
 // https://mui.com/system/styled/#using-the-theme
 const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
-  ...styledBorder,
+  border: "1px solid gray",
+  borderRadius: "8px",
   marginTop: "8px",
   "&.Mui-disabled": { opacity: 1 },
   "&:focus": {
