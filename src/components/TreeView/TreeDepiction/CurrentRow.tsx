dadd55b20878bc4bcd0ce0a67d0be983220b5494
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-05-27 15:08:03 -0400

    [TreeView] Use wrapping children grid for narrow windows

--- src/components/TreeView/TreeDepiction/CurrentRow.tsx
+++ src/components/TreeView/TreeDepiction/CurrentRow.tsx
@@ -1,4 +1,4 @@
-import { Button, ImageList, ImageListItem } from "@mui/material";
+import { Box, Button, Grid2, ImageList, ImageListItem } from "@mui/material";
 import { ReactElement } from "react";
 
 import DomainTileButton, {
@@ -6,32 +6,102 @@ import DomainTileButton, {
 } from "components/TreeView/TreeDepiction/DomainTileButton";
 import {
   Direction,
-  TreeDepictionProps,
+  TreeRowProps,
 } from "components/TreeView/TreeDepiction/TreeDepictionTypes";
+import { parent as parentSvg } from "resources/tree";
 
-export default function CurrentRow(props: TreeDepictionProps): ReactElement {
-  const { next, previous } = props.currentDomain;
+export default function CurrentRow(props: TreeRowProps): ReactElement {
+  return props.small ? (
+    <CurrentRowSm {...props} />
+  ) : (
+    <CurrentRowLg {...props} />
+  );
+}
 
-  const currentTile = (
+function CurrentTile(props: TreeRowProps): ReactElement {
+  const { animate, currentDomain } = props;
+
+  return (
     <Button
-      color="primary"
-      disabled={!props.currentDomain.parent}
+      disabled={!currentDomain.parent}
       fullWidth
       id="current-domain"
-      onClick={() => props.animate(props.currentDomain)}
-      size="large"
-      style={{ height: "95%" }}
+      onClick={() => animate(currentDomain)}
+      sx={{ height: "100%", p: 1 }}
       variant="contained"
     >
-      <DomainText domain={props.currentDomain} extraProps={{ minWidth: 200 }} />
+      <DomainText domain={currentDomain} />
     </Button>
   );
+}
 
-  return props.small ? (
-    currentTile
-  ) : (
-    <ImageList cols={7} gap={20} rowHeight={"auto"}>
-      <ImageListItem cols={2}>
+function CurrentRowLg(props: TreeRowProps): ReactElement {
+  const { next, parent, previous } = props.currentDomain;
+
+  return (
+    <>
+      {parent && (
+        <>
+          <Box>
+            <DomainTileButton
+              direction={Direction.Up}
+              domain={parent}
+              onClick={props.animate}
+            />
+          </Box>
+          <img
+            src={parentSvg}
+            style={{ transform: "scaleY(-1)" }}
+            width={props.colWidth}
+          />
+        </>
+      )}
+      <ImageList cols={7} gap={20} sx={{ mx: 1, my: 0 }}>
+        <ImageListItem cols={2}>
+          {previous && (
+            <DomainTileButton
+              direction={Direction.Prev}
+              domain={previous}
+              onClick={props.animate}
+            />
+          )}
+        </ImageListItem>
+        <ImageListItem cols={3}>
+          <CurrentTile {...props} />
+        </ImageListItem>
+        <ImageListItem cols={2}>
+          {next && (
+            <DomainTileButton
+              direction={Direction.Next}
+              domain={next}
+              onClick={props.animate}
+            />
+          )}
+        </ImageListItem>
+      </ImageList>
+    </>
+  );
+}
+
+function CurrentRowSm(props: TreeRowProps): ReactElement {
+  const { next, parent, previous } = props.currentDomain;
+
+  return (
+    <Grid2 container spacing={2} sx={{ px: 2, width: window.innerWidth }}>
+      {!!parent && (
+        <>
+          <Grid2 key="uncle" size={4} />
+          <Grid2 key="parent" size={4}>
+            <DomainTileButton
+              direction={Direction.Up}
+              domain={parent}
+              onClick={props.animate}
+            />
+          </Grid2>
+          <Grid2 key="aunt" size={4} />
+        </>
+      )}
+      <Grid2 key="prev" size={4}>
         {previous && (
           <DomainTileButton
             direction={Direction.Prev}
@@ -39,9 +109,11 @@ export default function CurrentRow(props: TreeDepictionProps): ReactElement {
             onClick={props.animate}
           />
         )}
-      </ImageListItem>
-      <ImageListItem cols={3}>{currentTile}</ImageListItem>
-      <ImageListItem cols={2}>
+      </Grid2>
+      <Grid2 key="current" size={4}>
+        <CurrentTile {...props} />
+      </Grid2>
+      <Grid2 key="next" size={4}>
         {next && (
           <DomainTileButton
             direction={Direction.Next}
@@ -49,7 +121,7 @@ export default function CurrentRow(props: TreeDepictionProps): ReactElement {
             onClick={props.animate}
           />
         )}
-      </ImageListItem>
-    </ImageList>
+      </Grid2>
+    </Grid2>
   );
 }
--- src/components/TreeView/TreeDepiction/tests/CurrentRow.test.tsx
+++ src/components/TreeView/TreeDepiction/tests/CurrentRow.test.tsx
@@ -7,24 +7,48 @@ import testDomainMap, {
 } from "components/TreeView/tests/SemanticDomainMock";
 
 describe("CurrentRow", () => {
-  it("renders with no siblings", async () => {
-    await createTree(testDomainMap[mapIds.parent]);
-    expect(screen.getAllByRole("button")).toHaveLength(1);
+  describe("wide", () => {
+    it("renders with no parent and no siblings", async () => {
+      await createTree(testDomainMap[mapIds.head]);
+      expect(screen.getAllByRole("button")).toHaveLength(1);
+    });
+
+    it("renders with parent and no siblings", async () => {
+      await createTree(testDomainMap[mapIds.parent]);
+      expect(screen.getAllByRole("button")).toHaveLength(2);
+    });
+
+    it("renders with parent and 1 sibling", async () => {
+      await createTree(testDomainMap[mapIds.firstKid]);
+      expect(screen.getAllByRole("button")).toHaveLength(3);
+    });
+
+    it("renders with parent and 2 siblings", async () => {
+      await createTree(testDomainMap[mapIds.middleKid]);
+      expect(screen.getAllByRole("button")).toHaveLength(4);
+    });
   });
 
-  it("renders with 1 sibling", async () => {
-    await createTree(testDomainMap[mapIds.firstKid]);
-    expect(screen.getAllByRole("button")).toHaveLength(2);
-  });
+  describe("narrow", () => {
+    it("renders with no parent and no siblings", async () => {
+      await createTree(testDomainMap[mapIds.head]);
+      expect(screen.getAllByRole("button")).toHaveLength(1);
+    });
 
-  it("renders with 2 siblings", async () => {
-    await createTree(testDomainMap[mapIds.middleKid]);
-    expect(screen.getAllByRole("button")).toHaveLength(3);
-  });
+    it("renders with parent and no siblings", async () => {
+      await createTree(testDomainMap[mapIds.parent], true);
+      expect(screen.getAllByRole("button")).toHaveLength(2);
+    });
 
-  it("hides siblings when small", async () => {
-    await createTree(testDomainMap[mapIds.middleKid], true);
-    expect(screen.getAllByRole("button")).toHaveLength(1);
+    it("renders with parent and 1 sibling", async () => {
+      await createTree(testDomainMap[mapIds.firstKid], true);
+      expect(screen.getAllByRole("button")).toHaveLength(3);
+    });
+
+    it("renders with parent and 2 siblings", async () => {
+      await createTree(testDomainMap[mapIds.middleKid], true);
+      expect(screen.getAllByRole("button")).toHaveLength(4);
+    });
   });
 });
 
@@ -34,7 +58,12 @@ async function createTree(
 ): Promise<void> {
   await act(async () => {
     render(
-      <CurrentRow animate={jest.fn()} currentDomain={domain} small={small} />
+      <CurrentRow
+        animate={jest.fn()}
+        colWidth={100}
+        currentDomain={domain}
+        small={small}
+      />
     );
   });
 }
