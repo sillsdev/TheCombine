2207bd818fdc1dba1d80273e280b61d4db19f3f9
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-06-10 12:15:55 -0400

    Tighten security on user info

--- src/api/api/project-api.ts
+++ src/api/api/project-api.ts
@@ -39,9 +39,9 @@ import {
 // @ts-ignore
 import { Project } from "../models";
 // @ts-ignore
-import { User } from "../models";
-// @ts-ignore
 import { UserCreatedProject } from "../models";
+// @ts-ignore
+import { UserStub } from "../models";
 /**
  * ProjectApi - axios parameter creator
  * @export
@@ -551,7 +551,10 @@ export const ProjectApiFp = function (configuration?: Configuration) {
       projectId: string,
       options?: any
     ): Promise<
-      (axios?: AxiosInstance, basePath?: string) => AxiosPromise<Array<User>>
+      (
+        axios?: AxiosInstance,
+        basePath?: string
+      ) => AxiosPromise<Array<UserStub>>
     > {
       const localVarAxiosArgs =
         await localVarAxiosParamCreator.getAllProjectUsers(projectId, options);
@@ -738,7 +741,7 @@ export const ProjectApiFactory = function (
     getAllProjectUsers(
       projectId: string,
       options?: any
-    ): AxiosPromise<Array<User>> {
+    ): AxiosPromise<Array<UserStub>> {
       return localVarFp
         .getAllProjectUsers(projectId, options)
         .then((request) => request(axios, basePath));
