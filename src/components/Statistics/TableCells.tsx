2f5fdd8638603b99fa8839ed50d798fe5155fa4d
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-06-19 09:02:32 -0400

    Simplify cell styling

--- src/components/Statistics/TableCells.tsx
+++ src/components/Statistics/TableCells.tsx
@@ -1,18 +1,12 @@
 import { TableCell, Typography } from "@mui/material";
-import { CSSProperties, ReactElement } from "react";
+import { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
-
-const cellStyle: CSSProperties = {
-  borderBottomStyle: "dotted",
-  borderBottomWidth: 1,
-  position: "relative",
-};
 
 export function HeadCell(props: { titleId: string }): ReactElement {
   const { t } = useTranslation();
 
   return (
-    <TableCell style={cellStyle}>
+    <TableCell sx={{ borderBottomWidth: 1 }}>
       <Typography variant="subtitle1">{t(props.titleId)}</Typography>
     </TableCell>
   );
@@ -20,7 +14,7 @@ export function HeadCell(props: { titleId: string }): ReactElement {
 
 export function Cell(props: { text?: string | number | null }): ReactElement {
   return (
-    <TableCell style={cellStyle}>
+    <TableCell sx={{ borderBottomStyle: "dotted", borderBottomWidth: 1 }}>
       <Typography variant="body1">{props.text}</Typography>
     </TableCell>
   );
