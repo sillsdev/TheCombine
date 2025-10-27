2a762966a431e33be2c0a969f26a09a703006a0f
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-10-27 08:24:50 -0400

    Consolidate images

---
+++ src/resources/htw/index.js
@@ -0,0 +1,6 @@
+import copyright from "resources/htw/ccBySa.png";
+import harvest from "resources/htw/harvestEthiopia.jpg";
+import thresh from "resources/htw/threshBangladesh.jpg";
+import winnow from "resources/htw/winnowIndia.jpg";
+
+export { copyright, harvest, thresh, winnow };
