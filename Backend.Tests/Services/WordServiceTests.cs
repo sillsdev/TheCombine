7d68d6c1758c2065ecf98b880b8c2bb8cbe60429
Danny Rorabaugh <imnasnainaec@gmail.com>
2026-02-16 09:22:44 -0500

    Add missing Word Controller/Service tests

--- Backend.Tests/Services/WordServiceTests.cs
+++ Backend.Tests/Services/WordServiceTests.cs
@@ -80,6 +80,37 @@ namespace Backend.Tests.Services
         }
 
         [Test]
+        public void TestDeleteFrontierWordNotInFrontierNull()
+        {
+            var wordNotInFrontier = _wordRepo.Add(new Word { ProjectId = ProjId }).Result;
+            Assert.That(_wordService.DeleteFrontierWord(ProjId, UserId, wordNotInFrontier.Id).Result, Is.Null);
+            Assert.That(_wordService.DeleteFrontierWord("wrong-proj", UserId, WordId).Result, Is.Null);
+        }
+
+        [Test]
+        public void TestDeleteFrontierWordCopiesToWordsAndRemovesFrontier()
+        {
+            var oldId = _wordRepo.Create(new Word { ProjectId = ProjId }).Result.Id;
+
+            var deletedId = _wordService.DeleteFrontierWord(ProjId, UserId, oldId).Result;
+
+            Assert.That(deletedId, Is.Not.Null);
+            Assert.That(deletedId, Is.Not.EqualTo(oldId));
+            var deletedWord = _wordRepo.GetWord(ProjId, deletedId!).Result;
+            Assert.That(deletedWord, Is.Not.Null);
+            Assert.That(deletedWord!.Accessibility, Is.EqualTo(Status.Deleted));
+            Assert.That(deletedWord!.History.Last(), Is.EqualTo(oldId));
+            Assert.That(deletedWord!.EditedBy.Last(), Is.EqualTo(UserId));
+
+            var allWordIds = _wordRepo.GetAllWords(ProjId).Result.Select(w => w.Id).ToList();
+            Assert.That(allWordIds, Has.Count.EqualTo(2));
+            Assert.That(allWordIds, Does.Contain(oldId));
+            Assert.That(allWordIds, Does.Contain(deletedId!));
+
+            Assert.That(_wordRepo.GetFrontier(ProjId).Result, Is.Empty);
+        }
+
+        [Test]
         public void TestUpdateNotInFrontierNull()
         {
             Assert.That(_wordService.Update(ProjId, UserId, WordId, new Word()).Result, Is.Null);
