b640c3fa30c1c5b0737ed41d7ba6c65ea3733210
Danny Rorabaugh <imnasnainaec@gmail.com>
2026-03-05 16:22:03 -0500

    Add new public repo methods to interface

--- Backend/Interfaces/IWordRepository.cs
+++ Backend/Interfaces/IWordRepository.cs
@@ -10,9 +10,17 @@ namespace BackendFramework.Interfaces
         Task<List<Word>> GetAllWords(string projectId);
         Task<Word?> GetWord(string projectId, string wordId);
         Task<List<Word>> GetWords(string projectId, List<string> wordIds);
+        Task<Word> RepoCreate(Word word);
+        Task<List<Word>> RepoCreate(List<Word> words);
         Task<Word> Create(Word word, bool clearModified = true);
         Task<List<Word>> Create(List<Word> words, bool clearModified = true);
+        Task<Word?> RepoUpdateFrontier(Word word, Action<Word, Word> modifyNewWordFromOldWord);
         Task<Word?> Update(Word word);
+        Task<List<Word>?> RepoReplaceFrontier(string projectId, List<Word> newWords,
+            List<string> idsToDelete, Action<Word, Word> modifyUpdatedWord, Action<Word> modifyDeletedWord);
+        Task<List<Word>> RepoRevertReplaceFrontier(string projectId, List<string> idsToRestore,
+            List<string> idsToDelete, Action<Word> modifyDeletedWord);
+        Task<Word?> RepoDeleteFrontier(string projectId, string wordId, Action<Word> modifyWord);
         Task<Word?> ModifyAndDeleteFrontier(string projectId, string wordId, Func<Word, Word> modifyWord);
         Task<bool> DeleteAllFrontierWords(string projectId);
         Task<bool> HasWords(string projectId);
@@ -23,6 +31,7 @@ namespace BackendFramework.Interfaces
         Task<List<Word>> GetAllFrontier(string projectId);
         Task<Word?> GetFrontier(string projectId, string wordId, string? audioFileName = null);
         Task<List<Word>> GetFrontierWithVernacular(string projectId, string vernacular);
+        Task<List<Word>> RepoRestoreFrontier(string projectId, List<string> wordIds);
         Task<List<Word>> AddFrontier(List<Word> words);
         Task<int> CountFrontierWordsWithDomain(string projectId, string domainId);
     }
