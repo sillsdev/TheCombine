149661851337a866c3c7b4518dacc9316c387bd3
Danny Rorabaugh <imnasnainaec@gmail.com>
2026-03-09 15:41:45 -0400

    Fix up WordRepository public method order

--- Backend/Interfaces/IWordRepository.cs
+++ Backend/Interfaces/IWordRepository.cs
@@ -11,13 +11,6 @@ namespace BackendFramework.Interfaces
         Task<Word?> GetWord(string projectId, string wordId);
         Task<Word> Create(Word word);
         Task<List<Word>> Create(List<Word> words);
-        Task<Word?> UpdateFrontier(string projectId, string wordId, Action<Word> modifyWord);
-        Task<Word?> UpdateFrontier(Word word, Action<Word, Word?> modifyNewWordFromOldWord);
-        Task<List<Word>?> ReplaceFrontier(string projectId, List<Word> newWords,
-            List<string> idsToDelete, Action<Word, Word?> modifyUpdatedWord, Action<Word> modifyDeletedWord);
-        Task<bool> RevertReplaceFrontier(string projectId, List<string> idsToRestore,
-            List<string> idsToDelete, Action<Word> modifyDeletedWord);
-        Task<Word?> DeleteFrontier(string projectId, string wordId, Action<Word> modifyWord);
         Task<bool> DeleteAllFrontierWords(string projectId);
         Task<bool> HasWords(string projectId);
         Task<bool> HasFrontierWords(string projectId);
@@ -27,8 +20,15 @@ namespace BackendFramework.Interfaces
         Task<List<Word>> GetAllFrontier(string projectId);
         Task<Word?> GetFrontier(string projectId, string wordId, string? audioFileName = null);
         Task<List<Word>> GetFrontierWithVernacular(string projectId, string vernacular);
-        Task<bool> RestoreFrontier(string projectId, string wordId);
         Task<List<Word>> AddFrontier(List<Word> words);
+        Task<Word?> DeleteFrontier(string projectId, string wordId, Action<Word> modifyDeletedWord);
+        Task<bool> RestoreFrontier(string projectId, string wordId);
+        Task<Word?> UpdateFrontier(string projectId, string wordId, Action<Word> modifyUpdatedWord);
+        Task<Word?> UpdateFrontier(Word word, Action<Word, Word?> modifyUpdatedWord);
+        Task<List<Word>?> ReplaceFrontier(string projectId, List<Word> newWords, List<string> idsToDelete,
+            Action<Word, Word?> modifyUpdatedWord, Action<Word> modifyDeletedWord);
+        Task<bool> RevertReplaceFrontier(string projectId, List<string> idsToRestore, List<string> idsToDelete,
+            Action<Word> modifyDeletedWord);
         Task<int> CountFrontierWordsWithDomain(string projectId, string domainId);
     }
 }
