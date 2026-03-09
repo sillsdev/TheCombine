9f77851cce131450b20328e1dac92bbf1f2133e9
D. Ror <imnasnainaec@gmail.com>
2026-03-09 07:54:00 -0400

    Move transaction scaffolding into MongoDbContext

--- Backend.Tests/Mocks/MongoDbContextMock.cs
+++ Backend.Tests/Mocks/MongoDbContextMock.cs
@@ -8,27 +8,28 @@ namespace Backend.Tests.Mocks;
 public class MongoDbContextMock : IMongoDbContext
 {
     public IMongoDatabase Db => throw new NotSupportedException();
+
     public Task<IMongoTransaction> BeginTransaction()
+        => Task.FromResult<IMongoTransaction>(new MongoTransactionMock());
+
+    public Task<T> ExecuteWithTransaction<T>(Func<IClientSessionHandle, Task<T>> operation)
     {
-        return Task.FromResult<IMongoTransaction>(new MongoTransactionMock());
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> ExecuteWithTransactionAllowNull<T>(Func<IClientSessionHandle, Task<T?>> operation) where T : class
+    {
+        throw new NotImplementedException();
     }
 
     private sealed class MongoTransactionMock : IMongoTransaction
     {
         public IClientSessionHandle Session => null!;
 
-        public Task CommitTransactionAsync()
-        {
-            return Task.CompletedTask;
-        }
+        public Task CommitTransactionAsync() => Task.CompletedTask;
 
-        public Task AbortTransactionAsync()
-        {
-            return Task.CompletedTask;
-        }
+        public Task AbortTransactionAsync() => Task.CompletedTask;
 
-        public void Dispose()
-        {
-        }
+        public void Dispose() { }
     }
 }
--- Backend/Contexts/MongoDbContext.cs
+++ Backend/Contexts/MongoDbContext.cs
@@ -1,25 +1,93 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using BackendFramework.Interfaces;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
 namespace BackendFramework.Contexts;
 
+/// <summary>
+/// MongoDB context for accessing the configured database and executing transactional operations.
+/// </summary>
 public class MongoDbContext : IMongoDbContext
 {
+    /// <summary>
+    /// Gets the configured MongoDB database instance.
+    /// </summary>
     public IMongoDatabase Db { get; }
 
+    /// <summary>
+    /// Creates a new <see cref="MongoDbContext"/> from application settings.
+    /// </summary>
+    /// <param name="options">Options containing the Mongo connection string and database name.</param>
     public MongoDbContext(IOptions<Startup.Settings> options)
     {
         var client = new MongoClient(options.Value.ConnectionString);
         Db = client.GetDatabase(options.Value.CombineDatabase);
     }
 
+    /// <summary>
+    /// Begins a MongoDB transaction and returns a disposable transaction wrapper.
+    /// </summary>
+    /// <returns>A transaction wrapper containing the active client session.</returns>
     public async Task<IMongoTransaction> BeginTransaction()
     {
         var session = await Db.Client.StartSessionAsync();
         session.StartTransaction();
         return new MongoTransactionWrapper(session);
+    }
+
+    /// <summary>
+    /// Executes an operation in a transaction, committing on success and aborting on exception.
+    /// </summary>
+    /// <typeparam name="T">The operation result type.</typeparam>
+    /// <param name="operation">Operation to execute with the transaction session.</param>
+    /// <returns>The operation result.</returns>
+    public async Task<T> ExecuteWithTransaction<T>(Func<IClientSessionHandle, Task<T>> operation)
+    {
+        using var transaction = await BeginTransaction();
+        try
+        {
+            var result = await operation(transaction.Session);
+            await transaction.CommitTransactionAsync();
+            return result;
+        }
+        catch
+        {
+            await transaction.AbortTransactionAsync();
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Executes an operation in a transaction, committing when a non-null result is returned.
+    /// </summary>
+    /// <typeparam name="T">The operation result reference type.</typeparam>
+    /// <param name="operation">Operation to execute with the transaction session.</param>
+    /// <returns>
+    /// The operation result when non-null; otherwise <see langword="null"/> after aborting the transaction.
+    /// </returns>
+    public async Task<T?> ExecuteWithTransactionAllowNull<T>(Func<IClientSessionHandle, Task<T?>> operation)
+        where T : class
+    {
+        using var transaction = await BeginTransaction();
+        try
+        {
+            var result = await operation(transaction.Session);
+            if (result is null)
+            {
+                await transaction.AbortTransactionAsync();
+                return null;
+            }
+
+            await transaction.CommitTransactionAsync();
+            return result;
+        }
+        catch
+        {
+            await transaction.AbortTransactionAsync();
+            throw;
+        }
     }
 
     private class MongoTransactionWrapper(IClientSessionHandle session) : IMongoTransaction
@@ -28,19 +96,10 @@ public class MongoDbContext : IMongoDbContext
 
         public IClientSessionHandle Session => _session;
 
-        public Task CommitTransactionAsync()
-        {
-            return _session.CommitTransactionAsync();
-        }
+        public Task CommitTransactionAsync() => _session.CommitTransactionAsync();
 
-        public Task AbortTransactionAsync()
-        {
-            return _session.AbortTransactionAsync();
-        }
+        public Task AbortTransactionAsync() => _session.AbortTransactionAsync();
 
-        public void Dispose()
-        {
-            _session.Dispose();
-        }
+        public void Dispose() => _session.Dispose();
     }
 }
