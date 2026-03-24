c219e5cd93433a66a129dde6ad3d314eb2489ff4
D. Ror <imnasnainaec@gmail.com>
2026-03-24 14:24:09 -0400

    [deploy] Use Helm's `upgrade --install` (#4207)
    - Fix setup not working with Rancher Desktop on Ubuntu 24.04 (remove `-a`/`--all`, not a valid flag in all settings, and doesn't help us since it lists non-deployed things---e.g., failed---which we don't handle anyway)
    - No longer unnecessarily dictate upgrade vs install
    - Ensure `--clean` respects `--dry-run`
    - Only run `create-admin-user` on install, not on upgrade

--- deploy/scripts/enum_types.py
+++ deploy/scripts/enum_types.py
@@ -4,16 +4,6 @@ from enum import Enum, unique
 
 
 @unique
-class HelmAction(Enum):
-    """Enumerate helm commands for maintaining The Combine on the target system."""
-
-    INSTALL = "install"
-    """INSTALL is used when the chart is not already installed on the target."""
-    UPGRADE = "upgrade"
-    """UPGRADE is used when the chart is already installed."""
-
-
-@unique
 class ExitStatus(Enum):
     SUCCESS = 0
     FAILURE = 1
